c7638a01692e8ca21c53fa1f7f779cca633560af
power12562 <kimsiwoo1005@gmail.com>
2025-04-11 18:40:43 +0900

    [TEST] 하이러키용 테스트 코드 변경
    [TEST] 하이러키용 테스트 코드 변경

--- Source/GA6thFinal_Framework/ProjectVG/Source/TestEditor/ObjectTestEditor.cpp
+++ Source/GA6thFinal_Framework/ProjectVG/Source/TestEditor/ObjectTestEditor.cpp
@@ -1,9 +1,10 @@
 ﻿#include "ObjectTestEditor.h"
+#include "Source/EditorTools/EditorInspectorView.h"
 
 using namespace u8_literals;
 using namespace Global;
 
-static void TestTreeNode(const Transform& node)
+static void TransformTreeNode(const Transform& node)
 {
     ImGui::PushID(node.gameObject.GetInstanceID());
     if (ImGui::TreeNode(node.gameObject.ToString().data()))
@@ -11,9 +12,14 @@ static void TestTreeNode(const Transform& node)
         for (int i = 0; i < node.ChildCount; i++)
         {
             Transform& child = *node.GetChild(i);
-            TestTreeNode(child); // 재귀 호출
+            TransformTreeNode(child); // 재귀 호출
         }
         ImGui::TreePop();
+    }
+    ImGui::SameLine();
+    if (ImGui::Button("Select Inspector"))
+    {
+        EditorInspectorView::SetFocusObject(node.gameObject.GetWeakPtr());
     }
     ImGui::PopID();
 }
@@ -45,7 +51,6 @@ void ObjectTestEditor::OnFrame()
         engineCore->SceneManager.LoadScene("EmptyScene");
     }
 
-    static GameObject* selectObj = nullptr;
     if (ImGui::Button(u8"오브젝트 만들기"_c_str))
     {
         NewGameObject<GameObject>("Game Object");
@@ -64,99 +69,12 @@ void ObjectTestEditor::OnFrame()
             for (auto& obj : rootObjects)
             {
                 ImGui::PushID(obj.get());
-                {
-                    ImGui::Text("%s", obj->ToString().data());
-                    if (ImGui::Button("DontDestroy"))
-                    {
-                        GameObject::DontDestroyOnLoad(obj.get());
-                    }
-
-                    obj->transform.ImGuiDrawPropertys();                    
-                    static std::string transformData;
-                    if (ImGui::Button("save"))
-                    {
-                        transformData = obj->transform.SerializedReflectFields();
-                    }
-                    ImGui::SameLine();
-                    if (ImGui::Button("load"))
-                    {
-                        obj->transform.DeserializedReflectFields(transformData);
-                    }
-                    ImGui::SameLine();
-                    ImGui::Text(transformData.c_str());
-
-                    if (ImGui::Button("AddComponent"))
-                    {
-                        ImGui::OpenPopup("Select Component");
-                        selectObj = obj.get();
-                    }
-                    ImGui::SameLine();
-                    if (ImGui::Button("Destroy"))
-                    {
-                        GameObject::Destroy(obj.get());
-                    }
-                    obj->ImGuiDrawPropertys();
-
-                    for (size_t i = 0; i < obj->GetComponentCount(); i++)
-                    {
-                        if (auto component = obj->GetComponentAtIndex<Component>(i).lock())
-                        {
-                            static std::string componentData;
-
-                            ImGui::PushID(component.get());
-                            ImGui::Separator();
-                            {     
-                                ImGui::Text("%s", component->ClassName());
-                                component->ImGuiDrawPropertys();
-                                if (ImGui::Button("Destroy"))
-                                {
-                                    GameObject::Destroy(component.get());
-                                }
-                                if (ImGui::Button("save"))
-                                {
-                                    componentData = component->SerializedReflectFields();
-                                }
-                                ImGui::SameLine();
-                                if (ImGui::Button("load"))
-                                {
-                                    component->DeserializedReflectFields(componentData);
-                                }
-                                ImGui::SameLine();
-                                ImGui::Text(componentData.c_str());
-                            }
-                            ImGui::Separator();
-                            ImGui::PopID();
-                        }
-                    }
+                {      
+                    TransformTreeNode(obj->transform);
                 }
                 ImGui::PopID();
             }
         }
-    }
-
-    if (selectObj)
-    {
-        ImGui::PushID(selectObj);
-        if (ImGui::BeginPopupModal("Select Component", nullptr, ImGuiWindowFlags_AlwaysAutoResize))
-        {
-            ImGui::BeginChild("ScrollRegion", ImVec2(300, 200), 0, ImGuiWindowFlags_HorizontalScrollbar);
-            for (auto& key :
-                 engineCore->ComponentFactory.GetNewComponentKeyList())
-            {
-                if (ImGui::Button(key.c_str()))
-                {
-                    engineCore->ComponentFactory.AddComponentToObject(selectObj, key);
-                    ImGui::CloseCurrentPopup();
-                }
-            }
-            ImGui::EndChild();
-            if (ImGui::Button("Cancel")) {
-                ImGui::CloseCurrentPopup();
-            }
-            ImGui::SameLine();
-            ImGui::EndPopup();
-        }
-        ImGui::PopID();
     }
 }
 
