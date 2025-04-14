588f3aded1354ef058ce47d7b719e805c69181d3
power12562 <kimsiwoo1005@gmail.com>
2025-04-14 17:23:12 +0900

    [FEAT] Filter 값 저장하는 기능 추가
    [FEAT] Filter 값 저장하는 기능 추가

--- Source/GA6thFinal_Framework/ProjectVG/Source/EditorTools/EditorLogsTool/EditorLogsTool.cpp
+++ Source/GA6thFinal_Framework/ProjectVG/Source/EditorTools/EditorLogsTool/EditorLogsTool.cpp
@@ -3,6 +3,41 @@
 using namespace u8_literals;
 using namespace Global;
 
+static std::filesystem::path GetSettingFilePath()
+{
+    constexpr const wchar_t* LOG_SETTING_FILE_NAME = L"EditorLogSetting.setting.json";
+    std::filesystem::path settingPath = PROJECT_SETTING_PATH;
+    settingPath /= LOG_SETTING_FILE_NAME;
+    return settingPath;
+}
+
+static std::string ReadSettingFile()
+{
+    std::string data;
+    std::ifstream ifs(GetSettingFilePath());
+    if (ifs.is_open() == true)
+    {
+        data = std::string(std::istreambuf_iterator<char>(ifs), {});
+    }
+    ifs.close();
+    return data;
+}
+
+static void WriteSettingFile(std::string_view data)
+{
+    auto settingPath = GetSettingFilePath();
+    if (std::filesystem::exists(settingPath) == false)
+    {
+        std::filesystem::create_directories(settingPath.parent_path());
+    }
+    std::ofstream ofs(settingPath, std::ios::trunc);
+    if (ofs.is_open() == true)
+    {
+        ofs << data;
+    }
+    ofs.close();
+}
+
 EditorLogsTool::EditorLogsTool()
 {
     SetLabel(u8"로그"_c_str);
@@ -12,12 +47,20 @@ EditorLogsTool::EditorLogsTool()
 
 EditorLogsTool::~EditorLogsTool()
 {
-
 }
 
 void EditorLogsTool::OnStartGui()
 {
-    
+    std::string setting = ReadSettingFile();
+    if (setting.empty() == false)
+    {
+        DeserializedReflectFields(setting);
+    }
+}
+
+void EditorLogsTool::OnEndGui() 
+{
+    WriteSettingFile(SerializedReflectFields());
 }
 
 void EditorLogsTool::OnTickGui() 
@@ -28,7 +71,7 @@ void EditorLogsTool::OnTickGui()
         for (size_t i = prevLogCount; i < logMessages.size(); i++)
         {
             auto& [level, message, location] = logMessages[i];
-            if (LogFilterTable[level] == true)
+            if (ReflectFields->LogFilterTable[level] == true)
             {
                 _drawLogList.emplace_back(level, message, location);   
             }            
@@ -78,7 +121,7 @@ void EditorLogsTool::OnFrame()
         const auto& logMessages = engineCore->EngineLogger.GetLogMessages();
         for (auto& [level, message, location] : logMessages)
         {
-            if (LogFilterTable[level] == true)
+            if (ReflectFields->LogFilterTable[level] == true)
             {
                 _drawLogList.emplace_back(level, message, location);
             }
@@ -108,28 +151,28 @@ void EditorLogsTool::OnFrame()
 
 void EditorLogsTool::ResetLogColor()
 {
-    LogColorTable[LogLevel::LEVEL_TRACE] = DEBUG_COLOR_TRACE;
-    LogColorTable[LogLevel::LEVEL_DEBUG] = DEBUG_COLOR_DEBUG;
-    LogColorTable[LogLevel::LEVEL_INFO] = DEBUG_COLOR_INFO;
-    LogColorTable[LogLevel::LEVEL_WARNING] = DEBUG_COLOR_WARNING;
-    LogColorTable[LogLevel::LEVEL_ERROR] = DEBUG_COLOR_ERROR;
-    LogColorTable[LogLevel::LEVEL_FATAL] = DEBUG_COLOR_FATAL;
+    ReflectFields->LogColorTable[LogLevel::LEVEL_TRACE]   = ImGuiHelper::ImVec4ToArray(DEBUG_COLOR_TRACE);
+    ReflectFields->LogColorTable[LogLevel::LEVEL_DEBUG]   = ImGuiHelper::ImVec4ToArray(DEBUG_COLOR_DEBUG);
+    ReflectFields->LogColorTable[LogLevel::LEVEL_INFO]    = ImGuiHelper::ImVec4ToArray(DEBUG_COLOR_INFO);
+    ReflectFields->LogColorTable[LogLevel::LEVEL_WARNING] = ImGuiHelper::ImVec4ToArray(DEBUG_COLOR_WARNING);
+    ReflectFields->LogColorTable[LogLevel::LEVEL_ERROR]   = ImGuiHelper::ImVec4ToArray(DEBUG_COLOR_ERROR);
+    ReflectFields->LogColorTable[LogLevel::LEVEL_FATAL]   = ImGuiHelper::ImVec4ToArray(DEBUG_COLOR_FATAL);
 }
 
 void EditorLogsTool::ResetLogFilter()
 {
-    LogFilterTable[LogLevel::LEVEL_TRACE] = true;
-    LogFilterTable[LogLevel::LEVEL_DEBUG] = true;
-    LogFilterTable[LogLevel::LEVEL_INFO] = true;
-    LogFilterTable[LogLevel::LEVEL_WARNING] = true;
-    LogFilterTable[LogLevel::LEVEL_ERROR] = true;
-    LogFilterTable[LogLevel::LEVEL_FATAL] = true;
+    ReflectFields->LogFilterTable[LogLevel::LEVEL_TRACE] = true;
+    ReflectFields->LogFilterTable[LogLevel::LEVEL_DEBUG] = true;
+    ReflectFields->LogFilterTable[LogLevel::LEVEL_INFO] = true;
+    ReflectFields->LogFilterTable[LogLevel::LEVEL_WARNING] = true;
+    ReflectFields->LogFilterTable[LogLevel::LEVEL_ERROR] = true;
+    ReflectFields->LogFilterTable[LogLevel::LEVEL_FATAL] = true;
 }
 
 void EditorLogsTool::PrintLog(const std::tuple<int, std::string, LogLocation>& log)
 {
     auto& [level, message, location] = log;
-    ImGui::PushStyleColor(ImGuiCol_Text, LogColorTable[level]);
+    ImGui::PushStyleColor(ImGuiCol_Text, ImGuiHelper::ArrayToImVec4(ReflectFields->LogColorTable[level]));
     ImGui::TextUnformatted(message.c_str());
     ImGui::Text(std::format("{}, line : {}", location.function_name(), location.line()).c_str());
     ImGui::PopStyleColor();
@@ -141,10 +184,13 @@ void EditorLogsTool::ShowFilter()
     {
         ImGui::BeginHorizontal(i);
         ImGuiHelper::PushStyleCompactToItem(0.6f);
-        ImGui::ColorEdit4("##edit", (float*)&LogColorTable[i + 1], ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoLabel);
+        ImGui::ColorEdit4("##edit", 
+                          (float*)&ReflectFields->LogColorTable[i + 1],
+                          ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoLabel);
         ImGui::Text(LogLevel::LogLevelTo_c_str(i + 1));
         ImGui::SetCursorPosX(100.0f);
-        if (ImGui::Checkbox("##checkbox", &LogFilterTable[i + 1]))
+        if (ImGui::Checkbox("##checkbox",
+                            &ReflectFields->LogFilterTable[i + 1]))
         {
             _editFilter = true;
         }
