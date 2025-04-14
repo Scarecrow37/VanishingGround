823cf8b4f1a540d34d17d98beaed127ab8b24f6e
power12562 <kimsiwoo1005@gmail.com>
2025-04-14 15:27:33 +0900

    [FEAT] EditorSceneMenu 추가
    [FEAT] EditorSceneMenu 추가

---
+++ Source/GA6thFinal_Framework/ProjectVG/Source/EditorTools/EditorMenu/EditorSceneMenu.cpp
@@ -0,0 +1,12 @@
+﻿#include "EditorSceneMenu.h"
+
+using namespace Global;
+
+void EditorSceneMenu::OnMenu() 
+{
+    const auto& objectKeys = EGameObjectFactory::Engine::GetGameObjectKeys();
+    for (auto& i : objectKeys)
+    {
+        //ImGui::MenuItem();
+    }
+}
