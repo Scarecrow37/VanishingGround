f3f7d4a75c7bebc9845e5de7af85503d0b87bd1b
power12562 <kimsiwoo1005@gmail.com>
2025-04-21 10:09:28 +0900

    [TEST/VG-28] 테스트 코드 수정

--- Source/GA6thFinal_Framework/GameEngine/Source/Editor/Tool/ScriptTest/ScriptTestEditor.cpp
+++ Source/GA6thFinal_Framework/GameEngine/Source/Editor/Tool/ScriptTest/ScriptTestEditor.cpp
@@ -14,7 +14,7 @@ ScriptTestEditor::~ScriptTestEditor()
 
 void ScriptTestEditor::OnStartGui() 
 {
-    SetLabel(u8"스크립트 만들기"_c_str);
+    SetLabel(u8"테스트 에디터"_c_str);
 }
 
 void ScriptTestEditor::OnPreFrame() 
@@ -27,43 +27,6 @@ void ScriptTestEditor::OnFrame()
     using namespace Global;
     using namespace u8_literals;
     {
-        constexpr const char* testPath = "TestObjectSerialized.yaml";
-        ImGui::TextColored({0,0,1,1}, u8"직렬화 테스트"_c_str);
-        if (ImGui::BeginDragDropTarget())
-        {
-            if (const ImGuiPayload* payLoad =
-                ImGui::AcceptDragDropPayload(DragDropTransform::KEY))
-            {
-                using Data      = DragDropTransform::Data;
-                Data*      data = (Data*)payLoad->Data;
-                YAML::Node node = UmGameObjectFactory.SerializeToYaml(
-                    &data->pTransform->gameObject);
 
-                std::ofstream ofs(testPath, std::ios::trunc);
-                if (ofs.is_open())
-                {
-                    ofs << node;
-                }
-                ofs.close();
-            }
-            ImGui::EndDragDropTarget();
-        }
-        if (ImGui::Button(u8"역직렬화 테스트"_c_str))
-        {
-            YAML::Node node =  YAML::LoadFile(testPath);
-            UmGameObjectFactory.DeserializeToYaml(&node);
-        }
-
-        ImGui::TextColored({0,0,1,1}, u8"Instantiate"_c_str);
-        if (ImGui::BeginDragDropTarget())
-        {
-            if (const ImGuiPayload* payLoad = ImGui::AcceptDragDropPayload(DragDropTransform::KEY))
-            {
-                using Data      = DragDropTransform::Data;
-                Data*      data = (Data*)payLoad->Data;
-                GameObject::Instantiate(data->pTransform->gameObject);
-            }
-            ImGui::EndDragDropTarget();
-        }
     }
 }
