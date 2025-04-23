b7c3b7403e013e527df03278824dba1928984abc
power12562 <kimsiwoo1005@gmail.com>
2025-04-23 14:58:22 +0900

    [TEST/VG-28] 테스트 코드 수정

--- Source/GA6thFinal_Framework/GameEngine/Source/Editor/Tool/ScriptTest/ScriptTestEditor.cpp
+++ Source/GA6thFinal_Framework/GameEngine/Source/Editor/Tool/ScriptTest/ScriptTestEditor.cpp
@@ -31,15 +31,31 @@ void ScriptTestEditor::OnFrame()
         for (auto& [name, scene] : sceneMap)
         {
             ImGui::PushID(&scene);
+            ImGui::Separator();
             std::string sceneName = scene.Name;
             ImGui::Text("Name : %s", sceneName.c_str());
             ImGui::Text("isLoaded : %s", scene.isLoaded ? "true" : "false");
             std::string scenePath = scene.Path;
             ImGui::Text("Path : %s", scenePath.c_str());
-            if (ImGui::Button(u8"씬 저장 테스트"_c_str))
+            if (ImGui::Button(u8"씬 저장"_c_str))
             {
                 constexpr const char* TEST_SCENE_FOLDER = "Assets/Scenes";
-                UmSceneManager.WriteSceneToFile(scene, TEST_SCENE_FOLDER);
+                UmSceneManager.WriteSceneToFile(scene, TEST_SCENE_FOLDER, true);
+            }
+            ImGui::SameLine();
+            if (ImGui::Button(u8"씬 로드"_c_str))
+            {
+                UmSceneManager.LoadScene(scenePath);
+            }
+            ImGui::SameLine();
+            if (ImGui::Button(u8"씬 추가"_c_str))
+            {
+                UmSceneManager.LoadScene(scenePath, LoadSceneMode::ADDITIVE);
+            }
+            ImGui::SameLine();
+            if (ImGui::Button(u8"씬 언로드"_c_str))
+            {
+                UmSceneManager.UnloadScene(scenePath);
             }
             ImGui::PopID();
         }
