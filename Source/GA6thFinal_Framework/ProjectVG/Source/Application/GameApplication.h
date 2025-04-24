1a8f108e44bee16b0e46e0e728181050d922bac5
power12562 <kimsiwoo1005@gmail.com>
2025-04-24 15:15:46 +0900

    [FEATURE/VG-28] OnStartupComplete() 오버라이딩

--- Source/GA6thFinal_Framework/ProjectVG/Source/Application/GameApplication.h
+++ Source/GA6thFinal_Framework/ProjectVG/Source/Application/GameApplication.h
@@ -8,4 +8,10 @@ public:
     virtual ~GameApplication() override;
 
     EditorModule* _editorModule = nullptr;
+
+protected:
+    /*모듈 초기화 끝난 후 호출되는 함수*/
+    virtual void OnStartupComplete();
+    /*모듈 초기화 해제 끝난 후 호출되는 함수*/
+    virtual void OnShutdownComplete();
 };
