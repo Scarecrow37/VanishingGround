d4cdd31b59087b59464a3802865d2868d039b9d5
SeongU <blacknaco85@gmail.com>
2025-04-25 18:59:38 +0900

    [CHORE] 쓸모없는 renderer 코드 삭제

--- Source/GA6thFinal_Framework/GameEngine/Source/Engine/GraphicsCore/Renderer.h
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/GraphicsCore/Renderer.h
@@ -27,20 +27,7 @@ public:
     void Flip();
 
 private:
-    HRESULT CreatePipelineState();
-
-    // 임시
-    void UpdateFrameResource();
-
-private:
     std::vector<MeshRenderer*>     _components;
-    std::unique_ptr<Shader>        _shader;
-    ComPtr<ID3D12PipelineState>    _pipelineState[2];
-    std::unique_ptr<FrameResource> _frameResource;
     std::unordered_map<std::string, std::shared_ptr<RenderScene>> _renderScenes;
-    ComPtr<ID3D12Resource>         _cameraBuffer;
     UINT                           _currnetState;
-
-    ComPtr<ID3D12CommandAllocator> _commandAllocator;
-    ComPtr<ID3D12GraphicsCommandList> _commandList;
 };
