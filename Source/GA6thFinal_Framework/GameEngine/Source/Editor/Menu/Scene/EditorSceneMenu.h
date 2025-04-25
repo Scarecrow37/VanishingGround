7a2f467581f28eaeb418e851edf6c80c6b3d97fb
power12562 <kimsiwoo1005@gmail.com>
2025-04-25 23:43:56 +0900

    [CHORE/VG-28] 게임 오브젝트 생성 메뉴 버튼 재사용 가능하도록 함수 분리

--- Source/GA6thFinal_Framework/GameEngine/Source/Editor/Menu/Scene/EditorSceneMenu.h
+++ Source/GA6thFinal_Framework/GameEngine/Source/Editor/Menu/Scene/EditorSceneMenu.h
@@ -1,23 +1,5 @@
 ﻿#pragma once
 
-/*
- 게임 오브젝트 생성 메뉴
-*/
-class EditorSceneMenuGameObject : public EditorMenu
-{
-public:
-    EditorSceneMenuGameObject()
-    {
-        SetCallOrder(1);
-        SetPath("Scene");
-        SetLabel("Scene");
-    }
-    virtual ~EditorSceneMenuGameObject() = default;
-
-public:
-    virtual void OnMenu() override;
-};
-
 class EditorSceneMenuScenes : public EditorMenu
 {
 public:
@@ -28,6 +10,27 @@ public:
         SetLabel("Scene");
     }
     virtual ~EditorSceneMenuScenes() = default;
+
+public:
+    virtual void OnMenu() override;
+};
+
+/*
+ 게임 오브젝트 생성 메뉴
+*/
+class EditorSceneMenuGameObject : public EditorMenu
+{
+public:
+    static void ImGuiNewGameObjectMenuItems();
+
+public:
+    EditorSceneMenuGameObject()
+    {
+        SetCallOrder(1);
+        SetPath("Scene");
+        SetLabel("Scene");
+    }
+    virtual ~EditorSceneMenuGameObject() = default;
 
 public:
     virtual void OnMenu() override;
