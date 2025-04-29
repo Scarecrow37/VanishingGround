407584ba04d2f584c173d3aed7ca10515c149090
power12562 <kimsiwoo1005@gmail.com>
2025-04-30 00:10:55 +0900

    [FEATURE/VG-28] yyjsonValToString 함수 추가

--- Source/GA6thFinal_Framework/GameEngine/Source/Engine/ClassCore/ReflectHelper.cpp
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/ClassCore/ReflectHelper.cpp
@@ -7,3 +7,25 @@ void ReflectHelper::ImGuiDraw::Private::EngineLog(int logLevel, std::string_view
 {
     Global::engineCore->Logger.Log(logLevel, message, location);
 }
+
+std::string ReflectHelper::json::yyjsonValToString(yyjson_val* val)
+{
+    if (val == nullptr)
+        return "";
+
+    yyjson_mut_doc* doc     = yyjson_mut_doc_new(NULL);
+    yyjson_mut_val* mut_val = yyjson_val_mut_copy(doc, val);
+    yyjson_mut_doc_set_root(doc, mut_val);
+
+    size_t len;
+    char*  json = yyjson_mut_write(doc, NULL, &len);
+
+    std::string result;
+    if (json != nullptr)
+    {
+        result = std::string(json, len);
+    }
+    free(json);
+    yyjson_mut_doc_free(doc);
+    return result;
+}
