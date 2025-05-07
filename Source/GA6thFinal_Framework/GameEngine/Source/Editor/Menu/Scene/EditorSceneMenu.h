f30f4297a08b2c4e0480d5b868fbd1ddec46d911
power12562 <kimsiwoo1005@gmail.com>
2025-05-07 18:08:20 +0900

    [CHORE/VG-28] 불필요한 메뉴 삭제

--- Source/GA6thFinal_Framework/GameEngine/Source/Editor/Menu/Scene/EditorSceneMenu.h
+++ Source/GA6thFinal_Framework/GameEngine/Source/Editor/Menu/Scene/EditorSceneMenu.h
@@ -14,24 +14,3 @@ public:
 public:
     virtual void OnMenu() override;
 };
-
-/*
- 게임 오브젝트 생성 메뉴
-*/
-class EditorSceneMenuGameObject : public EditorMenu
-{
-public:
-    static void ImGuiNewGameObjectMenuItems();
-
-public:
-    EditorSceneMenuGameObject()
-    {
-        SetCallOrder(1);
-        SetPath("Scene");
-        SetLabel("Scene");
-    }
-    virtual ~EditorSceneMenuGameObject() = default;
-
-public:
-    virtual void OnMenu() override;
-};
