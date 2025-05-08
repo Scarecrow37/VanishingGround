689b8cefcf30a628ef2f0cbe772acb56c0d7e663
power12562 <kimsiwoo1005@gmail.com>
2025-05-08 13:57:06 +0900

    [FEATURE/VG-28] PackPrefabCommand 추가

---
+++ Source/GA6thFinal_Framework/GameEngine/Source/Editor/Tool/Hierarchy/Command/PackPrefabCommand.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "UmFramework.h"
+
+namespace Command
+{
+    namespace Hierarchy
+    {
+        class PackPrefabCommand : public UmCommand
+        {
+        public:
+            PackPrefabCommand(std::weak_ptr<GameObject> targetObject, const File::Guid& guid);
+            virtual ~PackPrefabCommand() = default;
+
+        private:
+            std::weak_ptr<GameObject> _targetObject;
+            File::Guid _currGuid;
+            File::Guid _prevGuid;
+
+            // UmCommand을(를) 통해 상속됨
+            void Execute() override;
+            void Undo() override;
+        };
+    } // namespace Hierarchy
+} // namespace Command
