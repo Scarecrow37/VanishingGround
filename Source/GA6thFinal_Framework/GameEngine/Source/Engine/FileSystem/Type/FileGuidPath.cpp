d76f27749e72a3fc6de73796e6923f4464592c63
PPAK_JU <taku7664@gmail.com>
2025-05-08 14:40:04 +0900

    [FIX] 파일 시스템 이벤트 호출 로직 수정

---
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/FileSystem/Type/FileGuidPath.cpp
@@ -0,0 +1,42 @@
+﻿#include "pch.h"
+#include "FileGuidPath.h"
+
+namespace File
+{
+    Guid::operator File::Path() const
+    {
+        return ToPath();
+    }
+    File::Path Guid::ToPath() const
+    {
+        return UmFileSystem.GetPathFromGuid(native());
+    }
+    bool Guid::IsNull() const
+    {
+        return NULL_GUID == native();
+    }
+    Path::operator File::Guid() const
+    {
+        return ToGuid();
+    }
+    File::Path Path::operator+(const File::FString& v)
+    {
+        return string() + v.string();
+    }
+    File::Path Path::operator/(const File::FString& v)
+    {
+        File::Path path = string();
+        path /= v;
+        return path.generic_string();
+    }
+    File::Guid Path::ToGuid() const
+    {
+        return UmFileSystem.GetGuidFromPath(native());
+    }
+    bool Path::IsNull() const
+    {
+        return NULL_PATH == native();
+    }
+    
+}
+
