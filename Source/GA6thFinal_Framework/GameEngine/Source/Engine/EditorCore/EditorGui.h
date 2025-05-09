35064f43aeea1d83b6c6f3c96ccb93d3c1e7e303
power12562 <kimsiwoo1005@gmail.com>
2025-05-09 11:33:31 +0900

    [CHORE/VG-28] IEditorObject 파일 분리

--- Source/GA6thFinal_Framework/GameEngine/Source/Engine/EditorCore/EditorGui.h
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/EditorCore/EditorGui.h
@@ -1,23 +1,6 @@
 ﻿#pragma once
 
 /*
-외부에서 에디터 툴에 나타내기 위한 인터페이스 객체
-GameObject, Asset 등은 이를 상속해 InspectorView 등에 그려질 내용을 구현한다.
-*/ 
-class IEditorObject
-{
-public:
-    IEditorObject() = default;
-    virtual ~IEditorObject() = default;
-public:
-    virtual void OnInspectorViewEnter() {};
-
-    virtual void OnInspectorStay() {};
-
-    virtual void OnInspectorExit() {};
-};
-
-/*
 에디터에 출력되는 창(프레임) 단위의 객체에 상속하기 위한 객체
 */
 class EditorGui : public ReflectSerializer
