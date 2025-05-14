df1e5d98a05f6961ebe02f62df148f6d75603958
power12562 <kimsiwoo1005@gmail.com>
2025-05-14 10:28:07 +0900

    [FEATURE/VG-28] MeshRender용 MeshComponent 추상 클래스 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/Graphics/MeshComponent.cpp
@@ -0,0 +1,21 @@
+﻿#include "MeshComponent.h"
+
+MeshComponent::MeshComponent() 
+    : 
+    _meshRenderer(_pMeshRenderer)
+{
+  
+}
+MeshComponent::~MeshComponent() = default;
+
+void MeshComponent::MakeMeshRenderer(MeshRenderer::RENDER_TYPE renderType, const Matrix& world)
+{
+    if (nullptr == _pMeshRenderer)
+    {
+        _pMeshRenderer.reset(new MeshRenderer(renderType, world));
+    } 
+    else
+    {
+        assert(!"이미 MeshRenderer가 존재합니다.");
+    }
+}
