89b9570e3e9f23e6013d228d28a7179d353a4e48
power12562 <kimsiwoo1005@gmail.com>
2025-05-15 12:36:45 +0900

    [FEATURE/VG-28] InputAuto용 커맨드 클래스 추가

---
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/ClassCore/Command/InputAutoCommand.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include "UmFramework.h"
+
+namespace Command
+{
+    namespace InputAuto
+    {
+        template <const char* COMMAND_NAME, typename DATA, typename PDATA>
+        class InputAutoCommand : public UmCommand
+        {
+        public:
+            InputAutoCommand(const DATA& prev, const DATA& curr, PDATA* pData) 
+                :
+                UmCommand(COMMAND_NAME),
+                _prev(prev),
+                _curr(curr),
+                _pData(pData)
+            {
+
+            }
+            virtual ~InputAutoCommand()
+            {
+
+            }
+
+        private:
+            DATA _prev;
+            DATA _curr;
+            PDATA* _pData;
+            virtual void Execute()
+            {
+                *_pData = _curr;
+            }
+
+            virtual void Undo()
+            {
+                 *_pData = _prev;
+            }
+        };
+
+    } // namespace InputAuto
+}
