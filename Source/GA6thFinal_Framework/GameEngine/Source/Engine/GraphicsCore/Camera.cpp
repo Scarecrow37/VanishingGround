de6a61984a89f60e523e10859525daf72fc6589f
power12562 <kimsiwoo1005@gmail.com>
2025-05-16 15:58:07 +0900

    [FIX/VG-28] Aspect 버그 수정

--- Source/GA6thFinal_Framework/GameEngine/Source/Engine/GraphicsCore/Camera.cpp
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/GraphicsCore/Camera.cpp
@@ -3,7 +3,10 @@
 
 void Camera::SetupPerspective(float fovDegree, float aspect, float nearZ, float farZ)
 {
-    aspect = std::max(aspect, Mathf::Epsilon);
+    if (std::isnan(aspect))
+    {
+        aspect = Mathf::Epsilon;
+    }
     _projection        = XMMatrixPerspectiveFovLH(XMConvertToRadians(fovDegree), aspect, nearZ, farZ);
     _projectionInverse = XMMatrixInverse(nullptr, _projection);
 }
