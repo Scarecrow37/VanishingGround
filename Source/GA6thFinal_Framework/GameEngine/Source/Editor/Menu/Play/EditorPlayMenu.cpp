754cbc7169c599b95367b87649ba72c4b45dfd87
power12562 <kimsiwoo1005@gmail.com>
2025-05-19 10:31:32 +0900

    [CHORE/VG-28] Play, Stop 버튼 더 잘보이도록 수정

--- Source/GA6thFinal_Framework/GameEngine/Source/Editor/Menu/Play/EditorPlayMenu.cpp
+++ Source/GA6thFinal_Framework/GameEngine/Source/Editor/Menu/Play/EditorPlayMenu.cpp
@@ -4,20 +4,14 @@ using namespace Global;
 
 void EditorPlayMenu::OnMenu() 
 {
-    if (ImGui::BeginMenu("Play"))
+    static const std::string play = std::format("{} Play", EditorIcon::ICON_PLAY);
+    if (ImGui::Button(play.c_str()))
     {
-        static const std::string play = std::format("{} Play", EditorIcon::ICON_PLAY);
-        if (ImGui::MenuItem(play.c_str()))
-        {
-            editorModule->PlayMode.Play();
-        }
-
-        static const std::string stop = std::format("{} Stop", EditorIcon::ICON_STOP);
-        if (ImGui::MenuItem(stop.c_str()))
-        {
-            editorModule->PlayMode.Stop();
-        }
-        ImGui::EndMenu();
+        editorModule->PlayMode.Play();
     }
-    
+    static const std::string stop = std::format("{} Stop", EditorIcon::ICON_STOP);
+    if (ImGui::Button(stop.c_str()))
+    {
+        editorModule->PlayMode.Stop();
+    }
 }
