5843af77b65b0453f1d8865aa808a2e43b4d5e21
power12562 <kimsiwoo1005@gmail.com>
2025-05-20 17:31:51 +0900

    [CHORE/VG-28] 주석 수정

--- Source/GA6thFinal_Framework/GameEngine/Source/Engine/EngineCore/ComponentFactory.cpp
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/EngineCore/ComponentFactory.cpp
@@ -165,8 +165,8 @@ bool EComponentFactory::InitalizeComponentFactory()
             newComponent = std::move(missing);
         }
         ResetComponent(gameObject, newComponent);       // 엔진에서 사용하기 위해 초기화
-        newComponent->_initFlags.SetAwake();                  // 초기화 플래그 설정
-        newComponent->_initFlags.SetStart();                  // 초기화 플래그 설정
+        newComponent->_initFlags.SetAwake();            // 초기화 플래그 설정
+        newComponent->_initFlags.SetStart();            // 초기화 플래그 설정
         gameObject->_components[index] = newComponent;  
         if (isFind == true)
         {
