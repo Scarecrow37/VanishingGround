3af56483f8a1ce87a2885778fb6b4e367e249380
power12562 <kimsiwoo1005@gmail.com>
2025-06-17 12:10:32 +0900

    [CHORE/VG-28] 클래스 이름 변경으로 인한 필터 수정

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/Test/Condition/TestMoveCondition.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "GameCore/FSM/FiniteStateMachine.h"
+
+class TestMoveCodition : public FSMCondition
+{
+public:
+    TestMoveCodition();
+    virtual ~TestMoveCodition() override;
+
+    void OnAwake() override;
+    void OnStart() override;
+
+    bool Evaluate() override;
+
+private:
+    class TestComponent* testComponent;
+};
