a7991fcaacf97f173ea5d85c93107f0ea512103f
power12562 <kimsiwoo1005@gmail.com>
2025-06-17 17:18:53 +0900

    [CHORE/VG-28] Turn 모드용 함수 추가

--- Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnMode/TurnMode.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnMode/TurnMode.cpp
@@ -1,5 +1,35 @@
 ﻿#include "TurnMode.h"
-
+#include "GameCore/FSM/FiniteStateMachine.h"
 
 TurnMode::TurnMode() = default;
 TurnMode::~TurnMode() = default;
+
+void TurnMode::BuildTurnModeFSM() 
+{
+    _finiteStateMachine = GetComponent<FiniteStateMachine>();
+    if (nullptr == _finiteStateMachine)
+    {
+        _finiteStateMachine = &AddComponent<FiniteStateMachine>();
+
+    }
+}
+
+void TurnMode::Reset() 
+{
+   
+}
+
+void TurnMode::Awake() 
+{
+    BuildTurnModeFSM();
+}
+
+void TurnMode::Start() 
+{
+
+}
+
+void TurnMode::Update() 
+{
+
+}
