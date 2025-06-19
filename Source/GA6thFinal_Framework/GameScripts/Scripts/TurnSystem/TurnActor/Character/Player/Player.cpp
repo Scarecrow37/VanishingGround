45ebcd2776ed2aaf5914913228ccd633f788836f
power12562 <kimsiwoo1005@gmail.com>
2025-06-19 16:57:01 +0900

    [FEATURE/VG-28] Player 클래스 추가
    CharacterBase를 상속받는 클래스

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnActor/Character/Player/Player.cpp
@@ -0,0 +1,19 @@
+﻿#include "Player.h"
+Player::Player()
+{
+    MaxHP      = 100;
+    MaxMP      = 100;
+    ChainCount = 0;
+}
+Player::~Player() = default;
+
+void Player::Awake() 
+{
+    Base::Awake();
+    gameObject->AddTag(TAG);
+}
+
+int Player::GetSpeed()
+{
+    return 0;
+}
