a6145239aea777ffd249d383bab6c042be790e62
power12562 <kimsiwoo1005@gmail.com>
2025-06-19 10:46:03 +0900

    [FIX/VG-28] Random 클래스를 static 클래스로 수정

--- Source/GA6thFinal_Framework/GameEngine/Source/Engine/Utility/Random.h
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/Utility/Random.h
@@ -4,19 +4,10 @@ class Random
 {
 public:
     /// <summary>
-    /// 무작위 난수 시드를 생성합니다.
-    /// </summary>
-    Random()
-    {
-        std::random_device rd;
-        engine.seed(rd());
-    }
-
-    /// <summary>
     /// 시드를 고정하고 싶을때 사용합니다.
     /// </summary>
-    /// <param name="seed"></param>
-    explicit Random(unsigned int seed) { engine.seed(seed); }
+    /// <param name="seed :">사용할 시드</param>
+    static void SetSeed(unsigned int seed);
 
     /// <summary>
     /// 무작위 int 난수를 생성합니다.
@@ -24,7 +15,7 @@ public:
     /// <param name="min :">최소값 이상</param>
     /// <param name="max :">최대값 이하</param>
     /// <returns>결과</returns>
-    int Range(int min, int max)
+    static int Range(int min, int max)
     {
         std::uniform_int_distribution<int> dist(min, max);
         return dist(engine);
@@ -36,12 +27,12 @@ public:
     /// <param name="min :">최소값 이상</param>
     /// <param name="max :">최대값 미만</param>
     /// <returns></returns>
-    float Range(float min, float max)
+    static float Range(float min, float max)
     {
         std::uniform_real_distribution<float> dist(min, max);
         return dist(engine);
     }
 
 private:
-    std::mt19937 engine;
+    static std::mt19937 engine;
 };
