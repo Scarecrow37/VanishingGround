21b9e1e347a501f3ca7863918930d276f8723b7a
power12562 <kimsiwoo1005@gmail.com>
2025-06-19 17:00:12 +0900

    [FEATURE/VG-28] Order용 프로퍼티 추가

--- Source/GA6thFinal_Framework/GameScripts/Scripts/GameCore/FSM/FSMCondition.h
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/GameCore/FSM/FSMCondition.h
@@ -3,6 +3,7 @@
 
 class FSMCondition : public IFSMEntity
 {
+    USING_PROPERTY(FSMCondition)
 public:
     FSMCondition() = default;
     virtual ~FSMCondition() override = default;
@@ -12,6 +13,10 @@ public:
     /// </summary>
     /// <returns></returns>
     virtual bool Evaluate() = 0;
+
+    GETTER(int, Order) { return ReflectFields->Order; }
+    SETTER(int, Order) { ReflectFields->Order = ReflectFields->Order; }
+    PROPERTY(Order)
 
     REFLECT_FIELDS_BEGIN(IFSMEntity)
     //전이 조건의 우선순위를 결정합니다. 숫자가 낮을수록 우선순위가 높습니다.
