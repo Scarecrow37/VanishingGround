d2d9550023cb001cc6b6ddf9b86cf878bd8aa37c
power12562 <kimsiwoo1005@gmail.com>
2025-06-19 16:57:36 +0900

    [FEATURE/VG-28] Enemy 클래스 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnActor/Character/Enemy/Enemy.cpp
@@ -0,0 +1,22 @@
+﻿#include "Enemy.h"
+
+Enemy::Enemy()
+{
+    MaxHP      = 100;
+    MaxMP      = 100;
+    ChainCount = 0;
+    Speed      = 0;
+}
+
+Enemy::~Enemy() = default;
+
+void Enemy::Awake() 
+{
+    Base::Awake();
+    gameObject->AddTag(TAG);
+}
+
+int Enemy::GetSpeed()
+{
+    return Speed;
+}
