3cb88dba60d043cf72f803a339920a5667f38b23
power12562 <kimsiwoo1005@gmail.com>
2025-06-19 16:58:43 +0900

    [FIX/VG-28] TurnMode의 기능 수정
    - MakeTurnList() 함수 추가
    - SortTurnList() 함수 추가
    - 디버깅용 노드 가독성 개선

--- Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnMode/TurnMode.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnMode/TurnMode.cpp
@@ -3,15 +3,88 @@
 #include "TurnSystem/TurnActor/TurnActor.h"
 
 //Condition
-#include "TurnSystem/Condition/BattleStartCodition.h"
+#include "TurnSystem/Condition/CombatStartCodition.h"
 #include "TurnSystem/Condition/RoundStartCondition.h"
 
 //State
-#include "TurnSystem/State/BattleStartPhase.h"
+#include "TurnSystem/State/CombatStartPhase.h"
 #include "TurnSystem/State/RoundStartPhase.h"   
 
-TurnMode::TurnMode() = default;
+//Character
+#include "TurnSystem/TurnActor/Character/Player/Player.h"
+#include "TurnSystem/TurnActor/Character/Enemy/Enemy.h"
+
+TurnMode::TurnMode() 
+    : 
+    _roundCount(0) 
+{
+
+}
 TurnMode::~TurnMode() = default;
+
+void TurnMode::MakeTurnList() 
+{
+    auto players = GameObject::FindGameObjectsWithTag(Player::TAG);
+    for (auto& weak : players)
+    {
+        if (false == weak.expired())
+        {
+            auto object = weak.lock();
+            Player* player = object->GetComponent<Player>();
+            if (nullptr != player)
+            {
+                _turnList.push_back(player);
+            }
+        }
+    }
+
+    auto enemies = GameObject::FindGameObjectsWithTag(Enemy::TAG);
+    for (auto& weak : enemies)
+    {
+        if (false == weak.expired())
+        {
+            auto object = weak.lock();
+            Enemy* enemy = object->GetComponent<Enemy>();
+            if (nullptr != enemy)
+            {
+                _turnList.push_back(enemy);
+            }
+        }
+    }
+}
+
+void TurnMode::SortTurnList()
+{
+    for (auto& actor : _turnList)
+    {
+        actor->SetRandomSpeed(Random::Range(0, 6));
+    }
+    std::sort(_turnList.begin(), _turnList.end(), [](TurnActor* actorA, TurnActor* actorB) 
+    {
+        int speedA = actorA->RoundSpeed;
+        int speedB = actorB->RoundSpeed;
+        if (speedA != speedB)
+        {
+            return speedA > speedB;
+        }
+        else
+        {
+            // Coin toss: 50% 확률로 actorA가 우선
+            return Random::Range(0, 1) == 1;
+        }      
+    });
+}
+
+TurnActor* TurnMode::PopTurnList()
+{
+    TurnActor* turnMode = nullptr;
+    if (false == _turnList.empty())
+    {
+        turnMode = _turnList.front();
+        _turnList.pop_front();
+    }
+    return turnMode;
+}
 
 void TurnMode::BuildTurnModeFSM() 
 {
@@ -21,25 +94,20 @@ void TurnMode::BuildTurnModeFSM()
         _finiteStateMachine = &AddComponent<FiniteStateMachine>();
 
         //State
-        _finiteStateMachine->AddState<BattleStartPhase>();
+        _finiteStateMachine->AddState<CombatStartPhase>();
         _finiteStateMachine->AddState<RoundStartPhase>();
 
         //Condition
-        _finiteStateMachine->AddCondition<BattleStartCodition>();
+        _finiteStateMachine->AddCondition<CombatStartCodition>();
         _finiteStateMachine->AddCondition<RoundStartCondition>();
 
         //Entry
-        _finiteStateMachine->SetEntryState<BattleStartPhase>();
+        _finiteStateMachine->SetEntryState<CombatStartPhase>();
 
         //Transition    
-        _finiteStateMachine->AddTransition<BattleStartPhase, BattleStartCodition, RoundStartPhase>();
+        _finiteStateMachine->AddTransition<CombatStartPhase, RoundStartCondition, RoundStartPhase>();
         
     }
-}
-
-void TurnMode::Reset() 
-{
-   
 }
 
 void TurnMode::Awake() 
@@ -47,38 +115,34 @@ void TurnMode::Awake()
     BuildTurnModeFSM();
 }
 
-void TurnMode::Start() 
-{
-    auto actors = GameObject::FindGameObjectsWithTag(TurnActor::TAG);
-    for (auto& weak : actors)
-    {
-        if (false == weak.expired())
-        {
-            auto actor = weak.lock();
-            TurnActor* turnActor = actor->GetComponent<TurnActor>();
-            if (nullptr != turnActor)
-            {
-                _trunList.push_back(turnActor);
-            }
-        }
-    }
-}
-
-void TurnMode::Update() 
-{
-
-}
-
 void TurnMode::ImGuiDrawPropertysEvent() 
 {
     if (ImGui::TreeNodeEx("TurnList", ImGuiTreeNodeFlags_DefaultOpen))
     {
-        for (auto& actor : _trunList)
+        if (ImGui::BeginTable("Transition", 3, ImGuiTableFlags_Borders | ImGuiTableFlags_RowBg))
         {
-            if (ImGui::Selectable(actor->gameObject->ToString().data()))
+            ImGui::TableSetupColumn("Name");
+            ImGui::TableSetupColumn("Type");
+            ImGui::TableSetupColumn("Round Speed");
+            ImGui::TableHeadersRow();
+            for (auto& actor : _turnList)
             {
-
+                ImGui::PushID(actor);
+                {
+                    ImGui::TableNextRow();
+                    ImGui::TableSetColumnIndex(0);
+                    if (ImGui::Selectable(actor->gameObject->ToString().data()))
+                    {
+                    }
+                    ImGui::TableSetColumnIndex(1);
+                    ImGui::Selectable(typeid(*actor).name() + 6);
+                    ImGui::TableSetColumnIndex(2);
+                    int roundSpeed = actor->RoundSpeed;
+                    ImGui::Text("%d", roundSpeed);
+                }
+                ImGui::PopID();
             }
+            ImGui::EndTable();
         }
         ImGui::TreePop();
     }
