7878d81c44ce758f2e42303f21bb2c7bf5bf1a39
power12562 <kimsiwoo1005@gmail.com>
2025-06-20 14:53:08 +0900

    [CHORE/VG-28] CheckPlayerCondition 클래스 이름 변경

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnMode/Condition/CheckRoundStartEnd.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "Base/GetTurnModeBaseC.h"
+
+/*
+* 라운드 시작 연출이 종료됬는지 확인합니다.
+*/
+class CheckRoundStartEnd : public GetTurnModeBaseC
+{
+public:
+    CheckRoundStartEnd();
+    virtual ~CheckRoundStartEnd();
+
+    void OnAwake() override;
+    void OnStart() override;
+    bool Evaluate() override;
+};
