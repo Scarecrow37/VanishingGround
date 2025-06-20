362bc67bae5ca8d254cc08eeff511c77a5c90ac1
Do_Suha <weqa123@naver.com>
2025-06-20 14:38:57 +0900

    컨트롤러 클래스 및 관련 구조체 추가
    `Controller.h` 파일에 `Input` 네임스페이스 내에 `Adapter` 클래스와 `Controller` 클래스를 추가했습니다.
    `Controller` 클래스는 버튼 타입을 정의하는 `Button` 타입과, 컨트롤러의 상태를 나타내는 `State` 구조체를 포함합니다.
    `State` 구조체는 엄지 막대의 위치를 나타내는 `ThumbStickAxis`, Trigger의 누름 정도를 나타내는 `TriggerValue`, 버튼 상태를 나타내는 `Buttons`를 포함합니다.
    또한, `Controller` 클래스는 생성자와 두 개의 private 멤버 변수 `_adapter`와 `_id`를 추가했습니다.
    주석은 한국어로 작성되어 각 구성 요소의 역할을 설명하고 있습니다.

--- Source/GA6thFinal_Framework/GameEngine/Source/Engine/InputCore/Devices/Controller/Controller.h
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/InputCore/Devices/Controller/Controller.h
@@ -2,8 +2,79 @@
 
 namespace Input
 {
+    class Adapter;
+
     class Controller
     {
+        /// <summary>
+        /// 컨트로러 버튼의 타입에 대한 비트을 정의합니다.
+        /// </summary>
+        using Button                    = unsigned short;
+        constexpr Button DPAD_UP        = 0x0001;
+        constexpr Button DPAD_DOWN      = 0x0002;
+        constexpr Button DPAD_LEFT      = 0x0004;
+        constexpr Button DPAD_RIGHT     = 0x0008;
+        constexpr Button START          = 0x0010;
+        constexpr Button BACK           = 0x0020;
+        constexpr Button LEFT_THUMB     = 0x0040;
+        constexpr Button RIGHT_THUMB    = 0x0080;
+        constexpr Button LEFT_SHOULDER  = 0x0100;
+        constexpr Button RIGHT_SHOULDER = 0x0200;
+        constexpr Button A              = 0x1000;
+        constexpr Button B              = 0x2000;
+        constexpr Button X              = 0x4000;
+        constexpr Button Y              = 0x8000;
 
+        /// <summary>
+        /// 컨트롤러의 식별자입니다.
+        /// </summary>
+        using ID = unsigned char;
+
+        /// <summary>
+        /// 엄지 막대의 위치에 대한 값입니다.
+        /// X축, Y축 모두 값의 범위는 -1.0 ~ 1.0 사이입니다.
+        /// magnitude는 벡터의 크기를 나타내며, 0.0 ~ 1.0 사이의 값을 가집니다.
+        /// </summary>
+        struct ThumbStickAxis
+        {
+            float x;
+            float y;
+            float magnitude;
+        };
+
+        /// <summary>
+        /// 컨트롤러의 Trigger의 누름 정도에 대한 값입니다.
+        /// 값의 범위는 0.0 ~ 1.0 사이입니다.
+        /// </summary>
+        using TriggerValue = float;
+
+        /// <summary>
+        /// 컨트롤러의 각 버튼의 누름에 대한 신호 값입니다.
+        /// 값의 범위는 <see cref="ControllerButton"/>를 참고하세요.
+        /// </summary>
+        using Buttons = Button;
+
+        /// <summary>
+        /// 컨트롤러의 현재 상태를 나타내는 구조체입니다.
+        /// </summary>
+        struct State
+        {
+            using Generation = DWORD;
+
+            Generation     Generation;
+            ThumbStickAxis LeftThumbStickAxis;
+            ThumbStickAxis RightThumbStickAxis;
+            TriggerValue   LeftTrigger;
+            TriggerValue   RightTrigger;
+            Buttons        Buttons;
+        };
+
+    public:
+        Controller(const Adapter* adapter);
+
+    private:
+        const Adapter* _adapter;
+
+        ID _id;
     };
-}
+} // namespace Input
