df452c68d26b585b168d6b51bce16d375cd2cb6a
power12562 <kimsiwoo1005@gmail.com>
2025-06-20 14:36:12 +0900

    [FEATURE/VG-28] RoundEndPhase 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnMode/State/RoundEndPhase.cpp
@@ -0,0 +1,23 @@
+﻿#include "RoundEndPhase.h"
+
+REGISTER_CLASS(FSMStateFactory, RoundEndPhase)
+
+RoundEndPhase::RoundEndPhase() {}
+
+RoundEndPhase::~RoundEndPhase() {}
+
+void RoundEndPhase::OnAwake() {}
+
+void RoundEndPhase::OnStart() 
+{
+    GetTurnModeBaseS::OnStart();
+}
+
+void RoundEndPhase::OnEnter() 
+{
+    
+}
+
+void RoundEndPhase::OnExit() {}
+
+void RoundEndPhase::OnUpdate() {}
---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnMode/State/RoundEndPhase.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "Base/GetTurnModeBaseS.h"
+
+/*
+* 라운드 종료 페이즈용 State 입니다.
+* 라운드 종료 연출을 실행해야합니다.
+*/
+class RoundEndPhase : public GetTurnModeBaseS
+{
+public:
+    RoundEndPhase();
+    virtual ~RoundEndPhase() override;
+
+protected:
+    void OnAwake() override;
+    void OnStart() override;
+    void OnEnter() override;
+    void OnExit() override;
+    void OnUpdate() override;
+};
