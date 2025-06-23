21a3028bc430a00cc70a18754689ae62f6fef195
power12562 <kimsiwoo1005@gmail.com>
2025-06-23 15:53:03 +0900

    [FIX/VG-28] 게임 씬에도 메시 추가하도록 변경

--- Source/GA6thFinal_Framework/GameScripts/Scripts/Mesh/MeshComponent.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/Mesh/MeshComponent.cpp
@@ -21,6 +21,7 @@ void MeshComponent::MakeMeshRenderer(MeshRenderType renderType, const Matrix& wo
     {
         _pMeshRenderer.reset(new MeshRenderer(renderType, world));
         _pMeshRenderer->RegisterRenderQueue("Editor");
+        _pMeshRenderer->RegisterRenderQueue("Game");
     } 
     else
     {
