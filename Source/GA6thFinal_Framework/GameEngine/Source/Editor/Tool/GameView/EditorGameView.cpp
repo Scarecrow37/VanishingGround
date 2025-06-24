0021ebc4cc922994a823e20cf9cacee9f31fdf0c
power12562 <kimsiwoo1005@gmail.com>
2025-06-24 12:41:59 +0900

    [FIX/VG-28] GameView 화면 출력 크기 계산 버그 수정

--- Source/GA6thFinal_Framework/GameEngine/Source/Editor/Tool/GameView/EditorGameView.cpp
+++ Source/GA6thFinal_Framework/GameEngine/Source/Editor/Tool/GameView/EditorGameView.cpp
@@ -17,9 +17,25 @@ void EditorGameView::OnFrameRender()
         auto gpuHandle = UmRenderer.GetRenderSceneImage("Game");
         ImVec2 contentMin = ImGui::GetWindowContentRegionMin();
         ImVec2 contentMax = ImGui::GetWindowContentRegionMax();
+
+        ImVec2 fullSize;
+        fullSize.x = contentMax.x - contentMin.x;
+        fullSize.y = contentMax.y - contentMin.y;
+
         ImVec2 size;
-        size.x = contentMax.x - contentMin.x;
-        size.y = size.x / (float)camera->Aspect;
+        float aspect = camera->Aspect;
+        if (fullSize.x / fullSize.y > aspect)
+        {
+            // 세로가 제한 요소 → 세로 기준 최대 크기, 가로는 비율로 맞춤
+            size.y = std::round(fullSize.y);
+            size.x = std::round(size.y * aspect);
+        }
+        else
+        {
+            // 가로가 제한 요소 → 가로 기준 최대 크기, 세로는 비율로 맞춤
+            size.x = std::round(fullSize.x);
+            size.y = std::round(size.x / aspect);
+        }
         ImGui::Image((ImTextureID)gpuHandle.ptr, size);
     }
 }
