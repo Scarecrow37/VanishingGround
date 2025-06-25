425d005c254139e12348639f79e3834f20289c72
power12562 <kimsiwoo1005@gmail.com>
2025-06-25 16:17:44 +0900

    [FEATURE/VG-28] Enemy FSM을 위한 Base 클래스 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnActor/Character/Enemy/Condition/Base/EnemyConditionBase.cpp
@@ -0,0 +1,22 @@
+﻿#include "pchScripts.h"
+#include "EnemyConditionBase.h"
+#include <TurnSystem/TurnActor/Character/Enemy/Enemy.h>
+
+EnemyConditionBase::EnemyConditionBase() 
+{
+
+}
+
+EnemyConditionBase::~EnemyConditionBase() 
+{
+
+}
+
+Enemy& EnemyConditionBase::GetEnemy()
+{
+    if (nullptr == _enemy)
+    {
+        _enemy = GetFSM().GetComponent<Enemy>();
+    }
+    return *_enemy;
+}
---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnActor/Character/Enemy/State/Base/EnemyStateBase.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <GameCore/FSM/FiniteStateMachine.h>
+
+class Enemy;
+class EnemyStateBase : public FSMState
+{
+public:
+    EnemyStateBase();
+    virtual ~EnemyStateBase() override;
+
+protected:
+    Enemy& GetEnemy();
+
+private:
+    Enemy* _enemy = nullptr;
+};
