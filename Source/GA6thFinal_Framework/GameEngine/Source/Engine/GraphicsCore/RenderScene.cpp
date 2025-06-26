36b76fe4d116a0067670728e98b6dac4f517e009
bmbany <bmbany@naver.com>
2025-06-26 14:21:36 +0900

    [FIX] GraphicsBase의 isActive가  정상작동 안하는 버그 수정 및 빈 model 예외처리 추가

--- Source/GA6thFinal_Framework/GameEngine/Source/Engine/GraphicsCore/RenderScene.cpp
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/GraphicsCore/RenderScene.cpp
@@ -92,7 +92,7 @@ void RenderScene::UpdateRenderScene()
     _numLight = {};
     for (auto& [isDestroy, light] : lights)
     {
-        if (!light->_isActive)
+        if (!(*light->_isActive))
             continue;
 
         switch (light->_type)
