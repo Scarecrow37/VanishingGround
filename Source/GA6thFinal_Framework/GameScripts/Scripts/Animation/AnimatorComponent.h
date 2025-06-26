73f63c1771fa49bbdb7aa08c2af899aa590cd627
power12562 <kimsiwoo1005@gmail.com>
2025-06-26 15:05:54 +0900

    [CHORE/VG-28] ModelAnimator 클래스 이름 AnimatorComponent로 수정

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/Animation/AnimatorComponent.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "UmFramework.h"
+
+class AnimatorComponent : public Component
+{
+    USING_PROPERTY(AnimatorComponent)
+public:
+    REFLECT_PROPERTY()
+
+public:
+    AnimatorComponent();
+    virtual ~AnimatorComponent();
+
+protected:
+    REFLECT_FIELDS_BEGIN(Component)
+    REFLECT_FIELDS_END(AnimatorComponent)
+};
