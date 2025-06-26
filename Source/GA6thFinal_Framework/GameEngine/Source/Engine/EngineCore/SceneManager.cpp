3553fd9ca3c2be7b63767940120b5a26551b4d13
power12562 <kimsiwoo1005@gmail.com>
2025-06-26 22:23:49 +0900

    [FIX/VG-28] 프리팹 인스턴스 갱신시 버그 수정

--- Source/GA6thFinal_Framework/GameEngine/Source/Engine/EngineCore/SceneManager.cpp
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/EngineCore/SceneManager.cpp
@@ -400,23 +400,26 @@ void ESceneManager::Engine::SwapPrefabInstance(GameObject* original, GameObject*
     ESceneManager& sceneManager = UmSceneManager;
     int index = original->GetInstanceID();
     std::shared_ptr<GameObject>& sOrigin = sceneManager._runtimeObjects[index];
-    std::shared_ptr<GameObject>  sRemake = remake->GetWeakPtr().lock();
-    std::swap(sOrigin->_instanceID, sRemake->_instanceID);
-    std::swap(sOrigin->_ownerScene, sRemake->_ownerScene);
-    std::swap(sOrigin, sRemake);
-    std::string objectData = sRemake->SerializedReflectFields();
-    sOrigin->DeserializedReflectFields(objectData);
-    sOrigin->_transform = sRemake->_transform;
-    sceneManager.EraseGameObjectMap(sRemake);
-    sceneManager.InsertGameObjectMap(sOrigin);
-
-    for (int i = 0; i < sOrigin->GetComponentCount(); ++i)
+    if (nullptr != sOrigin)
     {
-        Component* component = sOrigin->GetComponentAtIndex<Component>(i);
-        if (component)
+        std::shared_ptr<GameObject> sRemake = remake->GetWeakPtr().lock();
+        std::swap(sOrigin->_instanceID, sRemake->_instanceID);
+        std::swap(sOrigin->_ownerScene, sRemake->_ownerScene);
+        std::swap(sOrigin, sRemake);
+        std::string objectData = sRemake->SerializedReflectFields();
+        sOrigin->DeserializedReflectFields(objectData);
+        sOrigin->_transform = sRemake->_transform;
+        sceneManager.EraseGameObjectMap(sRemake);
+        sceneManager.InsertGameObjectMap(sOrigin);
+
+        for (int i = 0; i < sOrigin->GetComponentCount(); ++i)
         {
-            component->_initFlags.SetAwake();
-            component->_initFlags.SetStart();
+            Component* component = sOrigin->GetComponentAtIndex<Component>(i);
+            if (component)
+            {
+                component->_initFlags.SetAwake();
+                component->_initFlags.SetStart();
+            }
         }
     }
 }
