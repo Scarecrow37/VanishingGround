e25c3d77fc55aa87c0da421bbca2aae0bf32d7bd
power12562 <kimsiwoo1005@gmail.com>
2025-06-27 12:05:24 +0900

    [FIX/VG-28] _activeInHierarchy 값 계산시 IsVaild 체크하도록 수정

--- Source/GA6thFinal_Framework/GameEngine/Source/Engine/GameCore/GameObject/GameObject.cpp
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/GameCore/GameObject/GameObject.cpp
@@ -517,15 +517,18 @@ std::string GameObject::Helper::GenerateUniqueName(std::string_view baseName)
 void GameObject::Engine::ResetActiveInHierarchy(GameObject* obj) 
 {
     Transform* curr = &obj->_transform;
-    curr->gameObject->_activeInHierarchy = true;
-    while (curr != nullptr)
+    curr->gameObject->_activeInHierarchy = obj->IsValid();
+    if (true == curr->gameObject->_activeInHierarchy)
     {
-        if (false == curr->gameObject->ReflectFields->_activeSelf)
+        while (curr != nullptr)
         {
-            curr->gameObject->_activeInHierarchy = false;   
-            break;
-        }          
-        curr = curr->Parent;
+            if (false == curr->gameObject->ReflectFields->_activeSelf)
+            {
+                curr->gameObject->_activeInHierarchy = false;
+                break;
+            }
+            curr = curr->Parent;
+        }
     }
 
     for (auto& component : obj->_components)
@@ -542,7 +545,7 @@ void GameObject::Engine::UpdateActiveInHierarchy(GameObject* obj)
         Transform* parent = currTr->Parent;
         bool parentActiveInHierarchy = parent ? parent->gameObject->_activeInHierarchy : true;
         bool prevActive = curr->_activeInHierarchy;
-        bool currActive = parentActiveInHierarchy && curr->ReflectFields->_activeSelf;
+        bool currActive = parentActiveInHierarchy && curr->ReflectFields->_activeSelf && curr->IsValid();
         if (prevActive != currActive)
         {
             curr->_activeInHierarchy = currActive;
