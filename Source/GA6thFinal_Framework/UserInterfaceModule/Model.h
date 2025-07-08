a9f43aaaac6a7d32d59d3ea4902eceab1bd3a9f4
Do_Suha <weqa123@naver.com>
2025-07-08 12:18:38 +0900

    C++20 표준 적용 및 모델 클래스 추가
    `Requires.h`에 `<functional>` 포함.
    `UserInterfaceModule.vcxproj`에 C++20 표준 설정 추가.
    `UserInterfaceModule.vcxproj`의 `<ItemGroup>`에 새로운 파일 포함.
    `UserInterfaceModule.vcxproj.filters`에 `Model` 필터 추가 및 관련 파일 포함.
    `Model.cpp`에 헤더 파일 포함.
    `Model.h`에 옵저버 패턴을 구현한 `IModel`과 값을 관리하는 `Model` 클래스 추가.

---
+++ Source/GA6thFinal_Framework/UserInterfaceModule/Model.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+namespace UI
+{
+    template <typename T>
+    class IModel
+    {
+    public:
+        void AddObserver(const std::function<void(T)>& observer) { _observer.push_back(observer); }
+
+    protected:
+        void Notify(T value) const
+        {
+            for (const auto& observer : _observer)
+            {
+                observer(value);
+            }
+        }
+
+    private:
+        std::vector<std::function<void(T)>> _observer;
+    };
+
+    template <typename T>
+    class Model : public IModel<T>
+    {
+    public:
+        Model() : _value(T()) {}
+
+        explicit Model(T value) : _value(value) {}
+
+        T Get() const { return _value; }
+
+        void Set(T value) { _value = value; }
+
+    private:
+        T _value;
+    };
+} // namespace UI
