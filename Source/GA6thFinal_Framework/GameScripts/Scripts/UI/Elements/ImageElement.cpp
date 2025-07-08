833953706d3af1261be681ffe8b4745dda77e090
Do_Suha <weqa123@naver.com>
2025-07-08 15:56:23 +0900

    ImageElement 클래스 수정 및 드래그 앤 드롭 기능 추가
    - `ImageElement.cpp`에서 `pchScripts.h` 제거 및 `ImageElement.h` 추가
    - 드래그 앤 드롭 이벤트 처리 로직 구현
    - `ImageElement.h`에 생성자 추가 및 `FilePath` 반사 속성 정의
    - `Guid` 필드 제거 및 `_guidRef` 비공개 멤버 변수 추가

--- Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Elements/ImageElement.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Elements/ImageElement.cpp
@@ -1,2 +1,26 @@
 ﻿#include "pchScripts.h"
 #include "ImageElement.h"
+
+ImageElement::ImageElement()
+{
+    FilePath.SetInputAutoEvent([this]() {
+        if (ImGui::BeginDragDropTarget())
+        {
+            if (const ImGuiPayload* payLoad = ImGui::AcceptDragDropPayload(DragDropAsset::KEY))
+            {
+                const DragDropAsset::Data* data    = static_cast<DragDropAsset::Data*>(payLoad->Data);
+                if (const auto context = data->pContext->lock(); nullptr != context)
+                {
+                    const auto& path      = context->GetPath();
+                    if (const auto extension = path.extension(); extension == L".png")
+                    {
+                        _guidRef            = path.ToGuid();
+                        ReflectFields->Guid = _guidRef.string();
+                        // TODO: UmSceneManager.ResourceManager.RequestModelResource(this, _guidRef);
+                    }
+                }
+            }
+            ImGui::EndDragDropTarget();
+        }
+    });
+}
--- Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Elements/ImageElement.h
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Elements/ImageElement.h
@@ -1,12 +1,23 @@
 ﻿#pragma once
 #include "../Base/UIComponent.h"
+#include "UmFramework.h"
 
 class ImageElement : public UIComponent
 {
     USING_PROPERTY(ImageElement)
 
+public:
+    ImageElement();
+
+    REFLECT_PROPERTY(FilePath)
+    GETTER_ONLY(std::string, FilePath) { return _guidRef.ToPath().string(); }
+    PROPERTY(FilePath)
+
 protected:
     REFLECT_FIELDS_BEGIN(UIComponent)
-
+    std::string Guid;
     REFLECT_FIELDS_END(ImageElement)
+
+private:
+    File::GuidRef _guidRef;
 };
