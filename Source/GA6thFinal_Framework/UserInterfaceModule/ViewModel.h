7b7bc9ee27a740b56cb52258ec1636578bdbefca
Do_Suha <weqa123@naver.com>
2025-07-08 12:48:53 +0900

    ViewModel 클래스 추가 및 프로젝트 파일 업데이트
    `UserInterfaceModule.vcxproj`와 `UserInterfaceModule.vcxproj.filters` 파일에 `ViewModel.h`와 `ViewModel.cpp` 파일을 추가했습니다.
    `ViewModel` 클래스는 템플릿을 사용하여 모델과의 상호작용을 처리하며, 콜백을 설정하고 모델의 변화를 관찰하여 변환된 값을 전달하는 기능을 포함합니다.

---
+++ Source/GA6thFinal_Framework/UserInterfaceModule/ViewModel.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include "Model.h"
+
+namespace UI
+{
+    template <typename T, typename U>
+    class ViewModel
+    {
+    public:
+        explicit ViewModel(IModel<T>* model) : _model(model)
+        {
+            _model->AddObserver([this](T value) {
+                if (_callback)
+                    _callback(Convert(value));
+            });
+        }
+
+        ViewModel(const ViewModel&)            = default;
+        ViewModel& operator=(const ViewModel&) = default;
+        ViewModel(ViewModel&&)                 = default;
+        ViewModel& operator=(ViewModel&&)      = default;
+        virtual ~ViewModel()                   = default;
+
+        void SetCallback(const std::function<void(U)>& callback) { _callback = callback; }
+
+    protected:
+        virtual U Convert(const T& value) { return static_cast<U>(value); }
+
+    private:
+        IModel<T>*             _model;
+        std::function<void(U)> _callback;
+    };
+} // namespace UI
