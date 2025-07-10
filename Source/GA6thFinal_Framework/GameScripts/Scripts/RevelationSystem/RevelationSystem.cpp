9342e3c6e842da4aeba8caf118f626831433168f
power12562 <kimsiwoo1005@gmail.com>
2025-07-10 12:11:07 +0900

    [FEATURE/VG-330] RevelationSystem 컴포넌트 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/RevelationSystem/RevelationSystem.cpp
@@ -0,0 +1,9 @@
+﻿#include "pchScripts.h"
+#include "RevelationSystem.h"
+RevelationSystem::RevelationSystem() = default;
+RevelationSystem::~RevelationSystem() = default;
+
+void RevelationSystem::Reset() 
+{
+    static_instance = this;
+}
---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/RevelationSystem/RevelationSystem.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "UmFramework.h"
+class RevelationSystem : public Component
+{
+    USING_PROPERTY(RevelationSystem)      
+public:
+    static RevelationSystem* GetInstance() { return static_instance; }
+
+public:
+    RevelationSystem();
+    ~RevelationSystem() override;
+
+public:
+    REFLECT_PROPERTY()
+
+protected:
+    REFLECT_FIELDS_BEGIN(Component)
+    REFLECT_FIELDS_END(RevelationSystem)
+
+    void Reset() override;
+
+private:
+    inline static RevelationSystem* static_instance = nullptr;
+
+};
