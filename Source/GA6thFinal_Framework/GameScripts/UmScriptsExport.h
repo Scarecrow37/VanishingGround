879a79ebf4a499fe918f2a15a7926b271fba18d1
power12562 <kimsiwoo1005@gmail.com>
2025-07-10 12:10:46 +0900

    [CHORE/VG-28] 스크립트 생성 템플릿 수정

--- Source/GA6thFinal_Framework/GameScripts/UmScriptsExport.h
+++ Source/GA6thFinal_Framework/GameScripts/UmScriptsExport.h
@@ -91,3 +91,6 @@ UMREAL_COMPONENT(TestUI)
 
 #include "Scripts\WeaponSystem/WeaponSystem.h"
 UMREAL_COMPONENT(WeaponSystem)
+
+#include "Scripts\RevelationSystem/RevelationSystem.h"
+UMREAL_COMPONENT(RevelationSystem)
