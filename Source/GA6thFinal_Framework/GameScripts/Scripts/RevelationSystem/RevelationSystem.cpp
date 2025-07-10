3bb5e3039cf12207c81a1e8fdf2296f1345ae16e
power12562 <kimsiwoo1005@gmail.com>
2025-07-10 14:21:25 +0900

    [FEATURE/VG-330] 역직렬화 기능 추가

--- Source/GA6thFinal_Framework/GameScripts/Scripts/RevelationSystem/RevelationSystem.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/RevelationSystem/RevelationSystem.cpp
@@ -3,7 +3,43 @@
 RevelationSystem::RevelationSystem() = default;
 RevelationSystem::~RevelationSystem() = default;
 
-void RevelationSystem::Reset() 
+void RevelationSystem::SerializedReflectEvent() 
+{
+    ReflectFields->RevelationActionDatas.clear();
+    for (auto& [key, action] : _actions)
+    {
+        std::string data = action->SerializedReflectFields();
+        ReflectFields->RevelationActionDatas.emplace_back(key, data);
+    }
+}
+
+void RevelationSystem::DeserializedReflectEvent() 
+{
+    _actions.clear();
+    for (int i = 0; i < ReflectFields->RevelationActionDatas.size(); ++i)
+    {
+        auto& [key, data] = ReflectFields->RevelationActionDatas[i];
+        RevelationActionBase* action = _actions[key].get();
+        action->DeserializedReflectFields(data);
+    }
+}
+
+void RevelationSystem::ImGuiDrawPropertysEvent() 
+{
+    if (ImGui::BeginCombo("Actions", "a"))
+    {
+        ImGui::EndCombo();
+    }
+}
+
+void RevelationSystem::Reset()
 {
     static_instance = this;
+
+    _actions.clear();
+    const auto& construtors = GetInstanceConstructors();
+    for (auto& [key, func] : construtors)
+    {
+        _actions[key].reset(func());
+    }
 }
--- Source/GA6thFinal_Framework/GameScripts/Scripts/RevelationSystem/RevelationSystem.h
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/RevelationSystem/RevelationSystem.h
@@ -1,6 +1,10 @@
 ﻿#pragma once
-#include "UmFramework.h"
-class RevelationSystem : public Component
+#include "RevelationAction/Base/RevelationActionBase.h"
+
+//RevelationActionBase를 상속받은 클래스를 팩토리에 등록할때 사용합니다.
+#define REGISTER_REVELATION_CLASS(REVELATION) REGISTER_CLASS(RevelationSystem, REVELATION)
+
+class RevelationSystem : public Component, public FactoryConstructor<RevelationActionBase>
 {
     USING_PROPERTY(RevelationSystem)      
 public:
@@ -11,15 +15,50 @@ public:
     ~RevelationSystem() override;
 
 public:
+    template<typename T>
+    RevelationActionBase* GetAction()
+    {
+        static_assert(std::is_base_of_v<RevelationActionBase, T>("T must be derived from RevelationActionBase."));
+        return GetActionToName(typeid(T).name());
+    }
+
+    RevelationActionBase* GetActionToName(std::string_view typeidName) 
+    { 
+        const char* key = typeidName.data();
+        return _actions[key].get(); 
+    }
+
+public:
     REFLECT_PROPERTY()
 
 protected:
     REFLECT_FIELDS_BEGIN(Component)
+    std::vector<std::pair<std::string, std::string>> RevelationActionDatas;
     REFLECT_FIELDS_END(RevelationSystem)
+
+    /// <summary>
+    /// <para> 직렬화 직전 자동으로 호출되는 이벤트 함수입니다. </para>
+    /// <para> 직접 override 해서 사용합니다.                 </para>
+    /// </summary>
+    void SerializedReflectEvent() override;
+
+    /// <summary>
+    /// <para> 역직렬화 이후 자동으로 호출되는 이벤트 함수 입니다.  </para>
+    /// <para> 직접 override 해서 사용합니다.                     </para>
+    /// </summary>
+    void DeserializedReflectEvent() override;
+
+    /// <summary>
+    /// <para>  ImGuiDrawPropertys() 호출 이후 콜되는 이벤트 함수입니다. </para>
+    /// </summary>
+    void ImGuiDrawPropertysEvent() override;
 
     void Reset() override;
 
 private:
     inline static RevelationSystem* static_instance = nullptr;
+
+private:
+    std::unordered_map<std::string, std::unique_ptr<RevelationActionBase>> _actions;
 
 };
