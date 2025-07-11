0a63844ecd385e4f0832ca4905b86ab110a78d8b
Do_Suha <weqa123@naver.com>
2025-07-11 13:22:49 +0900

    [FEATURE/VG-31] 그리드 패널 기능 개선
    - GridPanel 클래스의 상속 구조를 UIComponent에서 AreaUIComponent로 변경
    - 기본 생성자 추가 및 소멸자 삭제
    - 자식 게임 오브젝트 추가 및 디버그 정보 그리기 메서드 추가
    - 그리드 선 그리기 기능을 위한 DrawLine 메서드 추가
    - Columns와 Rows 속성에 대한 getter/setter 추가 및 값 범위 제한
    - GridPanelSlot 클래스의 상속 구조를 UIComponent에서 PanelSlotComponent로 변경
    - REFLECT_FIELDS_BEGIN 매크로 변경

--- Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Panels/GridPanel.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Panels/GridPanel.cpp
@@ -1,2 +1,56 @@
 ﻿#include "pchScripts.h"
 #include "GridPanel.h"
+
+GridPanel::GridPanel() = default;
+
+void GridPanel::OnAttachChild(GameObject* childGameObject)
+{
+    UIComponent::OnAttachChild(childGameObject);
+    childGameObject->AddComponent<GridPanelSlot>();
+}
+
+void GridPanel::OnDrawDebug()
+{
+    AreaUIComponent::OnDrawDebug();
+    DrawLine(DirectX::Colors::White);
+}
+
+void GridPanel::OnDrawDebugSelected()
+{
+    AreaUIComponent::OnDrawDebugSelected();
+    DrawLine(DirectX::Colors::Yellow);
+}
+
+void GridPanel::DrawLine(FXMVECTOR color) const
+{
+    const unsigned int columns = Columns;
+    const unsigned int rows    = Rows;
+    if (columns == 0 || rows == 0) return;
+    
+
+    const long stepX = _size.cx / static_cast<LONG>(columns);
+    for (unsigned int i = 1; i < columns; ++i)
+    {
+        POINT start{};
+        start.x = _point.x + stepX * static_cast<LONG>(i);
+        start.y = _point.y;
+
+        POINT end{};
+        end.x = start.x;
+        end.y = start.y + _size.cy;
+
+        UmDebugDrawCore.Draw("Editor", {start, end}, color);
+    }
+
+    const long stepY = _size.cy / static_cast<LONG>(rows);
+    for (unsigned int i = 1; i < rows; ++i)
+    {
+        POINT start{};
+        start.x = _point.x;
+        start.y = _point.y + stepY * static_cast<LONG>(i);
+        POINT end{};
+        end.x = start.x + _size.cx;
+        end.y = start.y;
+        UmDebugDrawCore.Draw("Editor", {start, end}, color);
+    }
+}
--- Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Panels/GridPanel.h
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Panels/GridPanel.h
@@ -1,21 +1,41 @@
 ﻿#pragma once
-#include "UI/Base/UIComponent/UIComponent.h"
+#include "UI/Base/AreaUIComponent/AreaUIComponent.h"
+#include "UI/Base/PanelSlotComponent/PanelSlotComponent.h"
 
-class GridPanel : public UIComponent
+class GridPanel : public AreaUIComponent
 {
     USING_PROPERTY(GridPanel)
 public:
-    GridPanel()           = default;
-    ~GridPanel() override = default;
+    GridPanel();
 
-    REFLECT_PROPERTY()
+    REFLECT_PROPERTY(Columns, Rows)
+
+    GETTER(unsigned int, Columns) { return ReflectFields->Columns; }
+    SETTER(unsigned int, Columns) { ReflectFields->Columns = std::clamp(value, 1u, 64u); }
+    PROPERTY(Columns)
+
+    GETTER(unsigned int, Rows) { return ReflectFields->Rows; }
+    SETTER(unsigned int, Rows) { ReflectFields->Rows = std::clamp(value, 1u, 64u); }
+    PROPERTY(Rows)
 
 protected:
-    REFLECT_FIELDS_BEGIN(Component)
+    REFLECT_FIELDS_BEGIN(AreaUIComponent)
+    unsigned int Columns = 1;
+    unsigned int Rows = 1;
     REFLECT_FIELDS_END(GridPanel)
+
+    void OnAttachChild(GameObject* childGameObject) override;
+
+    void OnDrawDebug() override;
+
+    void OnDrawDebugSelected() override;
+
+private:
+    void DrawLine(FXMVECTOR color) const;
+
 };
 
-class GridPanelSlot : public UIComponent
+class GridPanelSlot : public PanelSlotComponent
 {
     USING_PROPERTY(GridPanelSlot)
 public:
@@ -25,6 +45,6 @@ public:
     REFLECT_PROPERTY()
 
 protected:
-    REFLECT_FIELDS_BEGIN(Component)
+    REFLECT_FIELDS_BEGIN(PanelSlotComponent)
     REFLECT_FIELDS_END(GridPanelSlot)
 };
