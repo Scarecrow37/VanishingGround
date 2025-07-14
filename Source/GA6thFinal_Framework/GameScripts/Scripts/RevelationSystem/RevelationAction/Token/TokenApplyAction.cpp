7f6299217ca6660294494d35c964a0260506f2a5
power12562 <kimsiwoo1005@gmail.com>
2025-07-14 14:45:38 +0900

    [FEATURE/VG-330] TokenApplyAction 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/RevelationSystem/RevelationAction/Token/TokenApplyAction.cpp
@@ -0,0 +1,28 @@
+﻿#include "pchScripts.h"
+#include "TokenApplyAction.h"
+#include <RevelationSystem/RevelationSystem.h>
+
+REGISTER_REVELATION_CLASS(TokenApplyAction)
+
+TokenApplyAction::TokenApplyAction() 
+    : 
+    RevelationActionBase(u8"대상에게 토큰 부여")
+{
+
+}
+
+std::string_view TokenApplyAction::GetActionInfo()
+{
+    constexpr const char8_t* info = u8"대상에게 출혈III 2개 부여";
+    return (const char*)info;
+}
+
+void TokenApplyAction::ImGuiDrawActionEditor() 
+{
+
+}
+
+void TokenApplyAction::Execute(CharacterBase* attacker, CharacterBase* target) 
+{
+
+}
---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/RevelationSystem/RevelationAction/Token/TokenApplyAction.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "../Base/RevelationActionBase.h"
+#include <DamageSystem/Interface/IDamageAction.h>
+
+class TokenApplyAction : public RevelationActionBase, public IDamageAction
+{
+    USING_PROPERTY(TokenApplyAction)
+public:
+    TokenApplyAction();
+    ~TokenApplyAction() override = default;
+    REFLECT_PROPERTY()
+
+protected:
+    REFLECT_FIELDS_BEGIN(RevelationActionBase)
+    
+    REFLECT_FIELDS_END(TokenApplyAction)
+
+
+public:
+    // RevelationActionBase을(를) 통해 상속됨
+    std::string_view GetActionInfo() override;
+    void             ImGuiDrawActionEditor() override;
+
+    // IDamageAction을(를) 통해 상속됨
+    void Execute(CharacterBase* attacker, CharacterBase* target) override;
+
+};
