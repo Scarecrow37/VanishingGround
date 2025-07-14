193e1d11cc81cd7693cad0dbdce9606beb41a7c3
power12562 <kimsiwoo1005@gmail.com>
2025-07-14 12:50:36 +0900

    [FIX/VG-330] IDamageAction 상속받로속 수정

--- Source/GA6thFinal_Framework/GameScripts/Scripts/RevelationSystem/RevelationAction/Base/RevelationActionBase.h
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/RevelationSystem/RevelationAction/Base/RevelationActionBase.h
@@ -1,5 +1,6 @@
 ﻿#pragma once
 #include "UmFrameWork.h"
+#include <DamageSystem/Interface/IDamageAction.h>
 
 // RevelationActionBase를 상속받은 클래스를 팩토리에 등록할때 사용합니다.
 #define REGISTER_REVELATION_CLASS(REVELATION) REGISTER_CLASS(RevelationSystem, REVELATION)
@@ -8,7 +9,7 @@
 * 계시 효과를 정의하는 Base 클래스 입니다. cpp에서 REGISTER_REVELATION_CLASS를 해야합니다.
 * 생성자에서 계시 이름을 지정해줘야 합니다.
 */
-class RevelationActionBase abstract : public ReflectSerializer
+class RevelationActionBase abstract : public ReflectSerializer, public IDamageAction
 {
     USING_PROPERTY(RevelationActionBase)
 public:
@@ -33,6 +34,9 @@ protected:
 
 private:
     std::string_view _name;
+
+protected:
+    void Execute(CharacterBase* attacker, CharacterBase* target) override;
 
 };
 
