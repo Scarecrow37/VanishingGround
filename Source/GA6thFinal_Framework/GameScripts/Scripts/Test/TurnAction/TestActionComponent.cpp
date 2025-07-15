aaba379cd883e9c44136ee475707fd28cf616603
power12562 <kimsiwoo1005@gmail.com>
2025-07-15 17:13:32 +0900

    [FEATURE/VG-316] TestActionComponent 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/Test/TurnAction/TestActionComponent.cpp
@@ -0,0 +1,46 @@
+﻿#include "pchScripts.h"
+#include "TestActionComponent.h"
+#include <TurnSystem/TurnMode/TurnMode.h>
+
+TestActionComponent::TestActionComponent() = default;
+TestActionComponent::~TestActionComponent() = default;
+
+void TestActionComponent::Start() 
+{
+    TurnMode* turnMode = TurnMode::GetInstance();
+    if (turnMode)
+    {
+        _testAction.reset(new TestAction);
+        turnMode->AddTurnAction(_testAction.get());
+    }
+}
+
+void TestAction::OnCombatStart()
+{
+    __debugbreak();
+}
+
+void TestAction::OnRoundStart()
+{
+    __debugbreak();
+}
+
+void TestAction::OnRoundEnd()
+{
+    __debugbreak();
+}
+
+void TestAction::OnEachTurnStart(CharacterBase* destination)
+{
+    __debugbreak();
+}
+
+void TestAction::OnTurnStart()
+{
+    __debugbreak();
+}
+
+void TestAction::OnTurnEnd()
+{
+    __debugbreak();
+}
---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/Test/TurnAction/TestActionComponent.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include "UmFramework.h"
+#include <TurnSystem/TurnAction/TurnAction.h>
+
+class TestAction : public TurnAction
+{
+public:
+    TestAction()           = default;
+    ~TestAction() override = default;
+
+    // TurnAction을(를) 통해 상속됨
+    void OnCombatStart() override;
+    void OnRoundStart() override;
+    void OnRoundEnd() override;
+    void OnEachTurnStart(CharacterBase* destination) override;
+    void OnTurnStart() override;
+    void OnTurnEnd() override;
+};
+
+
+
+class TestActionComponent : public Component
+{
+    USING_PROPERTY(TestActionComponent)
+
+public:
+    TestActionComponent();
+    ~TestActionComponent() override;
+
+public:
+    REFLECT_PROPERTY()
+
+protected:
+    REFLECT_FIELDS_BEGIN(Component)
+    REFLECT_FIELDS_END(TestActionComponent)
+
+    void Start() override;
+
+    std::unique_ptr<TestAction> _testAction;
+};
