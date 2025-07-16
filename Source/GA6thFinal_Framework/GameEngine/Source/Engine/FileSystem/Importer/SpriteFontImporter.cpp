48b0e83424b7441913e616a3c5ab312ecbd27c45
PPAK_JU <taku7664@gmail.com>
2025-07-16 13:12:35 +0900

    [CHORE/VG-29] SpriteFontImporter 디테일 정리

--- Source/GA6thFinal_Framework/GameEngine/Source/Engine/FileSystem/Importer/SpriteFontImporter.cpp
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/FileSystem/Importer/SpriteFontImporter.cpp
@@ -29,27 +29,38 @@ void Impoerter::SpriteFontImporter::OnRequestedDragDrop(const File::Path& path)
     }
 }
 
+#define IMGUI_TEXT_BLOCK(text, offset)\
+ImGui::Text("Font Size");\
+ImGui::SameLine(offset);\
+ImGui::SeparatorEx(ImGuiSeparatorFlags_Vertical);\
+ImGui::SameLine();
+
 void Impoerter::SpriteFontImporter::DrawImGuiImportSetting() 
 {
     std::string path = _importPath.string();
     ImGui::BeginDisabled();
-    ImGui::InputText("##ImportPath", &path, ImGuiInputTextFlags_ReadOnly);
+    ImGui::InputText("##import path", &path, ImGuiInputTextFlags_ReadOnly);
+    if (ImGui::BeginItemTooltip())
+    {
+        ImGui::Text(path.c_str());
+        ImGui::EndTooltip();
+    }
     ImGui::EndDisabled();
     ImGui::SameLine();
     if (ImGui::Button(EditorIcon::ICON_FOLDER_OPEN))
     {
         File::ShowOpenFolderDialog(NULL, L"경로 선택", _importPath.c_str(), _importPath);
     }
-
     ImGui::Separator();
-
     if (ImGui::CollapsingHeader("Import Settings##sprite font importer", ImGuiTreeNodeFlags_DefaultOpen))
     {
-        ImGui::Text("Font Import Settings");
-        // 여기에 폰트 임포트 설정을 추가할 수 있습니다.
-        // 예: 폰트 크기, 스타일 등
-        ImGui::InputInt("Font Size", &_fontSize);
-        ImGui::ColorEdit4("Font Color", (float*)&_fontColor,
-                          ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoLabel);
+        const float separatorOffset = 130.0f;
+        IMGUI_TEXT_BLOCK("Font Size", separatorOffset);
+        ImGui::DragInt("##font size", &_fontSize, 1.0f);
+        _fontSize = std::max(1, _fontSize); // Clamp
+
+        IMGUI_TEXT_BLOCK("Font Color", separatorOffset);
+        ImGui::ColorEdit4("#font color", (float*)&_fontColor, ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoLabel);
+      
     }
 }
