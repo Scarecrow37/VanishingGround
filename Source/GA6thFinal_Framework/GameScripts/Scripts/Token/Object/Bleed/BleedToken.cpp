9777d04849fc9104b5303e5304af8271d9f84db0
PPAK_JU <taku7664@gmail.com>
2025-07-16 14:45:03 +0900

    [CHORE/VG-328] 출혈 토큰 정리

--- Source/GA6thFinal_Framework/GameScripts/Scripts/Token/Object/Bleed/BleedToken.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/Token/Object/Bleed/BleedToken.cpp
@@ -6,10 +6,8 @@
 namespace TokenObject
 {
     REGISTER_TOKEN(Bleed1)
-    REGISTER_TOKEN(Bleed2)
-    REGISTER_TOKEN(Bleed3)
 
-    void Bleed1::OnTurnStart(CharacterBase* owner)
+    void Bleed1::OnRoundStart(CharacterBase* owner)
     {
         auto& tokenInventory = owner->GetTokenInventory();
         int   stackCount     = tokenInventory.GetTokenStackFromID(ID);
@@ -19,32 +17,9 @@ namespace TokenObject
                                       (const char*)u8"의 토큰이 발동했습니다. (", stackCount, (const char*)u8"스택)");
         UmLogger.Log(LogLevel::LEVEL_WARNING, msg);
         // TODO: 데미지 적용
-        tokenInventory.RemoveTokenStackFromID(ID);
-    }
+        int damage = ReflectFields->TickDamage * stackCount;
 
-    void Bleed2::OnTurnStart(CharacterBase* owner)
-    {
-        auto& tokenInventory = owner->GetTokenInventory();
-        int   stackCount     = tokenInventory.GetTokenStackFromID(ID);
 
-        GameObject& gameObject = owner->gameObject;
-        std::string msg = std::format("{}{} {}{}{}{}", gameObject.ToString(), (const char*)u8"에게서", GetTokenName(),
-                                      (const char*)u8"의 토큰이 발동했습니다. (", stackCount, (const char*)u8"스택)");
-        UmLogger.Log(LogLevel::LEVEL_WARNING, msg);
-        // TODO: 데미지 적용
-        tokenInventory.RemoveTokenStackFromID(ID);
-    }
-
-    void Bleed3::OnTurnStart(CharacterBase* owner)
-    {
-        auto& tokenInventory = owner->GetTokenInventory();
-        int   stackCount     = tokenInventory.GetTokenStackFromID(ID);
-
-        GameObject& gameObject = owner->gameObject;
-        std::string msg = std::format("{}{} {}{}{}{}", gameObject.ToString(), (const char*)u8"에게서", GetTokenName(),
-                                      (const char*)u8"의 토큰이 발동했습니다. (", stackCount, (const char*)u8"스택)");
-        UmLogger.Log(LogLevel::LEVEL_WARNING, msg);
-        // TODO: 데미지 적용
         tokenInventory.RemoveTokenStackFromID(ID);
     }
 } // namespace TokenObject
