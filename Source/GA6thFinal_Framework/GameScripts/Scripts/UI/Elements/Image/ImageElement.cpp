5fb7e0fbc53d72130fb5c800217c42f2dbdae0d3
Do_Suha <weqa123@naver.com>
2025-07-16 17:27:30 +0900

    [REFACTOR/VG-31] 이미지 요소 리셋 함수 수정
    `ImageElement::Reset()`에서 `_renderer`의 등록 방식을 변경했습니다.
    `RegisterRenderQueue()` 호출을 제거하고 `RegisterComponent()`를 추가하여
    렌더링 큐 대신 컴포넌트로 등록하도록 수정했습니다.

--- Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Elements/Image/ImageElement.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Elements/Image/ImageElement.cpp
@@ -39,7 +39,7 @@ void ImageElement::Reset()
     try
     {
         _renderer = std::make_unique<SpriteRenderer>(_worldMatrix, SpriteType::MODE_2D);
-        _renderer->RegisterRenderQueue();
+        _renderer->RegisterComponent();
         _renderer->SetActive(&EnableInHierarchy);
     }
     catch (...)
