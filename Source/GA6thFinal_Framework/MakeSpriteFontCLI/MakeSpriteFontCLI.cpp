a924438429c474bd5c2baa6f6ee06d21e58633f8
Do_Suha <weqa123@naver.com>
2025-07-18 10:11:54 +0900

    [FEATURE/VG-31] MakeSpriteFontCLI 프로젝트 추가
    - C++/CLI로 작성된 새로운 동적 라이브러리 "MakeSpriteFontCLI"가 추가되었습니다.
    - 솔루션 파일에 프로젝트가 포함되었고, 관련 빌드 설정이 정의되었습니다.
    - 애플리케이션 아이콘과 리소스 스크립트가 추가되었습니다.
    - 어셈블리 메타데이터 및 미리 컴파일된 헤더 설정이 포함되었습니다.
    - MakeSpriteFont 라이브러리와의 상호작용을 위한 기능이 구현되었습니다.

---
+++ Source/GA6thFinal_Framework/MakeSpriteFontCLI/MakeSpriteFontCLI.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "MakeSpriteFontCLI.h"
+
+using namespace System;
+using namespace System::Collections::Generic;
+
+array<String ^> ^ ToManagedStringArray(const std::vector<std::string>& vec)
+{
+    array<String ^> ^ arr = gcnew array<String ^>(static_cast<int>(vec.size()));
+    for (int i = 0; i < vec.size(); ++i)
+    {
+        arr[i] = gcnew String(vec[i].c_str());
+    }
+    return arr;
+}
+
+int MakeSpriteFontCLI::MakeSpriteFontCLI::Run(const std::vector<std::string>& options)
+{
+    MakeSpriteFont::Library ^ library = gcnew MakeSpriteFont::Library();
+    return library->Make(ToManagedStringArray(options));
+}
