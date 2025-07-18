ce6ed188918d40fb60d60365c3e2281c227fc7ce
Do_Suha <weqa123@naver.com>
2025-07-18 10:06:41 +0900

    [FEATURE/VG-31] MakeSpriteFont 프로젝트 추가
    - DirectXTK MakeSpriteFont 도구를 위한 "MakeSpriteFont" 프로젝트를 솔루션에 추가했습니다.
    - 비트맵에서 글리프를 추출하고 스프라이트 폰트를 생성하는 기능을 포함하는 여러 C# 파일을 추가했습니다.
    - 프로젝트의 빌드 설정과 참조를 정의하는 `MakeSpriteFont.csproj` 파일이 추가되었습니다.
    - 각 파일에는 MIT 라이센스에 대한 저작권 정보가 포함되어 있습니다.
    - 글리프 처리, 비트맵 조작, 명령줄 옵션 파싱을 위한 다양한 클래스가 구현되었습니다.

---
+++ Source/GA6thFinal_Framework/MakeSpriteFont/PrivateFontCollectionImporter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeSpriteFont
+{
+    public class PrivateFontCollectionImporter : TrueTypeImporter
+    {
+        // Attempts to instantiate the requested GDI+ font object.
+        protected override Font CreateFont(CommandLineOptions options)
+        {
+            PrivateFontCollection fontCollection = new PrivateFontCollection();
+            fontCollection.AddFontFile(options.SourceFont);
+            FontFamily fontFamily = fontCollection.Families.FirstOrDefault();
+            if (fontFamily == null)
+            {
+                throw new Exception(string.Format("Unable to load font from '{0}'.", options.SourceFont));
+            }
+
+            Font font = new Font(fontFamily, PointsToPixels(options.FontSize), options.FontStyle, GraphicsUnit.Pixel);
+
+            string fontName = Path.GetFileNameWithoutExtension(options.SourceFont);
+
+            try
+            {
+                // The font constructor automatically substitutes fonts if it can't find the one requested.
+                // But we prefer the caller to know if anything is wrong with their data. A simple string compare
+                // isn't sufficient because some fonts (eg. MS Mincho) change names depending on the locale.
+
+                // Early out: in most cases the name will match the current or invariant culture.
+                if (fontName.Equals(font.FontFamily.GetName(CultureInfo.CurrentCulture.LCID), StringComparison.OrdinalIgnoreCase) ||
+                    fontName.Equals(font.FontFamily.GetName(CultureInfo.InvariantCulture.LCID), StringComparison.OrdinalIgnoreCase))
+                {
+                    return font;
+                }
+
+                // Check the font name in every culture.
+                foreach (CultureInfo culture in CultureInfo.GetCultures(CultureTypes.SpecificCultures))
+                {
+                    if (fontName.Equals(font.FontFamily.GetName(culture.LCID), StringComparison.OrdinalIgnoreCase))
+                    {
+                        return font;
+                    }
+                }
+
+                // A font substitution must have occurred.
+                throw new Exception(string.Format("Can't find font '{0}'.", options.SourceFont));
+            }
+            catch
+            {
+                font.Dispose();
+                throw;
+            }
+        }
+    }
+}
