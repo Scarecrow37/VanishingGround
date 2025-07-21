c97d55e5bce70dcf014c17731350c583a102a0a1
Do_Suha <weqa123@naver.com>
2025-07-21 19:45:05 +0900

    [FEATURE/VG-31] 앵커 패널 시스템 추가 및 그리드 패널 제거
    새로운 `AnchorPanel` 및 `AnchorPanelSlot` 클래스를 추가하여 UI 요소의 앵커링 기능을 구현했습니다.
    자식 게임 오브젝트의 배치 변경을 처리하는 메서드와 선택된 앵커 포인트를 시각적으로 표시하는 기능을 포함합니다.
    기존의 `GridPanel` 및 `GridPanelSlot` 클래스는 제거되었습니다.

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Panels/Anchor/AnchorPanel.cpp
@@ -0,0 +1,132 @@
+﻿#include "pchScripts.h"
+#include "AnchorPanel.h"
+
+AnchorPanel::AnchorPanel() = default;
+
+void AnchorPanel::OnAttachChild(GameObject* childGameObject)
+{
+    EditablePlacementUIComponent::OnAttachChild(childGameObject);
+    auto& slot = childGameObject->AddComponent<AnchorPanelSlot>();
+    slot.SetPlacement(ReflectFields->Basefields.get().Basefields.get().Point,
+                      ReflectFields->Basefields.get().Basefields.get().Size);
+}
+
+void AnchorPanel::OnPlacementChange()
+{
+    EditablePlacementUIComponent::OnPlacementChange();
+    for (int i = 0; i < transform->GetChildCount(); ++i)
+    {
+        const Transform* child      = transform->GetChild(i);
+        GameObject&      gameObject = child->gameObject;
+        for (int j = 0; j < gameObject.GetComponentCount(); ++j)
+        {
+            if (AnchorPanelSlot* slot = gameObject.GetComponentAtIndex<AnchorPanelSlot>(j))
+            {
+                POINT point = ReflectFields->Basefields.get().Basefields.get().Point;
+                auto [x, y] = ReflectFields->Basefields.get().Basefields.get().ScopePoint;
+                point.x += x;
+                point.y += y;
+                slot->SetPlacement(point,
+                                   ReflectFields->Basefields.get().Basefields.get().Size);
+            }
+        }
+    }
+}
+
+AnchorPanelSlot::AnchorPanelSlot() = default;
+
+void AnchorPanelSlot::OnSetPlacement()
+{
+    const POINT point         = ReflectFields->Basefields.get().Basefields.get().Point;
+    const auto size = ReflectFields->Basefields.get().Basefields.get().Size;
+
+    _anchorPoint              = point;
+    switch (AnchorType anchor = ReflectFields->Anchor)
+    {
+    case AnchorType::TOP_LEFT:
+        // Nothing to do here, as the point is already at the top left.
+        break;
+    case AnchorType::TOP_CENTER:
+        _anchorPoint.x += size.cx / 2;
+        break;
+    case AnchorType::TOP_RIGHT:
+        _anchorPoint.x += size.cx;
+        break;
+    case AnchorType::CENTER_LEFT:
+        _anchorPoint.y += size.cy / 2;
+        break;
+    case AnchorType::CENTER:
+        _anchorPoint.x += size.cx / 2;
+        _anchorPoint.y += size.cy / 2;
+        break;
+    case AnchorType::CENTER_RIGHT:
+        _anchorPoint.x += size.cx;
+        _anchorPoint.y += size.cy / 2;
+        break;
+    case AnchorType::BOTTOM_LEFT:
+        _anchorPoint.y += size.cy;
+        break;
+    case AnchorType::BOTTOM_CENTER:
+        _anchorPoint.x += size.cx / 2;
+        _anchorPoint.y += size.cy;
+        break;
+    case AnchorType::BOTTOM_RIGHT:
+        _anchorPoint.x += size.cx;
+        _anchorPoint.y += size.cy;
+        break;
+    }
+
+    PassScopedPlacement(_anchorPoint, size);
+}
+
+void AnchorPanelSlot::ImGuiDrawPropertysEvent()
+{
+    PanelSlotComponent::ImGuiDrawPropertysEvent();
+    static const char* items[]      = {"Top Left",     "Top Center",  "Top Right",     "Center Left", "Center",
+                                       "Center Right", "Bottom Left", "Bottom Center", "Bottom Right"};
+    int                currentIndex = static_cast<int>(ReflectFields->Anchor);
+    if (ImGui::Combo("##Anchor", &currentIndex, items, IM_ARRAYSIZE(items)))
+    {
+        ReflectFields->Anchor = static_cast<AnchorType>(currentIndex);
+        OnSetPlacement();
+    }
+    ImGui::SameLine();
+    ImGui::Text("Anchor");
+}
+
+void AnchorPanelSlot::DrawDebugSelected()
+{
+    PanelSlotComponent::DrawDebugSelected();
+
+    auto [anchorX, anchorY] = _anchorPoint;
+
+    constexpr LONG lineSize = 20;
+
+    // Top to bottom line
+    {
+        POINT start{anchorX, anchorY - lineSize};
+        POINT end{anchorX, anchorY + lineSize};
+        UmDebugDrawCore.Draw("Editor", {start, end}, DirectX::Colors::Yellow);
+    }
+
+    // Left to right line
+    {
+        POINT start{anchorX - lineSize, anchorY};
+        POINT end{anchorX + lineSize, anchorY};
+        UmDebugDrawCore.Draw("Editor", {start, end}, DirectX::Colors::Yellow);
+    }
+
+    // LeftTop to RightBottom line
+    {
+        POINT start{anchorX - lineSize, anchorY - lineSize};
+        POINT end{anchorX + lineSize, anchorY + lineSize};
+        UmDebugDrawCore.Draw("Editor", {start, end}, DirectX::Colors::Yellow);
+    }
+
+    // RightTop to LeftBottom line
+    {
+        POINT start{anchorX + lineSize, anchorY - lineSize};
+        POINT end{anchorX - lineSize, anchorY + lineSize};
+        UmDebugDrawCore.Draw("Editor", {start, end}, DirectX::Colors::Yellow);
+    }
+}
---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Panels/Anchor/AnchorPanel.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+#include "UI/Base/EditablePlacementUIComponent/EditablePlacementUIComponent.h"
+#include "UI/Base/PanelSlotComponent/PanelSlotComponent.h"
+
+class AnchorPanel : public EditablePlacementUIComponent
+{
+    USING_PROPERTY(AnchorPanel)
+public:
+    AnchorPanel();
+
+    void OnAttachChild(GameObject* childGameObject) override;
+    void OnPlacementChange() override;
+
+protected:
+    REFLECT_FIELDS_BEGIN(EditablePlacementUIComponent)
+    REFLECT_FIELDS_END(AnchorPanel)
+};
+
+class AnchorPanelSlot : public PanelSlotComponent
+{
+    friend AnchorPanel;
+    USING_PROPERTY(AnchorPanelSlot)
+
+public:
+    enum class AnchorType : unsigned char
+    {
+        TOP_LEFT,
+        TOP_CENTER,
+        TOP_RIGHT,
+        CENTER_LEFT,
+        CENTER,
+        CENTER_RIGHT,
+        BOTTOM_LEFT,
+        BOTTOM_CENTER,
+        BOTTOM_RIGHT
+    };
+
+public:
+    AnchorPanelSlot();
+
+    void OnSetPlacement() override;
+
+protected:
+    void ImGuiDrawPropertysEvent() override;
+    void DrawDebugSelected() override;
+
+protected:
+    REFLECT_FIELDS_BEGIN(PanelSlotComponent)
+    AnchorType Anchor = AnchorType::TOP_LEFT;
+    REFLECT_FIELDS_END(AnchorPanelSlot)
+
+private:
+    POINT _anchorPoint;
+};
