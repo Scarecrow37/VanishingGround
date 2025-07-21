bf9f4c052a0ee58c6c9a061345361f4c59e7a142
power12562 <kimsiwoo1005@gmail.com>
2025-07-21 14:19:39 +0900

    [FEATURE/VG-316] 계시 시스템 라운드 시작시 뽑도록 기능 추가

--- Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnMode/State/Base/TurnModeStateBase.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnMode/State/Base/TurnModeStateBase.cpp
@@ -2,6 +2,7 @@
 #include "TurnModeStateBase.h"
 #include "TurnSystem/TurnMode/TurnMode.h"
 #include <WeaponSystem/WeaponSystem.h>
+#include <RevelationSystem/RevelationSystem.h>    
 
 TurnModeStateBase::TurnModeStateBase() {}
 
@@ -14,5 +15,10 @@ void TurnModeStateBase::OnStart()
     if (_weaponSystem == nullptr)
     {
         UmLogger.Log(LogLevel::LEVEL_WARNING, u8"Game System에 Weapon System이 존재하지 않습니다.");
+    }
+    _revelationSystem = GetFSM().GetComponent<RevelationSystem>();
+    if (_revelationSystem == nullptr)
+    {
+        UmLogger.Log(LogLevel::LEVEL_WARNING, u8"Game System에 Revelation System이 존재하지 않습니다.");
     }
 }
