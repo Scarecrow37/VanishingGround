26f77680e957ce06e5c149108d8ada6c48eca2bb
power12562 <kimsiwoo1005@gmail.com>
2025-07-22 12:44:21 +0900

    [CHORE/VG-316] DamageSystem에 Info용 구조체 추가

--- Source/GA6thFinal_Framework/GameScripts/Scripts/DamageSystem/DamageSystem.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/DamageSystem/DamageSystem.cpp
@@ -4,9 +4,16 @@
 #include <Stats/Enemy/EnemyStats.h>
 #include <Stats/Weapon/WeaponStats.h>
 
-int DamageSystem::CalculateDamage(const PlayerStats& attacker, const WeaponStats& attackerWeapon, const EnemyStats& target)
+int DamageSystem::CalculateDamage(const PlayerInfo& attacker, const EnemyInfo& target)
 {
     int result = 0;
-    result = attackerWeapon.CriticalDamage;
+    result     = attacker._weaponStats.CriticalDamage;
+    return result;
+}
+
+int DamageSystem::CalculateDamage(const EnemyInfo& attacker, const PlayerInfo& target)
+{
+    int result = 0;
+    result = 10;
     return result;
 }
--- Source/GA6thFinal_Framework/GameScripts/Scripts/DamageSystem/DamageSystem.h
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/DamageSystem/DamageSystem.h
@@ -6,8 +6,38 @@ class Player;
 struct PlayerStats;
 struct WeaponStats;
 
+struct PlayerInfo
+{
+    friend class DamageSystem;
+    PlayerInfo(const Player& player, const WeaponStats& weaponStats, const PlayerStats& playerStats)
+        : _player(player), _weaponStats(weaponStats), _playerStats(playerStats)
+    {
+    }
+    ~PlayerInfo() = default;
+
+private:
+    const Player&      _player;
+    const WeaponStats& _weaponStats;
+    const PlayerStats& _playerStats;
+};
+
+struct EnemyInfo
+{
+    friend class DamageSystem;
+    EnemyInfo(const Enemy& enemy, const EnemyStats& enemyStats) 
+        : _enemy(enemy), _enemyStats(enemyStats) 
+    {
+    }
+    ~EnemyInfo() = default;
+
+private:
+    const Enemy& _enemy;
+    const EnemyStats& _enemyStats;
+};
+
 class DamageSystem : public Component
 {
 public:
-    static int CalculateDamage(const PlayerStats& attacker, const WeaponStats& attackerWeapon, const EnemyStats& target);
+    static int CalculateDamage(const PlayerInfo& attacker, const EnemyInfo& target);
+    static int CalculateDamage(const EnemyInfo& attacker, const PlayerInfo& target);
 };
