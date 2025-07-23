9a8ad22c1af24b9b3f7218ef7791b086aefd1ba5
power12562 <kimsiwoo1005@gmail.com>
2025-07-23 18:09:33 +0900

    [FEATURE/VG-316] MultiTargetDamageAction 기능 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnAction/MultiTargetDamage/MultiTargetDamageAction.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <TurnSystem/TurnAction/TurnActionFactory.h>
+
+class MultiTargetDamageAction : public TurnAction
+{
+    USING_PROPERTY(MultiTargetDamageAction)
+public:
+    MultiTargetDamageAction() = default;
+    ~MultiTargetDamageAction() override = default;
+    REFLECT_PROPERTY()
+        
+protected:
+    REFLECT_FIELDS_BEGIN(TurnAction)
+    
+    REFLECT_FIELDS_END(MultiTargetDamageAction)
+
+
+private:
+    const std::string& GetActionName() override;
+    const std::string& GetActionInfo() override;
+    void               ImGuiDrawActionEditor() override;
+    void               OnPlayerBattleTargetSelected(Battle::EnemyTargetFlag& targetFlag) override;
+
+    void UpdateActionInfo();
+    std::string _actionInfo;
+};
+
