45d556a14893139c2474795ce075b31f39c14099
Do_Suha <weqa123@naver.com>
2025-07-24 17:40:43 +0900

    [FEATURE/VG-180] 중앙 정렬 기능 개선
    CenterWrapper 클래스의 중앙 정렬 로직을 개선하여, 자식 컴포넌트의 중앙 위치를 계산하는 새로운 메서드를 추가했습니다.
    수평 및 수직 중앙 정렬 여부를 확인할 수 있는 메서드도 추가되었으며, 자식 게임 오브젝트가 추가될 때 중앙 정렬을 적용할 수 있도록 관련 메서드가 수정되었습니다.
    또한, CenterType 열거형을 변경하고, 중앙 정렬 상태를 쉽게 설정할 수 있는 getter와 setter를 추가했습니다.

--- Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Wrappers/Center/CenterWrapper.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Wrappers/Center/CenterWrapper.cpp
@@ -1,52 +1,56 @@
 ﻿#include "pchScripts.h"
 #include "CenterWrapper.h"
 
+
+POINT CenterWrapper::GetCenterPoint(const SIZE childSize) const
+{
+    const auto [absoluteX, absoluteY] = GetAbsolutePoint();
+    const auto [width, height]        = GetSize();
+
+    POINT centerPoint{.x = absoluteX, .y = absoluteY};
+    if (IsCenterHorizontal())
+    {
+        centerPoint.x += (width - childSize.cx) / 2;
+    }
+    if (IsCenterVertical())
+    {
+        centerPoint.y += (height - childSize.cy) / 2;
+    }
+    return centerPoint;
+}
+
+bool CenterWrapper::IsCenterHorizontal() const
+{
+    return ReflectFields->CenterType & CENTER_HORIZONTAL;
+}
+
+bool CenterWrapper::IsCenterVertical() const
+{
+    return ReflectFields->CenterType & CENTER_VERTICAL;
+}
+
 void CenterWrapper::OnPlacementChange()
 {
     EditablePlacementUIComponent::OnPlacementChange();
-    for (int i = 0; i < transform->GetChildCount(); ++i)
-    {
-        const Transform* child      = transform->GetChild(i);
-        GameObject&      gameObject = child->gameObject;
-        for (int j = 0; j < gameObject.GetComponentCount(); ++j)
-        {
-            if (PlacementUIComponent* component = gameObject.GetComponentAtIndex<PlacementUIComponent>(j))
-            {
-                SIZE  childSize       = component->GetSize();
-                POINT point           = ReflectFields->Basefields.get().Basefields.get().Point;
-                SIZE  size            = ReflectFields->Basefields.get().Basefields.get().Size;
-                auto [scopeX, scopeY] = ReflectFields->Basefields.get().Basefields.get().ScopePoint;
-                point.x += scopeX;
-                point.y += scopeY;
-                switch (ReflectFields->CenterType)
-                {
-                case CenterType::BOTH:
-                    point.x += (size.cx - childSize.cx) / 2;
-                    point.y += (size.cy - childSize.cy) / 2;
-                    break;
-                case CenterType::HORIZONTAL:
-                    point.x += (size.cx - childSize.cx) / 2;
-                    break;
-                case CenterType::VERTICAL:
-                    point.y += (size.cy - childSize.cy) / 2;
-                    break;
-                }
-                component->SetScopePlacement(point, size);
-            }
-        }
-    }
+
+    AssignChild();
 }
 
-void CenterWrapper::ImGuiDrawPropertysEvent()
+void CenterWrapper::OnAttachChild(GameObject* childGameObject)
 {
-    EditablePlacementUIComponent::ImGuiDrawPropertysEvent();
-    static const char* items[]      = {"Both", "Horizontal", "Vertical"};
-    int                currentIndex = static_cast<int>(ReflectFields->CenterType);
-    if (ImGui::Combo("##CenterType", &currentIndex, items, IM_ARRAYSIZE(items)))
-    {
-        ReflectFields->CenterType = static_cast<CenterType>(currentIndex);
-        OnPlacementChange();
-    }
-    ImGui::SameLine();
-    ImGui::Text("Center Type");
+    EditablePlacementUIComponent::OnAttachChild(childGameObject);
+
+    AssignChild();
+}
+
+void CenterWrapper::AssignChild() const
+{
+    Transform&                         transform  = this->transform;
+    std::vector<PlacementUIComponent*> components = FindChildComponents<PlacementUIComponent>()(transform);
+    std::ranges::for_each(components, [this](PlacementUIComponent* component) {
+        const SIZE  childSize = component->GetSize();
+        const POINT point     = GetCenterPoint(childSize);
+        const SIZE  size      = GetSize();
+        component->SetScopePlacement(point, size);
+    });
 }
--- Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Wrappers/Center/CenterWrapper.h
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Wrappers/Center/CenterWrapper.h
@@ -5,19 +5,50 @@ class CenterWrapper : public EditablePlacementUIComponent
 {
     USING_PROPERTY(CenterWrapper)
 
-    enum class CenterType : unsigned char
+    using CenterType = unsigned char;
+
+    static constexpr CenterType CENTER_HORIZONTAL = 0b01;
+    static constexpr CenterType CENTER_VERTICAL   = 0b10;
+
+public:
+    REFLECT_PROPERTY(Horizontal, Vertical)
+
+    GETTER(bool, Horizontal) { return IsCenterHorizontal(); }
+    SETTER(bool, Horizontal)
     {
-        BOTH,
-        HORIZONTAL,
-        VERTICAL,
-    };
+        if (value)
+            ReflectFields->CenterType |= CENTER_HORIZONTAL;
+        else
+            ReflectFields->CenterType &= ~CENTER_HORIZONTAL;
+        OnPlacementChange();
+    }
+    PROPERTY(Horizontal)
+
+    GETTER(bool, Vertical) { return IsCenterVertical(); }
+    SETTER(bool, Vertical)
+    {
+        if (value)
+            ReflectFields->CenterType |= CENTER_VERTICAL;
+        else
+            ReflectFields->CenterType &= ~CENTER_VERTICAL;
+        OnPlacementChange();
+    }
+    PROPERTY(Vertical)
+
+public:
+    POINT GetCenterPoint(SIZE childSize) const;
+    bool  IsCenterHorizontal() const;
+    bool  IsCenterVertical() const;
 
 protected:
     void OnPlacementChange() override;
-    void ImGuiDrawPropertysEvent() override;
+    void OnAttachChild(GameObject* childGameObject) override;
+
+private:
+    void AssignChild() const;
 
 protected:
     REFLECT_FIELDS_BEGIN(EditablePlacementUIComponent)
-    CenterType CenterType = CenterType::BOTH;
+    CenterType CenterType = CENTER_HORIZONTAL | CENTER_VERTICAL;
     REFLECT_FIELDS_END(CenterWrapper)
 };
