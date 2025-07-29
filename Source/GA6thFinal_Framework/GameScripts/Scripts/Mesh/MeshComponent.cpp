3c5072157c3059d16e8396bb67258ae5b37ef050
bmbany <bmbany@naver.com>
2025-07-29 10:59:05 +0900

    [FIX] 예외처리 추가

--- Source/GA6thFinal_Framework/GameScripts/Scripts/Mesh/MeshComponent.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/Mesh/MeshComponent.cpp
@@ -180,6 +180,10 @@ void MeshComponent::InitMaterial()
 
     for (size_t i = 0; i < meshCount; i++)
     {
+        if (ReflectFields->CustomDepth.size() <= i)
+        {
+            continue;
+        }
         Renderer->OnCustomDepth(ReflectFields->CustomDepth[i], (UINT)i);
 
         //materials[i].ShadingModel = (Material::ShadingModelType)ReflectFields->ShadingModel[i];
