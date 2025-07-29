6eefed1e8e39fba162a04cc1df94b3c3af58e47a
bmbany <bmbany@naver.com>
2025-07-29 18:24:32 +0900

    [FEATURE] 렌더 패스 데이터 직렬화 기능 추가
    `ShadowPassProperty`의 직렬화 및 역직렬화 기능을 추가하고,
    렌더 패스 속성을 파일에 저장하고 로드하는 기능을 구현했습니다.
    UI에 데이터 저장 및 로드 버튼을 추가하였으며,
    `OnFrameRender` 함수의 접근 제어자를 변경했습니다.

--- Source/GA6thFinal_Framework/GameEngine/Source/Editor/Tool/RenderPassData/EditorRenderPassData.cpp
+++ Source/GA6thFinal_Framework/GameEngine/Source/Editor/Tool/RenderPassData/EditorRenderPassData.cpp
@@ -3,7 +3,6 @@
 
 EditorRenderPassData::EditorRenderPassData()
 {
-    //SetVisible(false);
     SetLabel("RenderPassData");
     SetDockLayout(ImGuiDir_Right);
 }
@@ -12,6 +11,117 @@ EditorRenderPassData::~EditorRenderPassData()
 {
 }
 
+// ShadowPassProperty를 문자열로 변환
+void SerializeShadowProperty(std::ostream& os, const ShadowPassProperty& prop)
+{
+    os << "        Type = ShadowPassProperty\n";
+    os << "        NearPlane = " << prop.NearPlane << "\n";
+    os << "        FarPlane = " << prop.FarPlane << "\n";
+    os << "        SplitFactor = " << prop.SplitFactor << "\n";
+}
+
+// 문자열에서 ShadowPassProperty를 복원
+void DeserializeShadowProperty(std::istream& is, ShadowPassProperty& prop)
+{
+    std::string line, key, equals;
+    while (std::getline(is, line) && line.find('}') == std::string::npos)
+    {
+        std::stringstream ss(line);
+        ss >> key >> equals;
+        if (key == "NearPlane") ss >> prop.NearPlane;
+        else if (key == "FarPlane") ss >> prop.FarPlane;
+        else if (key == "SplitFactor") ss >> prop.SplitFactor;
+    }
+}
+
+void SaveData(const std::string& filePath)
+{
+    std::filesystem::path path(filePath);   
+    
+    path.replace_extension(".inl");    
+    
+    std::ofstream outFile(path);
+
+    if (!outFile.is_open())
+    {
+        return;
+    }
+
+    auto& renderPassProperties = UmGraphics.GetRenderPassProperties();
+
+    for (const auto& [sceneName, properties] : renderPassProperties)
+    {
+        outFile << "Scene " << sceneName << "\n{\n";
+        for (const auto& [passName, pair] : properties)
+        {
+            outFile << "    Pass " << passName << "\n    {\n";
+            const auto& property = pair.first;
+
+            if (property.type() == typeid(ShadowPassProperty))
+            {
+                SerializeShadowProperty(outFile, std::any_cast<const ShadowPassProperty&>(property));
+            }
+            // else if (property.type() == typeid(AnotherProperty)) { ... } // 다른 타입 추가
+
+            outFile << "    }\n";
+        }
+        outFile << "}\n\n";
+    }
+}
+
+void LoadData(const std::string& filePath)
+{
+    std::ifstream inFile(filePath);
+    if (!inFile.is_open())
+    {
+        return;
+    }
+
+    auto& renderPassProperties = UmGraphics.GetRenderPassProperties();
+    std::string line, keyword, name;
+
+    while (std::getline(inFile, line))
+    {
+        std::stringstream ss(line);
+        ss >> keyword >> name;
+
+        if (keyword == "Scene")
+        {
+            std::string currentSceneName = name;
+            std::getline(inFile, line); // '{' 라인 스킵
+
+            while (std::getline(inFile, line) && line.find('}') == std::string::npos)
+            {
+                std::stringstream pass_ss(line);
+                pass_ss >> keyword >> name;
+
+                if (keyword == "Pass")
+                {
+                    std::string currentPassName = name;
+                    std::getline(inFile, line); // '{' 라인 스킵
+
+                    // Pass에 해당하는 속성 찾기
+                    if (renderPassProperties.count(currentSceneName) && renderPassProperties[currentSceneName].count(currentPassName))
+                    {
+                        auto& property = renderPassProperties[currentSceneName][currentPassName].first;
+
+                        // 타입 확인 및 복원
+                        std::string typeLine;
+                        std::getline(inFile, typeLine);
+                        std::stringstream type_ss(typeLine);
+                        type_ss >> keyword >> name >> name; // "Type = ShadowPassProperty"
+
+                        if (name == "ShadowPassProperty" && property.type() == typeid(ShadowPassProperty))
+                        {
+                            DeserializeShadowProperty(inFile, std::any_cast<ShadowPassProperty&>(property));
+                        }
+                        // else if (name == "AnotherProperty") { ... }
+                    }
+                }
+            }
+        }
+    }
+}
 void EditShadowProperty(std::any& property)
 {
     auto& shadowProps = std::any_cast<ShadowPassProperty&>(property);
@@ -24,6 +134,7 @@ void EditShadowProperty(std::any& property)
 void EditorRenderPassData::OnFrameRender()
 {
     auto& renderPassProperties = UmGraphics.GetRenderPassProperties();
+
     for (auto& [sceneName, properties] : renderPassProperties)
     {
         if (ImGui::TreeNodeEx(sceneName.c_str()))
@@ -64,6 +175,27 @@ void EditorRenderPassData::OnFrameRender()
                 }
             }
             ImGui::TreePop();
+        }
+    }
+    if (ImGui::Button("SaveData"))
+    {
+        std::vector<std::pair<LPCWSTR, LPCWSTR>> filters = {{L"Save Files (*.inl)", L"*.inl\0\0"}};
+        File::Path exportPath;
+        bool result = File::ShowSaveFileDialog(NULL, L"Export Render Pass Data", L"", L"data.inl", filters, exportPath);
+        if (result)
+        {
+            SaveData(exportPath.string());
+        }
+    }
+    ImGui::SameLine();
+    if (ImGui::Button("LoadData"))
+    {
+        std::vector<std::pair<LPCWSTR, LPCWSTR>> filters = {{L"Load Files (*.inl)", L"*.inl\0\0"}};
+        File::Path importPath;
+        bool result = File::ShowOpenFileDialog(NULL, L"Import Render Pass Data", L"", filters, importPath);
+        if (result)
+        {
+            LoadData(importPath.string());
         }
     }
 }
