64b3d411dacc758627de853b092ff9606f3f5485
Do_Suha <weqa123@naver.com>
2025-07-30 20:00:13 +0900

    [FEATURE/VG-180] TurnQueueView 클래스 추가
    게임의 턴 큐를 시각적으로 표현하는 `TurnQueueView` 클래스와 관련된 소스 파일을 추가했습니다.
    `GameScripts.vcxproj` 파일에 해당 파일들을 포함하도록 수정하였으며, 턴 큐의 프레임과 초상화를 초기화하고 관리하는 메서드를 구현했습니다.
    로깅 기능을 추가하여 이미지 요소를 찾지 못했을 때 경고 메시지를 출력합니다.

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Views/TurnQueue/TurnQueueView.cpp
@@ -0,0 +1,75 @@
+﻿#include "pchScripts.h"
+#include "TurnQueueView.h"
+
+#include "ViewModels/TurnQueue/TurnQueueViewModel.h"
+#include "UI/Elements/Image/ImageElement.h"
+
+TurnQueueView::TurnQueueView() = default;
+
+void TurnQueueView::Reset()
+{
+    Component::Reset();
+
+    InitializeFramesAndPortraits();
+
+    UmTime.Invoke(this, 0, [this] {
+        UmWatcher.Watch<TurnQueueViewModel, std::vector<TurnUIData>>(
+            "TestValue", [this](const std::vector<TurnUIData>& value) {
+                const size_t dataSize = value.size();
+                for (size_t i = 0; i < 7; ++i)
+                {
+                    if (i < dataSize)
+                    {
+                        _turnQueueFrames[i]->SetImage(value[i].Frame);
+                        _turnQueuePortraits[i]->SetImage(value[i].ActorPortrait);
+                    }
+                    else
+                    {
+                        _turnQueueFrames[i]->SetImage(File::GuidRef());
+                        _turnQueuePortraits[i]->SetImage(File::GuidRef());
+                    }
+                }
+            });
+    });
+}
+
+void TurnQueueView::FindImageElementWithTag(const std::string& tag, std::array<ImageElement*, 7>& elements,
+                                            const size_t index)
+{
+    if (const std::weak_ptr<GameObject> taggedObject = GameObject::FindWithTag(tag); taggedObject.expired())
+    {
+        UmLogger.Log(LogLevel::LEVEL_WARNING, tag + " GameObject를 찾을 수 없습니다.");
+    }
+    else
+    {
+        const auto    object       = taggedObject.lock();
+        ImageElement* imageElement = object->GetComponent<ImageElement>();
+        if (nullptr == imageElement)
+        {
+            UmLogger.Log(LogLevel::LEVEL_WARNING, tag + " GameObject에 ImageElement가 없습니다.");
+            return;
+        }
+        elements[index] = imageElement;
+    }
+}
+
+void TurnQueueView::FindFramesWithTag(const std::string& tag, const size_t index)
+{
+    FindImageElementWithTag(tag + " Frame", _turnQueueFrames, index);
+}
+
+void TurnQueueView::FindPortraitsWithTag(const std::string& tag, const size_t index)
+{
+    FindImageElementWithTag(tag + " Portrait", _turnQueuePortraits, index);
+}
+
+void TurnQueueView::InitializeFramesAndPortraits()
+{
+    FindFramesWithTag("1st Turn", 0);
+    FindFramesWithTag("2nd Turn", 1);
+    FindFramesWithTag("3rd Turn", 2);
+    for (size_t i = 3; i < 7; ++i)
+    {
+        FindFramesWithTag(std::to_string(i + 1) + "th Turn", i);
+    }
+}
---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Views/TurnQueue/TurnQueueView.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+class ImageElement;
+
+class TurnQueueView : public Component
+{
+    USING_PROPERTY(Component)
+
+public:
+    TurnQueueView();
+
+protected:
+    void Reset() override;
+
+private:
+    static void FindImageElementWithTag(const std::string& tag, std::array<ImageElement*, 7>& elements, size_t index);
+    void        FindFramesWithTag(const std::string& tag, size_t index);
+    void        FindPortraitsWithTag(const std::string& tag, size_t index);
+    void        InitializeFramesAndPortraits();
+
+protected:
+    REFLECT_FIELDS_BEGIN(Component)
+    REFLECT_FIELDS_END(TurnQueueView)
+
+private:
+    std::array<ImageElement*, 7> _turnQueueFrames;
+    std::array<ImageElement*, 7> _turnQueuePortraits;
+};
