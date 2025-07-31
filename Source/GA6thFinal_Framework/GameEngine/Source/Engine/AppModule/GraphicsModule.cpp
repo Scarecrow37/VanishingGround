e6f965ebd2509d31bc53530cdb71ce624c5f6530
bmbany <bmbany@naver.com>
2025-07-31 10:57:33 +0900

    [FEATURE/VG-309] 렌더 패스 데이터 저장 및 로드 기능 추가
    `EditorRenderPassData` 클래스의 파일 저장 및 로드 메서드를 새로운 함수로 변경하고, 관련된 직렬화 및 역직렬화 기능을 `RenderPassDataHelper`에 추가했습니다. `RendererFileEvent`에서 파일 로드 시 새로운 함수를 사용하도록 수정하였으며, 코드 정리를 통해 가독성을 향상시켰습니다.

--- Source/GA6thFinal_Framework/GameEngine/Source/Engine/AppModule/GraphicsModule.cpp
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/AppModule/GraphicsModule.cpp
@@ -43,7 +43,7 @@ void GraphicsModule::PreInitialize()
     else
     {
         UmGraphics.SetCurrentScene("Game");
-    }
+    }    
 }
 
 void GraphicsModule::ModuleInitialize()
--- Source/GA6thFinal_Framework/GameEngine/Source/Engine/GraphicsCore/RendererFileEvent.cpp
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/GraphicsCore/RendererFileEvent.cpp
@@ -1,18 +1,11 @@
 ﻿#include "pch.h"
 #include "RendererFileEvent.h"
+#include "Engine/GraphicsCore/RenderPassDataHelper.h"
 
-void RendererFileEvent::OnFileRegistered(const File::Path& path)
+void RendererFileEvent::OnPostRequestedLoad()
 {
-    // 추후 에디터 실행 시 리소스를 로드 한다면 여기에 추가
-}
+    auto filePath = UmFileSystem.GetBuildSettingPath();
+    filePath /= "GraphicsSetting.inl";
 
-void RendererFileEvent::OnFileModified(const File::Path& path)
-{
-    // 리소스 수정 시 이벤트
-}
-
-void RendererFileEvent::OnRequestedInspect(const File::Path& path)
-{
-    // 인스펙터에 보여줄 내용
-    // ex) 미리보기
+    LoadRenderPassData(filePath.string());
 }
