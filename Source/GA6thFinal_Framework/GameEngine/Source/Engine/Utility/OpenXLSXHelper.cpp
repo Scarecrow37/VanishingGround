feec7faef52c88f89fc63ab30382478a65b7f033
power12562 <kimsiwoo1005@gmail.com>
2025-07-31 12:40:46 +0900

    [FEATURE/VG-28] OpenXLSXHelper용 Find 함수 추가

---
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/Utility/OpenXLSXHelper.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+
+std::pair<unsigned int, unsigned int> OpenXLSXHelper::FindColumRowToData(const OpenXLSX::XLWorksheet& workSheet,
+                                                                         std::string_view             findData)
+{
+    unsigned int colum;
+    unsigned int row;
+    unsigned int columCount = workSheet.columnCount();
+    unsigned int rowCount = workSheet.rowCount();
+    for (colum = 1; colum <= columCount; ++colum)
+    {
+        for (row = 1; row <= rowCount; ++row)
+        {
+            auto dataValue = workSheet.cell(colum, row);
+            if (dataValue)
+            {
+                std::string data = dataValue.getString();
+                if (data == findData)
+                {
+                    return std::pair<unsigned int, unsigned int>(colum, row);
+                }
+            }
+        }
+    }
+    colum = FAIL_COLUM;
+    row   = FAIL_ROW;
+    return std::pair<unsigned int, unsigned int>(colum, row);
+}
--- Source/GA6thFinal_Framework/GameEngine/Source/Engine/Utility/OpenXLSXHelper.h
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/Utility/OpenXLSXHelper.h
@@ -16,5 +16,26 @@
 
 namespace OpenXLSXHelper
 {
+    using Colum = unsigned int;
+    using Row   = unsigned int;
 
+    constexpr Colum FAIL_COLUM = std::numeric_limits<unsigned int>::max();
+    constexpr Row   FAIL_ROW   = std::numeric_limits<unsigned int>::max();
+
+    /// <summary>
+    /// Find 함수 성공 여부를 체크합니다.
+    /// </summary>
+    inline bool IsFindSuccess(const std::pair<Colum, Row>& findValue)
+    {
+        bool result = findValue.first != FAIL_COLUM || findValue.second != FAIL_ROW;
+        return result;
+    }
+
+    /// <summary>
+    /// 워크시트에 해당 데이터가 존재하면 첫번째로 만난 Colum, Row 인덱스를 반환합니다.
+    /// </summary>
+    /// <param name="workSheet :">찾을 워크시트</param>
+    /// <param name="findData :">찾을 데이터</param>
+    /// <returns>실패시 unsigend int max를 반환합니다.</returns>
+    std::pair<Colum, Row> FindColumRowToData(const OpenXLSX::XLWorksheet& workSheet, std::string_view findData);
 }
