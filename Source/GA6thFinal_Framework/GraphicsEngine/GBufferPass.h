299cb61123826674dacbf347d858991f71168b7d
bmbany <bmbany@naver.com>
2025-08-05 16:36:49 +0900

    [FEATURE/VG-309] G Buffer Pass 프로퍼티 추가

--- Source/GA6thFinal_Framework/GraphicsEngine/GBufferPass.h
+++ Source/GA6thFinal_Framework/GraphicsEngine/GBufferPass.h
@@ -27,6 +27,7 @@ public:
 
 public:
     void Initialize(RenderScene* ownerScene, RenderTechnique* ownerTechnique, ID3D12GraphicsCommandList* commandList) override;
+    void AddRenderPassDatas(std::string_view sceneName) override;
     void Update(ID3D12GraphicsCommandList* commadList) override;
     void Begin(ID3D12GraphicsCommandList* commandList) override;
     void Draw(ID3D12GraphicsCommandList* commandList) override;
@@ -40,6 +41,8 @@ private:
     std::vector<ComPtr<ID3D12PipelineState>>                      _psos;
     std::array<D3D12_CPU_DESCRIPTOR_HANDLE, GBuffer::GBUFFER_END> _gBufferHandles;
     std::vector<RenderData>                                       _renderDatas[MeshType::END];
+
+    SharedResource<RenderTarget> _gBufferRenderTargets[4];
 
     FX<GE::VS::STATIC_FR, GE::PS::GBUFFER>                        _fxStaticMesh;
     FX<GE::VS::SKELETAL_FR, GE::PS::GBUFFER>                      _fxSkeletalMesh;
