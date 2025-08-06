a4dda68c8f3b20be31b8bc586a2a4a8dbb2ba969
power12562 <kimsiwoo1005@gmail.com>
2025-08-06 20:35:16 +0900

    [FEATURE/VG-28] GetValidRowCount, GetValidColumnCount 추가 및 ParseSheetWithColumnKeys 최적화

--- Source/GA6thFinal_Framework/GameEngine/Source/Engine/Utility/OpenXLSXHelper.cpp
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/Utility/OpenXLSXHelper.cpp
@@ -1,44 +1,62 @@
 ﻿#include "pch.h"
 
-std::pair<unsigned int, unsigned int> OpenXLSXHelper::FindRowColumnToData(const OpenXLSX::XLWorksheet& workSheet,
-                                                                         std::string_view             findData)
+using namespace OpenXLSXHelper;
+
+std::pair<Row, Column> OpenXLSXHelper::FindRowColumnToData(const OpenXLSX::XLWorksheet& workSheet, std::string_view findData)
 {
     unsigned int row;
+    unsigned int rowCount = workSheet.rowCount();
     unsigned int column;
     unsigned int columnCount = workSheet.columnCount();
-    unsigned int rowCount = workSheet.rowCount();
-    for (column = 1; column <= columnCount; ++column)
+
+    int emptyCount = 0;
+    for (row = 1; row <= rowCount; ++row)
     {
-        for (row = 1; row <= rowCount; ++row)
+        constexpr int emptyCountMax = 3;
+        bool isEmpty = true;
+        for (column = 1; column <= columnCount; ++column)
         {
             auto dataValue = workSheet.cell(row, column);
             if (dataValue)
             {
                 std::string data = dataValue.getString();
-                if (data == findData)
+                if (false == data.empty())
                 {
-                    return std::pair<unsigned int, unsigned int>(row, column);
+                    if (data == findData)
+                    {
+                        return std::pair<unsigned int, unsigned int>(row, column);
+                    }
+                    isEmpty = false;
                 }
             }
         }
+
+        if (isEmpty)
+        {
+            if (emptyCountMax <= ++emptyCount)
+            {
+                break;
+            }
+        }
     }
-    row   = FAIL_ROW;
+    row    = FAIL_ROW;
     column = FAIL_COLUMN;
     return std::pair<unsigned int, unsigned int>(row, column);
 }
 
-std::vector<std::pair<std::string, std::vector<std::string>>> OpenXLSXHelper::ParseSheetWithColumnKeys(const OpenXLSX::XLWorksheet& workSheet, Row keyRowIndex)
+std::vector<std::pair<std::string, std::vector<std::string>>> OpenXLSXHelper::ParseSheetWithColumnKeys(const OpenXLSX::XLWorksheet& workSheet, Row keyRowIndex, unsigned int threshold)
 {
     std::vector<std::pair<std::string, std::vector<std::string>>> sheetDatas;
 
-    unsigned int rowCount = workSheet.rowCount();
+    unsigned int rowCount = GetValidRowCount(workSheet, threshold);
     unsigned int rowStart = keyRowIndex + 1;
     if (rowStart <= rowCount)
     {
-        unsigned int columnCount = workSheet.columnCount();
+        unsigned int columnCount = GetValidColumnCount(workSheet, threshold);
         sheetDatas.reserve(columnCount);
         for (unsigned int column = 1; column <= columnCount; ++column)
         {
+            int  emptyRowCount = 0;
             auto keyValue = workSheet.cell(keyRowIndex, column);
             if (keyValue)
             {
@@ -48,11 +66,22 @@ std::vector<std::pair<std::string, std::vector<std::string>>> OpenXLSXHelper::Pa
                     std::vector<std::string> datas(size_t(rowCount - rowStart + 1));
                     for (unsigned int row = rowStart; row <= rowCount; ++row)
                     {
+                        constexpr int emptyRowCountMax = 1000;
                         auto dataValue = workSheet.cell(row, column);
                         if (dataValue)
                         {
                             unsigned int index = row - rowStart;
-                            datas[index]       = dataValue.getString();
+                            std::string  data  = dataValue.getString();
+                            datas[index]       = std::move(data);
+
+                            // 빈 데이터가 연속될시 해당 Column 스킵.
+                            if (false == datas[index].empty())
+                            {
+                                if (emptyRowCountMax <= ++emptyRowCount)
+                                {
+                                    break;
+                                }
+                            }
                         }
                     }
                     sheetDatas.emplace_back(key, datas);
@@ -61,4 +90,90 @@ std::vector<std::pair<std::string, std::vector<std::string>>> OpenXLSXHelper::Pa
         }
     }       
     return sheetDatas;
+}
+
+Row OpenXLSXHelper::GetValidRowCount(const OpenXLSX::XLWorksheet& workSheet, unsigned int threshold)
+{
+    unsigned int row;
+    unsigned int rowCount = workSheet.rowCount();
+    unsigned int column;
+    unsigned int columnCount = workSheet.columnCount();
+
+    unsigned int emptyRowCount = 0;
+    for (row = 1; row <= rowCount; ++row)
+    {
+        bool isEmpty = true;
+        unsigned int emptyColumnCount = 0;
+        for (column = 1; column <= columnCount; ++column)
+        {
+            auto dataValue = workSheet.cell(row, column);
+            if (dataValue)
+            {
+                std::string data = dataValue.getString();
+                if (false == data.empty())
+                {
+                    isEmpty = false;
+                }
+                else
+                {
+                    if (threshold < ++emptyColumnCount)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (isEmpty)
+        {
+            if (threshold < ++emptyRowCount)
+            {
+                return row;
+            }
+        }
+    }
+    return rowCount;
+}
+
+Column OpenXLSXHelper::GetValidColumnCount(const OpenXLSX::XLWorksheet& workSheet, unsigned int threshold)
+{
+    unsigned int row;
+    unsigned int rowCount = workSheet.rowCount();
+    unsigned int column;
+    unsigned int columnCount = workSheet.columnCount();
+
+    unsigned int emptyColumnCount = 0;
+    for (column = 1; column <= columnCount; ++column)
+    {
+        bool isEmpty = true;
+        unsigned int emptyRowCount = 0;
+        for (row = 1; row <= rowCount; ++row)
+        {
+            auto dataValue = workSheet.cell(row, column);
+            if (dataValue)
+            {
+                std::string data = dataValue.getString();
+                if (false == data.empty())
+                {
+                    isEmpty = false;
+                }
+                else
+                {
+                    if (threshold < ++emptyRowCount)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (isEmpty)
+        {
+            if (threshold < ++emptyColumnCount)
+            {
+                return column;
+            }
+        }
+    }
+    return columnCount;
 }
