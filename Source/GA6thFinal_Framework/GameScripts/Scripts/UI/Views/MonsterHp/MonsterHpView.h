3a89b79c3b9a09b91eee178c779421f658fd1482
Do_Suha <weqa123@naver.com>
2025-08-06 13:48:01 +0900

    [FEATURE/VG-180] 몬스터 HP UI 요소 및 뷰 모델 추가
    GameScripts.vcxproj에 MonsterHpView.h와 MonsterHpView.cpp 파일을 추가하여 몬스터의 HP를 표시하는 UI 요소를 구현했습니다.
    EnemyAction 관련 소스 파일들이 GameScripts.vcxproj.filters에 추가되어 적 행동 시스템이 확장되었습니다.
    HpViewModel.h와 HpViewModel.cpp 파일이 추가되어 HP 모델과 관련된 뷰 모델 로직이 구현되었습니다.
    UmScriptsExport.h에 MonsterHpView 컴포넌트를 추가하여 전체 시스템에 통합했습니다.
    MonsterHpView 클래스는 HP 텍스트 요소를 찾고 업데이트하는 기능을 포함하며, HpViewModel 클래스는 최대 HP를 기준으로 HP 값을 변환하는 기능을 제공합니다.

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Views/MonsterHp/MonsterHpView.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+class TextElement;
+
+class MonsterHpView : public Component
+{
+    USING_PROPERTY(MonsterHpView)
+
+public:
+    MonsterHpView();
+
+public:
+    void Watch(const std::string& key) const;
+
+protected:
+    void Awake() override;
+
+private:
+    void FindTextElement();
+
+protected:
+    REFLECT_FIELDS_BEGIN(Component)
+    REFLECT_FIELDS_END(MonsterHpView)
+
+private:
+    TextElement* _hpTextElement;
+};
---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/ViewModels/Hp/HpViewModel.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+class HpViewModel final : public MVVM::ViewModel<int, float>
+{
+public:
+    explicit HpViewModel(MVVM::Model<int>& model, int maxHp);
+
+protected:
+    float Convert(const int& value) override;
+
+private:
+    int _maxHp;
+};
