f0c9942e779b4ed8ff47e966a017ee21fddf535a
power12562 <kimsiwoo1005@gmail.com>
2025-08-11 12:26:50 +0900

    [REFACTOR/VG-316] 턴 액션을 위한 enum 모음 분리

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnAction/TurnActionEnums.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+/// <summary>
+/// 액션을 수행할 대상입니다.
+/// </summary>
+enum class TurnTarget
+{
+    SELF,           // 마지막으로 공격한 캐릭터
+    PLAYER,         // 플레이어
+    ENEMY,          // 마지막으로 공격당한 적
+    ALL_ENEMIES,    // 모든 적
+    ALL             // 모든 캐릭터
+};
