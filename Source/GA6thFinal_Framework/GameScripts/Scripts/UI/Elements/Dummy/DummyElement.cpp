5e1eda5789f212b307b89aafce09edc33c415ba6
Do_Suha <weqa123@naver.com>
2025-08-12 12:00:49 +0900

    [FEATURE/VG-181] DummyElement 및 DummyWrapper 개선
    DummyElement 클래스에 MeasureOverride 및 ArrangeOverride 메서드를 추가하여 크기 측정 및 배치 기능을 구현했습니다.
    DummyElement의 상속 클래스를 EditablePlacementUIComponent에서 UIComponent로 변경하였고,
    DummyWrapper의 MeasureOverride 및 ArrangeOverride 메서드의 로직을 수정하여 자식 요소의 가용 크기를 기반으로 측정하고 배치하도록 개선했습니다.

--- Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Elements/Dummy/DummyElement.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Elements/Dummy/DummyElement.cpp
@@ -2,3 +2,16 @@
 #include "DummyElement.h"
 
 DummyElement::DummyElement() = default;
+
+SIZE DummyElement::MeasureOverride(const SIZE availableSize)
+{
+    const SIZE desiredSize = MinSize()(availableSize, _requestedSize);
+    return desiredSize;
+}
+
+SIZE DummyElement::ArrangeOverride(const SIZE finalSize)
+{
+    const SIZE desiredSize = DesiredSize;
+    const SIZE actualSize  = MinSize()(finalSize, desiredSize);
+    return actualSize;
+}
--- Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Wrappers/Dummy/DummyWrapper.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Wrappers/Dummy/DummyWrapper.cpp
@@ -5,26 +5,26 @@ DummyWrapper::DummyWrapper() = default;
 
 SIZE DummyWrapper::MeasureOverride(const SIZE availableSize)
 {
+    const SIZE desiredSize = MinSize()(availableSize, _requestedSize);
+    const SIZE childAvailableSize = desiredSize - _padding.Size();
     if (const std::vector<UIComponent*> children = Children; children.size() > 0)
     {
-        children[0]->Measure(availableSize);
+        children[0]->Measure(childAvailableSize);
     }
 
-    const SIZE size = {.cx = std::min(availableSize.cx, _requestedSize.cx),
-                       .cy = std::min(availableSize.cy, _requestedSize.cy)};
-    return size;
+    return desiredSize;
 }
 
 SIZE DummyWrapper::ArrangeOverride(const SIZE finalSize)
 {
     const SIZE desiredSize = DesiredSize;
-    const SIZE size = {.cx = std::min(finalSize.cx, desiredSize.cx), .cy = std::min(finalSize.cy, desiredSize.cy)};
-
+    const SIZE actualSize = MinSize()(finalSize, desiredSize);
+    const SIZE childAvailableSize = actualSize - _padding.Size();
     if (const std::vector<UIComponent*> children = Children; children.size() > 0)
     {
-        const POINT absolutePosition = AbsolutePosition;
-        children[0]->Arrange(absolutePosition, size);
+        const POINT childPoint = AbsoluteChildPosition;
+        children[0]->Arrange(childPoint, childAvailableSize);
     }
 
-    return size;
+    return actualSize;
 }
