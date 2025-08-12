cd62f4cfd1775c4d9ff1c3f85d3c2f3559a48351
Do_Suha <weqa123@naver.com>
2025-08-12 10:46:30 +0900

    [FEATURE/VG-181] UIComponent 기능 확장 및 디버깅 추가
    - UIComponent.cpp와 UIComponent.h에 여러 연산자 오버로딩 함수와 메서드 추가
    - POINT와 SIZE 구조체에 대한 비교 및 산술 연산자 오버로딩 구현
    - MinSize 구조체 추가로 최소 크기 반환 기능 구현
    - UIComponent 클래스에 디버그 모드 제어를 위한 _isDebug 변수 추가
    - UIComponent의 동작을 제어하는 다양한 메서드 추가
    - 디버그 정보를 화면에 그리는 DrawDebug 및 DrawQuad 구조체 추가
    - UIComponent 속성 관리 용이성을 위한 접근자 및 설정자 메서드 추가
    - 전반적으로 UI 컴포넌트의 기능과 디버깅, 측정 관련 기능 강화

---
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/GameCore/Component/UIComponent.cpp
@@ -0,0 +1,254 @@
+﻿#include "pch.h"
+#include "UIComponent.h"
+
+bool UIComponent::_isDebug = false;
+
+bool operator==(const POINT& lhs, const POINT& rhs)
+{
+    return lhs.x == rhs.x && lhs.y == rhs.y;
+}
+
+bool operator!=(const POINT& lhs, const POINT& rhs)
+{
+    return !(lhs == rhs);
+}
+
+POINT operator+(const POINT& lhs, const POINT& rhs)
+{
+    return POINT{lhs.x + rhs.x, lhs.y + rhs.y};
+}
+
+POINT operator-(const POINT& lhs, const POINT& rhs)
+{
+    return POINT{lhs.x - rhs.x, lhs.y - rhs.y};
+}
+
+bool operator==(const SIZE& lhs, const SIZE& rhs)
+{
+    return lhs.cx == rhs.cx && lhs.cy == rhs.cy;
+}
+
+bool operator!=(const SIZE& lhs, const SIZE& rhs)
+{
+    return !(lhs == rhs);
+}
+
+SIZE operator+(const SIZE& lhs, const SIZE& rhs)
+{
+    return SIZE{lhs.cx + rhs.cx, lhs.cy + rhs.cy};
+}
+
+SIZE operator-(const SIZE& lhs, const SIZE& rhs)
+{
+    return SIZE{lhs.cx - rhs.cx, lhs.cy - rhs.cy};
+}
+
+SIZE MinSize::operator()(const SIZE& lhs, const SIZE& rhs) const
+{
+    return SIZE{.cx = std::min(lhs.cx, rhs.cx), .cy = std::min(lhs.cy, rhs.cy)};
+}
+
+UIComponent::UIComponent()
+    : Component(TYPE::UI), _requestedPoint{}, _requestedSize{}, _padding{}, _margin{}, _isMeasureDirty(false),
+      _isArrangeDirty(false)
+{
+}
+
+void UIComponent::OnAttachChild(GameObject* childGameObject)
+{
+    InvalidateMeasure();
+}
+
+void UIComponent::OnDetachParent(GameObject* previousParentGameObject)
+{
+    InvalidateMeasure();
+}
+
+void UIComponent::OnDrawDebugOverride()
+{
+    const POINT absolutePoint = AbsolutePosition;
+    const SIZE  size          = Size;
+
+    DrawQuad()(absolutePoint, size, 1, Colors::White);
+}
+
+void UIComponent::OnDrawDebugSelectedOverride()
+{
+    const POINT absolutePoint = AbsolutePosition;
+    const SIZE  size          = Size;
+
+    DrawQuad()(absolutePoint, size, 3, Colors::Yellow);
+}
+
+void UIComponent::ImGuiDrawPropertysEvent()
+{
+    Component::ImGuiDrawPropertysEvent();
+
+    if (ImGui::Button("Reset Placement"))
+    {
+        ResetPlacement();
+    }
+
+    if (ImGui::Button("Debug"))
+    {
+        _isDebug = !_isDebug;
+    }
+
+    if (_isDebug)
+    {
+        constexpr DrawDebug drawDebug;
+
+        auto [actualX, actualY] = ReflectFields->ActualPosition;
+        drawDebug("Actual Position", actualX, actualY);
+
+        auto [actualWidth, actualHeight] = ReflectFields->ActualSize;
+        drawDebug("Actual Size", actualWidth, actualHeight);
+
+        auto [offsetX, offsetY] = ReflectFields->Offset;
+        drawDebug("Offset", offsetX, offsetY);
+
+        auto [desiredWidth, desiredHeight] = ReflectFields->DesiredSize;
+        drawDebug("Desired Size", desiredWidth, desiredHeight);
+
+        auto [availableWidth, availableHeight] = ReflectFields->AvailableSize;
+        drawDebug("Available Size", availableWidth, availableHeight);
+    }
+}
+
+void UIComponent::DeserializedReflectEvent()
+{
+    Component::DeserializedReflectEvent();
+
+    _requestedPoint = ActualPosition;
+    _requestedSize  = ActualSize;
+    _margin         = MARGIN{ReflectFields->MarginLeft, ReflectFields->MarginTop, ReflectFields->MarginRight,
+                     ReflectFields->MarginBottom};
+    _padding        = PADDING{ReflectFields->PaddingLeft, ReflectFields->PaddingTop, ReflectFields->PaddingRight,
+                       ReflectFields->PaddingBottom};
+}
+
+void UIComponent::InvalidateMeasure()
+{
+    if (false == _isMeasureDirty)
+    {
+        UmUI.AddMeasureQueue(this);
+
+        if (UIComponent* parent = Parent; nullptr != parent)
+        {
+            parent->InvalidateMeasure();
+        }
+
+        _isMeasureDirty = true;
+    }
+}
+
+void UIComponent::InvalidateArrange()
+{
+    if (false == _isArrangeDirty)
+    {
+        UmUI.AddArrangeQueue(this);
+
+        _isArrangeDirty = true;
+    }
+}
+
+void UIComponent::Measure(const SIZE availableSize)
+{
+    ReflectFields->AvailableSize = availableSize;
+
+    const SIZE    desiredSize = ReflectFields->DesiredSize;
+
+    const SIZE overrideAvailableSize = availableSize - _margin.Size();
+    if (const SIZE newDesiredSize = MeasureOverride(overrideAvailableSize); desiredSize != newDesiredSize)
+    {
+        ReflectFields->DesiredSize = newDesiredSize;
+        InvalidateArrange();
+    }
+
+    _isMeasureDirty = false;
+}
+
+void UIComponent::Arrange(const POINT finalPosition, const SIZE finalSize)
+{
+    ReflectFields->Offset         = finalPosition;
+    ReflectFields->ActualPosition = _requestedPoint;
+    ReflectFields->ActualSize     = ArrangeOverride(finalSize);
+
+    _isArrangeDirty = false;
+}
+
+void UIComponent::Arrange()
+{
+    const SIZE finalSize      = ReflectFields->ActualSize;
+    ReflectFields->ActualSize = ArrangeOverride(finalSize);
+}
+
+void UIComponent::ResetPlacement()
+{
+    _requestedSize  = ReflectFields->AvailableSize;
+    InvalidateMeasure();
+    _requestedPoint = {.x = 0, .y = 0};
+    InvalidateArrange();
+}
+
+void UIComponent::OnDrawDebug()
+{
+    Component::OnDrawDebug();
+
+    if (const bool isEnable = EnableInHierarchy; isEnable)
+    {
+        OnDrawDebugOverride();
+    }
+}
+
+void UIComponent::OnDrawDebugSelected()
+{
+    Component::OnDrawDebugSelected();
+
+    if (const bool isEnable = EnableInHierarchy; isEnable)
+    {
+        OnDrawDebugSelectedOverride();
+    }
+}
+
+void DrawDebug::operator()(const char* label) const
+{
+    ImGui::SameLine();
+    ImGui::Text(label);
+}
+
+void DrawDebug::operator()(const char* label, const long x) const
+{
+    ImGui::Text("%d", x);
+    operator()(label);
+}
+
+void DrawDebug::operator()(const char* label, const long x, const long y) const
+{
+    ImGui::Text("%d x %d", x, y);
+    operator()(label);
+}
+
+void DrawQuad::operator()(POINT point, SIZE size, const int thickness, FXMVECTOR color) const {
+    const auto [x, y]          = point;
+    const auto [width, height] = size;
+
+    const POINT leftTop{.x = x, .y = y};
+    const POINT rightTop{.x = x + width, .y = y};
+    const POINT rightBottom{.x = x + width, .y = y + height};
+    const POINT leftBottom{.x = x, .y = y + height};
+
+    const int begin = 0 - thickness / 2;
+    const int end   = thickness / 2;
+
+    for (int i = begin; i <= end; ++i)
+    {
+        const XMFLOAT2 leftTopVector{static_cast<float>(leftTop.x + i), static_cast<float>(leftTop.y + i)};
+        const XMFLOAT2 rightTopVector{static_cast<float>(rightTop.x - i), static_cast<float>(rightTop.y + i)};
+        const XMFLOAT2 rightBottomVector{static_cast<float>(rightBottom.x - i),
+                                         static_cast<float>(rightBottom.y - i)};
+        const XMFLOAT2 leftBottomVector{static_cast<float>(leftBottom.x + i), static_cast<float>(leftBottom.y - i)};
+        UmGraphics.DebugDraw2D("Editor", XMLoadFloat2(&leftTopVector), XMLoadFloat2(&rightTopVector),
+                               XMLoadFloat2(&rightBottomVector), XMLoadFloat2(&leftBottomVector), color);
+    }
+}
---
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/GameCore/Component/UIComponent.h
@@ -0,0 +1,264 @@
+﻿#pragma once
+
+// POINT
+bool  operator==(const POINT& lhs, const POINT& rhs);
+bool  operator!=(const POINT& lhs, const POINT& rhs);
+POINT operator+(const POINT& lhs, const POINT& rhs);
+POINT operator-(const POINT& lhs, const POINT& rhs);
+
+// SIZE
+bool operator==(const SIZE& lhs, const SIZE& rhs);
+bool operator!=(const SIZE& lhs, const SIZE& rhs);
+SIZE operator+(const SIZE& lhs, const SIZE& rhs);
+SIZE operator-(const SIZE& lhs, const SIZE& rhs);
+
+struct MinSize
+{
+    SIZE operator()(const SIZE& lhs, const SIZE& rhs) const;
+};
+
+class UIComponent : public Component
+{
+    friend class Transform;
+    USING_PROPERTY(UIComponent)
+
+protected:
+    static bool _isDebug;
+
+public:
+    UIComponent();
+
+public:
+    REFLECT_PROPERTY(Point, Size, Padding, Margin)
+
+    GETTER(POINT, Point) { return ReflectFields->ActualPosition; }
+    SETTER(POINT, Point)
+    {
+        _requestedPoint = value;
+        InvalidateArrange();
+    }
+    PROPERTY(Point)
+
+    GETTER(SIZE, Size) { return ReflectFields->ActualSize; }
+    SETTER(SIZE, Size)
+    {
+        _requestedSize = value;
+        InvalidateMeasure();
+    }
+    PROPERTY(Size)
+
+    GETTER(PADDING, Padding) { return _padding; }
+    SETTER(PADDING, Padding)
+    {
+        _padding = value;
+        ReflectFields->PaddingLeft = value.left;
+        ReflectFields->PaddingTop  = value.top;
+        ReflectFields->PaddingRight = value.right;
+        ReflectFields->PaddingBottom = value.bottom;
+        InvalidateMeasure();
+    }
+    PROPERTY(Padding)
+
+    GETTER(MARGIN, Margin) { return _margin; }
+    SETTER(MARGIN, Margin)
+    {
+        _margin = value;
+        ReflectFields->MarginLeft = value.left;
+        ReflectFields->MarginTop  = value.top;
+        ReflectFields->MarginRight = value.right;
+        ReflectFields->MarginBottom = value.bottom;
+        InvalidateMeasure();
+    }
+    PROPERTY(Margin)
+
+
+    GETTER_ONLY(UIComponent*, Parent)
+    {
+        UIComponent*     parent    = nullptr;
+        const Transform&      transform = this->transform;
+        if (const Transform* parentTransform = transform.Parent; nullptr != parentTransform)
+        {
+            const GameObject& parentObject = parentTransform->gameObject;
+            parent                         = parentObject.GetComponentDynamic<UIComponent>();
+        }
+        return parent;
+    }
+    PROPERTY(Parent)
+
+    GETTER_ONLY(std::vector<UIComponent*>, Children)
+    {
+        std::vector<UIComponent*> children;
+        Transform&          transform = this->transform;
+        for (int i = 0; i < transform.GetChildCount(); ++i)
+        {
+            const Transform* child           = transform.GetChild(i);
+            GameObject&      gameObject      = child->gameObject;
+            std::vector<UIComponent*> childComponents = gameObject.GetComponents<UIComponent>();
+            std::ranges::move(childComponents, std::back_inserter(children));
+        }
+        return children;
+    }
+    PROPERTY(Children)
+
+    GETTER_ONLY(POINT, AbsolutePosition)
+    {
+        const POINT offset           = ReflectFields->Offset;
+        const POINT leftTopMargin    = _margin.LeftTop();
+        const POINT actualPosition   = ReflectFields->ActualPosition;
+        const POINT absolutePosition = offset + leftTopMargin + actualPosition;
+        return absolutePosition;
+    }
+    PROPERTY(AbsolutePosition)
+
+    GETTER_ONLY(POINT, AbsoluteChildPosition)
+    {
+        const POINT absolutePosition      = AbsolutePosition;
+        const POINT leftTopPadding        = _padding.LeftTop();
+        const POINT absoluteChildPosition = absolutePosition + leftTopPadding;
+        return absoluteChildPosition;
+    }
+    PROPERTY(AbsoluteChildPosition)
+
+    GETTER_ONLY(bool, IsMeasureDirty)
+    {
+        return _isMeasureDirty;
+    }
+    PROPERTY(IsMeasureDirty)
+
+    GETTER_ONLY(bool, IsArrangeDirty)
+    {
+        return _isArrangeDirty;
+    }
+    PROPERTY(IsArrangeDirty)
+
+    GETTER_ONLY(SIZE, DesiredSize) { return ReflectFields->DesiredSize; }
+    PROPERTY(DesiredSize)
+
+    GETTER_ONLY(POINT, Offset) { return ReflectFields->Offset; }
+    PROPERTY(Offset)
+
+    GETTER_ONLY(POINT, ActualPosition) { return ReflectFields->ActualPosition; }
+    PROPERTY(ActualPosition)
+
+    GETTER_ONLY(SIZE, ActualSize) { return ReflectFields->ActualSize; }
+    PROPERTY(ActualSize)
+
+public:
+    void Measure(SIZE availableSize);
+    void Arrange(POINT finalPosition, SIZE finalSize);
+    void Arrange();
+
+protected:
+    /// <summary>
+    /// 이 컴포넌트를 소유한 게임 오브젝트에 다른 자식 게임 오브젝트가 추가되었을 때 호출됩니다.
+    /// </summary>
+    /// <param name="childGameObject">연결될 자식 GameObject에 대한 포인터입니다.</param>
+    virtual void OnAttachChild(GameObject* childGameObject);
+
+    /// <summary>
+    /// 이 컴포넌트를 소유한 게임 오브젝트가 다른 부모 게임 오브젝트로부터 분리되었을 때 호출됩니다.
+    /// </summary>
+    /// <param name="previousParentGameObject">이전에 연결되어 있던 부모 GameObject에 대한 포인터입니다.</param>
+    virtual void OnDetachParent(GameObject* previousParentGameObject);
+
+    /// <summary>
+    /// OnDrawDebug에 의해 호출되는 디버그 오버라이드 함수입니다.
+    /// EnableInHierarchy가 true일 때만 호출됩니다.
+    /// </summary>
+    virtual void OnDrawDebugOverride();
+
+    /// <summary>
+    /// OnDrawDebugSelected에 의해 호출되는 디버그 선택 오버라이드 함수입니다.
+    /// EnableInHierarchy가 true일 때만 호출됩니다.
+    /// </summary>
+    virtual void OnDrawDebugSelectedOverride();
+
+
+    /// <summary>
+    /// UI 컴포넌트의 측정 로직을 구현하는 함수입니다.
+    /// </summary>
+    /// <param name="availableSize">측정에 사용할 가용 크기입니다.</param>
+    /// <returns>측정된 요소의 크기(SIZE)를 반환합니다.</returns>
+    virtual SIZE MeasureOverride(SIZE availableSize) = 0;
+
+    /// <summary>
+    /// 지정된 최종 크기에 맞게 요소를 배치하고 실제 배치된 크기를 반환합니다.
+    /// </summary>
+    /// <param name="finalSize">배치에 사용할 최종 크기입니다.</param>
+    /// <returns>실제로 배치된 요소의 크기(SIZE)를 반환합니다.</returns>
+    virtual SIZE ArrangeOverride(SIZE finalSize)     = 0;
+
+protected:
+    void ImGuiDrawPropertysEvent() override;
+    void DeserializedReflectEvent() override;
+
+    void InvalidateMeasure();
+    void InvalidateArrange();
+
+
+private:
+    void ResetPlacement();
+
+    void OnDrawDebug() override;
+    void OnDrawDebugSelected() override;
+
+protected:
+    REFLECT_FIELDS_BEGIN(Component)
+    POINT ActualPosition;
+    SIZE  ActualSize;
+    POINT Offset;
+    SIZE  DesiredSize;
+    SIZE  AvailableSize;
+
+    LONG  PaddingLeft;
+    LONG  PaddingTop;
+    LONG  PaddingRight;
+    LONG  PaddingBottom;
+
+    LONG  MarginLeft;
+    LONG  MarginTop;
+    LONG  MarginRight;
+    LONG  MarginBottom;
+    REFLECT_FIELDS_END(UIComponent)
+
+protected:
+    POINT _requestedPoint;
+    SIZE  _requestedSize;
+
+    PADDING _padding;
+    MARGIN  _margin;
+
+private:
+    bool _isMeasureDirty;
+    bool _isArrangeDirty;
+};
+
+struct DrawDebug
+{
+    void operator()(const char* label) const;
+    void operator()(const char* label, long x) const;
+    void operator()(const char* label, long x, long y) const;
+};
+
+struct DrawQuad
+{
+    void operator()(POINT point, SIZE size, const int thickness, FXMVECTOR color) const;
+};
+
+// TODO 지울지 고민
+template <typename T>
+struct FindChildComponents
+{
+    std::vector<T*> operator()(Transform& parentTransform)
+    {
+        std::vector<T*> components;
+        for (int i = 0; i < parentTransform.GetChildCount(); ++i)
+        {
+            const Transform* child           = parentTransform.GetChild(i);
+            GameObject&      gameObject      = child->gameObject;
+            std::vector<T*>  childComponents = gameObject.GetComponents<T>();
+            std::move(childComponents.begin(), childComponents.end(), std::back_inserter(components));
+        }
+        return components;
+    }
+};
