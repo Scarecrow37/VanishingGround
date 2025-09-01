71029028342ec46c0007f19085c135bc4c63d5b9
PPAK_JU <taku7664@gmail.com>
2025-09-01 19:07:47 +0900

    [FEATURE/VG-368] EffectManager 컴포넌트 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/EffectManager/EffectManager.cpp
@@ -0,0 +1,64 @@
+﻿#include "pchScripts.h"
+#include "EffectManager.h"
+#include "Particle/ParticleComponent.h"
+
+EffectManager::EffectManager() 
+{
+}
+
+EffectManager::~EffectManager() 
+{
+}
+
+void EffectManager::Awake() 
+{
+    // 기존 이펙트 풀 리셋
+    for (int i = 0; i < _weaponHitEffectPool.size(); ++i)
+    {
+        gameObject->Destroy(_weaponHitEffectPool[i]);
+    }
+    _weaponHitEffectPool.clear();
+
+    // HitEffectVFXGuid가 유효하면 이펙트 풀링 생성
+    if (File::NULL_GUID != ReflectFields->HitEffectVFXGuid)
+    {
+        for (int i = 0; i < ReflectFields->WeaponHitEffectPoolSize; ++i)
+        {
+            ParticleComponent& effect = AddComponent<ParticleComponent>();
+            _weaponHitEffectPool.push_back(&effect);
+            // TODO: ParticleComponent에 vfx파일 로드하는 함수 만들기
+        }
+    }
+}
+
+void EffectManager::SerializedReflectEvent() 
+{
+}
+
+void EffectManager::DeserializedReflectEvent() 
+{
+}
+
+void EffectManager::ImGuiDrawPropertysEvent() 
+{
+}
+
+bool EffectManager::SpawnWeaponHitEffect(const Vector3& position, const Vector3& rotation, const Vector3& scale)
+{
+    if (_weaponHitEffectPool.empty())
+    {
+        return false;
+    }
+
+    ParticleComponent* effect = _weaponHitEffectPool[_currentWeaponHitEffectIndex];
+    if (effect)
+    {
+        effect->StopEffect();
+        effect->PlayEffect();
+        effect->Position = position;
+        effect->Rotation = rotation;
+        effect->Scale    = scale;
+        // 다음 이펙트 인덱스 갱신
+        _currentWeaponHitEffectIndex = (_currentWeaponHitEffectIndex + 1) % _weaponHitEffectPool.size();
+        return true;
+    }
---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/EffectManager/EffectManager.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+class ParticleComponent;
+
+class EffectManager : public Component
+{
+    USING_PROPERTY(EffectManager)
+    inline static EffectManager* _staticInstance;
+
+public:
+    EffectManager();
+    ~EffectManager();
+    inline static EffectManager* GetInstance() { return _staticInstance; }
+
+private:
+    void Awake() override;
+    void SerializedReflectEvent() override;
+    void DeserializedReflectEvent() override;
+    void ImGuiDrawPropertysEvent() override;
+
+public:
+    bool SpawnWeaponHitEffect(const Vector3& position, 
+                              const Vector3& rotation = Vector3(0.0f, 0.0f, 0.0f),
+                              const Vector3& scale = Vector3(1.0f, 1.0f, 1.0f));
+
+private:
+    // Hit이펙트 풀링
+    std::vector<ParticleComponent*> _weaponHitEffectPool;   // 이펙트 풀
+    int _currentWeaponHitEffectIndex = 0; // 현재 사용할 이펙트 인덱스
+
+    REFLECT_FIELDS_BEGIN(Component)
+    std::string HitEffectVFXGuid;
+    int WeaponHitEffectPoolSize = 10; // 이펙트 풀 크기
+    REFLECT_FIELDS_END(EffectManager)
+};
