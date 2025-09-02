7d66e11be4347fe57a805ca0dc2ccaad7d49f055
power12562 <kimsiwoo1005@gmail.com>
2025-09-02 13:57:05 +0900

    [FEATURE/VG-28] 기즈모 매니저 추가

---
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/EngineCore/GizmoManager.cpp
@@ -0,0 +1,140 @@
+﻿#include "pch.h"
+#include "GizmoManager.h"
+#include "Editor/Tool/Scene/Command/EditorSceneCommands.h"
+
+EGizmoManager::EGizmoManager() 
+{
+    _targetWindow = nullptr;
+    _targetCamera = nullptr;
+}
+
+void EGizmoManager::SubmitSceneGizmo(SceneGizmo* gizmo)
+{
+    //활성화된 기즈모만 그림.
+    if (gizmo->_owner.gameObject->IsValid())
+    {
+        _sceneGizmos.emplace_back(gizmo->_owner.GetWeakPtr(), gizmo);
+    }  
+}
+
+void EGizmoManager::BeginDraw(ImGuiWindow* targetWindow, Camera* camera) 
+{
+    _targetWindow = targetWindow;
+    _targetCamera = camera;
+}
+
+void EGizmoManager::Draw()
+{
+    if (_targetWindow && _targetCamera)
+    {
+        // 유효한 기즈모만 그린다.
+        std::erase_if(_sceneGizmos, 
+        [this](const std::pair<std::weak_ptr<Component>, SceneGizmo*>& pair) 
+        {
+            // 생명 여부 확인
+            const std::weak_ptr<Component>&  weakOwner = pair.first;
+            const std::shared_ptr<Component> owner     = weakOwner.lock();
+            if (nullptr == owner)
+            {
+                return true;
+            }
+
+            // 프러스텀 컬링
+            BoundingFrustum frustum;
+            BoundingFrustum::CreateFromMatrix(frustum, _targetCamera->GetProjectionMatrix());
+            frustum.Transform(frustum, _targetCamera->GetWorldMatrix());
+
+            BoundingBox box;
+            box.Extents = {2, 2, 2};
+            box.Transform(box, owner->transform->GetWorldMatrix());
+
+            return false == frustum.Intersects(box);
+        });
+
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.0f, 0.0f, 0.0f, 0.0f));
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.f, 0.f, 0.f, 0.f));
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.0f, 0.0f, 0.0f, 0.0f)); 
+        for (auto& [weakOwner, gizmo] : _sceneGizmos)
+        {
+            if (gizmo->_icon)
+            {
+                ImVec2 screenPos;
+                if (CalculateGizmoScreenPosition(*gizmo, &screenPos))
+                {
+                    const ImGuiViewport*               viewport    = ImGui::GetMainViewport();
+                    const D3D12_GPU_DESCRIPTOR_HANDLE& imageHandle = gizmo->_icon->GetGPUHandle();
+
+                    //이미지 크기에 맞게 중심 이동
+                    screenPos.x -= gizmo->Size.x * 0.5f;
+                    screenPos.y -= gizmo->Size.y * 0.5f;
+                    ImGui::SetCursorPos(screenPos);
+                    if (ImGui::ImageButton((ImTextureID)imageHandle.ptr, gizmo->Size))
+                    {
+                        std::weak_ptr<GameObject> oldWp = EditorHierarchyTool::GetFocusObject();
+                        UmCommandManager.Do<Command::Hierarchy::FocusCommand>(oldWp, gizmo->_owner.gameObject->GetWeakPtr());
+                    }
+                }
+            }
+        }
+        ImGui::PopStyleColor(3);
+    } 
+    else
+    {
+        assert(!"BeginDraw를 먼저 호출해야 합니다.");
+    }
+}
+
+void EGizmoManager::EndDraw() 
+{
+    _targetWindow = nullptr;
+    _targetCamera = nullptr;
+    _sceneGizmos.clear();
+}
+
+bool EGizmoManager::CalculateGizmoScreenPosition(SceneGizmo& gizmo, ImVec2* outScreenPos)
+{
+    if (nullptr == outScreenPos)
+    {
+        return false;
+    }
+
+    //행렬 로드
+    const DirectX::XMMATRIX worldMatrix = DirectX::XMLoadFloat4x4(&gizmo._owner.transform->GetWorldMatrix());
+    const DirectX::XMMATRIX viewMatrix  = DirectX::XMLoadFloat4x4(&_targetCamera->GetViewMatrix());
+    const DirectX::XMMATRIX projMatrix  = DirectX::XMLoadFloat4x4(&_targetCamera->GetProjectionMatrix());
+
+    // 월드 좌표
+    DirectX::XMVECTOR worldPos = worldMatrix.r[3];
+
+    // WVP 변환
+    DirectX::XMMATRIX viewProjectionMatrix = DirectX::XMMatrixMultiply(viewMatrix, projMatrix);
+    DirectX::XMVECTOR clipPos              = DirectX::XMVector4Transform(worldPos, viewProjectionMatrix);
+
+    // 클립핑
+    DirectX::XMFLOAT4 clipPosF;
+    DirectX::XMStoreFloat4(&clipPosF, clipPos);  
+
+    // 카메라 뒤에 있으면 실패
+    if (clipPosF.w < 0.0f)
+    {
+        return false;
+    }
+
+    // 원근 나눗셈
+    DirectX::XMFLOAT3 ndcPos = {clipPosF.x / clipPosF.w, clipPosF.y / clipPosF.w, clipPosF.z / clipPosF.w};
+
+    // 화면 좌표 변환
+    const ImVec2& viewportPos  = _targetWindow->ContentRegionRect.Min;
+    const ImVec2  viewportSize = _targetWindow->ContentRegionRect.GetSize();
+    
+    ImVec2 screenPos;
+    screenPos.x = (ndcPos.x + 1.0f) * 0.5f * viewportSize.x + viewportPos.x;
+    screenPos.y = (1.0f - ndcPos.y) * 0.5f * viewportSize.y + viewportPos.y;
+
+    // ImGui 윈도우 로컬 좌표로 변환
+    ImVec2& windowPos = _targetWindow->Pos;
+    outScreenPos->x   = screenPos.x - windowPos.x;
+    outScreenPos->y   = screenPos.y - windowPos.y;
+
+    return true;
+}
---
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/EngineCore/GizmoManager.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+
+class SceneGizmo;
+
+class EGizmoManager
+{
+public:
+    using SceneGizmoQueue = std::vector<std::pair<std::weak_ptr<Component>, SceneGizmo*>>; //씬 기즈모 Queue
+
+    EGizmoManager();
+    ~EGizmoManager() = default;
+
+    /// <summary>
+    /// 이번 프레임에 렌더링할 기즈모를 등록합니다.
+    /// </summary>
+    /// <param name="gizmo :">그릴 대상</param>
+    void SubmitSceneGizmo(SceneGizmo* gizmo);
+
+    /// <summary>
+    /// 대상 Window와 Camera 기준으로 Gizmo를 그릴 준비를 합니다.
+    /// </summary>
+    /// <param name="targetWindow :">대상 ImGuiWindow</param>
+    /// <param name="camera :">대상 Camera</param>
+    void BeginDraw(ImGuiWindow* targetWindow, Camera* camera);
+
+    /// <summary>
+    /// 등록된 기즈모들을 Draw합니다. BeginDraw가 먼저 호출되야합니다.
+    /// </summary>
+    void Draw();
+
+    /// <summary>
+    /// Draw를 종료합니다.
+    /// </summary>
+    void EndDraw();
+
+    /// <summary>
+    /// 씬 기즈모 큐를 반환합니다.
+    /// </summary>
+    /// <returns>씬 기즈모 큐</returns>
+    const SceneGizmoQueue& GetSceneGizmoQueue() 
+    { 
+        return _sceneGizmos; 
+    }
+
+private:
+    SceneGizmoQueue _sceneGizmos;
+    ImGuiWindow*    _targetWindow;
+    Camera*         _targetCamera;
+
+ private:
+    //출력할 위치를 계산합니다.
+    bool CalculateGizmoScreenPosition(SceneGizmo& gizmo, ImVec2* outScreenPos);
+
+};
