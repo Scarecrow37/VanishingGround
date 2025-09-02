270e7284290a2f712f3bef591c28d2b41699c670
PPAK_JU <taku7664@gmail.com>
2025-09-02 12:43:48 +0900

    [FEATURE/VG29] GameView에서 카메라 직접 조작하는 기능 추가

--- Source/GA6thFinal_Framework/GameEngine/Source/Editor/Tool/GameView/EditorGameView.cpp
+++ Source/GA6thFinal_Framework/GameEngine/Source/Editor/Tool/GameView/EditorGameView.cpp
@@ -1,22 +1,29 @@
 ﻿#include "pch.h"
 #include "UmScripts.h"
+#include "Editor/DynamicCamera/EditorDynamicCamera.h"
 
 EditorGameView::EditorGameView() 
+    : _cameraMode(CAMERA_MODE_DEFAULT)
+    , _freeCamera(new EditorDynamicCamera)
 {
     SetLabel("Game");
     SetDockLayout(ImGuiDir_Up);
 }
 
-EditorGameView::~EditorGameView() {}
-
-void EditorGameView::OnFrameFocusEnter() 
+EditorGameView::~EditorGameView()
 {
+    if (_freeCamera)
+    {
+        delete _freeCamera;
+        _freeCamera = nullptr;
+    }
 }
 
 void EditorGameView::OnFrameRender() 
 {
-    CameraComponent* camera = ESceneManager::Engine::GetMainCamera();
-    if (camera && camera->gameObject->IsValid())    
+    std::shared_ptr<Camera> camera = UmGraphics.GetCamera("Game");
+    CameraComponent* cameraComponent = ESceneManager::Engine::GetMainCamera();
+    if (camera && cameraComponent && cameraComponent->gameObject->IsValid())    
     {
         auto   gpuHandle  = UmGraphics.GetRenderSceneImage("Game");
         ImVec2 contentMin = ImGui::GetWindowContentRegionMin();
@@ -27,7 +34,7 @@ void EditorGameView::OnFrameRender()
         fullSize.y = contentMax.y - contentMin.y;
 
         ImVec2 size;
-        float aspect = camera->Aspect;
+        float  aspect = cameraComponent->Aspect;
         if (fullSize.x / fullSize.y > aspect)
         {
             // 세로가 제한 요소 → 세로 기준 최대 크기, 가로는 비율로 맞춤
@@ -45,5 +52,35 @@ void EditorGameView::OnFrameRender()
         ImVec2 startPos = ImGui::GetWindowPos() + contentMin + centerOffset;
         ImGui::SetCursorScreenPos(startPos);
         ImGui::Image((ImTextureID)gpuHandle.ptr, size);
+
+        if (false == _freeCamera->IsMoved() &&
+            false == _freeCamera->IsRotated())
+        {
+            if (ImGui::BeginPopupContextItem())
+            {
+                if (ImGui::MenuItem("Default", "", _cameraMode == CAMERA_MODE_DEFAULT))
+                {
+                    Transform& tr = cameraComponent->gameObject->transform;
+                    camera->SetWorldMatrix(tr.GetWorldMatrix());
+                    _cameraMode = CAMERA_MODE_DEFAULT;
+                }
+                if (ImGui::MenuItem("Free Manipulate", "", _cameraMode == CAMERA_MODE_FREE_MANIPULATE))
+                {
+                    _freeCamera->SetPosition(Vector3::Zero);
+                    _freeCamera->SetRotation(Quaternion::Identity);
+                    _freeCamera->SetPivot(0.0f);
+                    _freeCamera->SetTarget(camera);
+                    _cameraMode = CAMERA_MODE_FREE_MANIPULATE;
+                }
+            }
+        }
+    }
+}
+
+void EditorGameView::OnFrameFocusStay()
+{
+    if (_cameraMode == CAMERA_MODE_FREE_MANIPULATE)
+    {
+        _freeCamera->Update();
     }
 }
--- Source/GA6thFinal_Framework/GameEngine/Source/Editor/Tool/GameView/EditorGameView.h
+++ Source/GA6thFinal_Framework/GameEngine/Source/Editor/Tool/GameView/EditorGameView.h
@@ -1,58 +1,24 @@
 ﻿#pragma once
 
+class CameraComponent;
+
 class EditorGameView : public EditorTool
 {
+    enum CameraMode
+    {
+        CAMERA_MODE_DEFAULT,
+        CAMERA_MODE_FREE_MANIPULATE,
+    };
 public:
     EditorGameView();
     virtual ~EditorGameView() override;
 
 protected:
-    virtual void OnTickGui() override {}
-    virtual void OnStartGui() override {}
-    virtual void OnEndGui() override {}
+    void OnFrameRender() override;
+    void OnFrameFocusStay() override;
 
-    /// <summary>
-    /// ImGui::Begin 호출 직전에 호출됩니다.
-    /// </summary>
-    virtual void OnPreFrameBegin() override {}
-
-    /// <summary>
-    /// ImGui::Begin 호출 직후에 호출됩니다.
-    /// </summary>
-    virtual void OnPostFrameBegin() override {}
-
-    /// <summary>
-    /// Gui의 프레임이 클리핑이 될 때 1회 호출됩니다.
-    /// </summary>
-    virtual void OnFrameClipped() override {}
-
-    /// <summary>
-    /// ImGui::End 호출 직후에 호출됩니다.
-    /// </summary>
-    virtual void OnFrameEnd() override {}
-
-    /// <summary>
-    /// 프레임이 포커싱 상태에 진입되었을 때 1회 호출됩니다.
-    /// </summary>
-    virtual void OnFrameFocusEnter() override;
-
-    /// <summary>
-    /// 프레임이 포커싱 상태에 있는 동안 매 프레임마다 호출됩니다.
-    /// </summary>
-    virtual void OnFrameFocusStay() override {}
-    /// <summary>
-    /// 프레임이 포커싱 상태에서 벗어났을 때 1회 호출됩니다.
-    /// </summary>
-    virtual void OnFrameFocusExit() override {}
-
-    /// <summary>
-    /// ImGui::Begin 호출 후 클리핑 테스트를 통과할 시 호출됩니다.
-    /// </summary>
-    virtual void OnFrameRender() override;
-     
-    /// <summary>
-    /// 프레임 Tab의 팝업이 열렸을 때 호출됩니다.
-    /// </summary>
-    virtual void OnFramePopupOpened() override {}
-
+private:
+    CameraMode _cameraMode;
+    CameraComponent* _mainCamera;
+    EditorDynamicCamera* _freeCamera;
 };
