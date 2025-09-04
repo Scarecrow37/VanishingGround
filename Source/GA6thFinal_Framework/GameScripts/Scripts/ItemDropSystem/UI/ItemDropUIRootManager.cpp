6479196bb6ee40a7095fef575bc52ea6f30b5129
power12562 <kimsiwoo1005@gmail.com>
2025-09-04 11:48:29 +0900

    [FEATURE/VG-371] 드랍 시스템을 위한 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/ItemDropSystem/UI/ItemDropUIRootManager.cpp
@@ -0,0 +1,54 @@
+﻿#include "pchScripts.h"
+#include "ItemDropUIRootManager.h"
+ItemDropUIRootManager::ItemDropUIRootManager() = default;
+ItemDropUIRootManager::~ItemDropUIRootManager()
+{
+    if (this == static_instance)
+    {
+        static_instance = nullptr;
+    }
+
+    ArtifactsUIFrameAsset.SetInputAutoEvent([this]() 
+    { 
+        if (ImGui::BeginDragDropTarget())
+        {
+            if (const ImGuiPayload* payLoad = ImGui::AcceptDragDropPayload(DragDropAsset::KEY))
+            {
+                DragDropAsset::Data* data      = static_cast<DragDropAsset::Data*>(payLoad->Data);
+                File::Path           path      = data->GetPath();
+                const auto           extension = path.extension();
+                if (extension == L".png")
+                {
+                    ReflectFields->ArtifactsUIFrameAssetGuid = data->GetGuid().string();
+                }
+            }
+            ImGui::EndDragDropTarget();
+        }
+        ImGuiHelper::HoveredToolTip(u8"유물 드랍 프레임 UI 에셋 경로입니다.");
+    });
+}
+
+void ItemDropUIRootManager::DeserializedReflectEvent() 
+{
+
+}
+
+void ItemDropUIRootManager::Awake()
+{
+    InitSingleTon();
+    gameObject->AddTag(ItemDropUIRootManager::TAG);
+    gameObject->ActiveSelf = false;
+    Base::Awake();
+}
+
+void ItemDropUIRootManager::InitSingleTon() 
+{
+    if (nullptr == static_instance)
+    {
+        static_instance = this;
+    }
+    else
+    {
+        GameObject::Destroy(gameObject);
+    }
+}
---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/ItemDropSystem/UI/ItemDropUIRootManager.h
@@ -0,0 +1,56 @@
+﻿#pragma once
+#include "UmFramework.h"
+class ItemDropUIRootManager : public Component
+{
+    USING_PROPERTY(ItemDropUIRootManager)
+    inline static ItemDropUIRootManager* static_instance = nullptr;
+public:
+    inline static constexpr const char* TAG = "Item Drop UI Root";
+
+    /// <summary>
+    /// ItemDropUIRootManager가 존재하면 포인터를 반환합니다.
+    /// </summary>
+    /// <param name="location :">기본 인자를 통해 해당 함수를 호출한 위치를 기록합니다.</param>
+    /// <returns>없으면 nullptr</returns>
+    static ItemDropUIRootManager* GetInstance(std::source_location location = std::source_location::current()) 
+    { 
+        if (static_instance)
+        {
+            return static_instance;
+        }
+        else
+        {
+            UmLogger.Log(LogLevel::LEVEL_WARNING, u8"Item Drop UI Root가 존재하지 않습니다.", location);
+            return nullptr;
+        }
+    }
+
+    ItemDropUIRootManager();
+    ~ItemDropUIRootManager() override;
+
+public:
+    REFLECT_PROPERTY(
+        ArtifactsUIFrameAsset
+    )
+
+    GETTER_ONLY(std::string, ArtifactsUIFrameAsset)
+    {
+        File::Guid  guid = ReflectFields->ArtifactsUIFrameAssetGuid;
+        std::string path = guid.ToPath().string();
+        return path;
+    }
+    //type : std::string
+    //유물 드랍 프레임 UI 에셋 경로입니다.
+    PROPERTY(ArtifactsUIFrameAsset)
+
+protected:
+    REFLECT_FIELDS_BEGIN(Component)
+    std::string ArtifactsUIFrameAssetGuid;
+    REFLECT_FIELDS_END(ItemDropUIRootManager)
+
+    void DeserializedReflectEvent() override;
+    void Awake() override;
+
+private:
+    void InitSingleTon();
+};
