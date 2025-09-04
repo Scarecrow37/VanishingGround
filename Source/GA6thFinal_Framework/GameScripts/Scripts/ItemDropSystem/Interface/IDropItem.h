01021c52f7f89f4585b8c505d479710368923c95
power12562 <kimsiwoo1005@gmail.com>
2025-09-04 11:48:18 +0900

    [FEATURE/VG-371] 드랍 시스템을 위한 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/ItemDropSystem/Interface/IDropItem.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+struct DropItemInfo
+{
+    int ID;
+};
+
+/*보상 시스템에 등장 가능한 유물들은 다음 인터페이스를 상속받아야 합니다.*/
+class IDropItem abstract
+{
+public:
+    IDropItem() = default;
+    virtual ~IDropItem() = default;
+
+    //아이템의 정보들을 반환합니다.
+    virtual DropItemInfo GetItemInfo() = 0;
+};
