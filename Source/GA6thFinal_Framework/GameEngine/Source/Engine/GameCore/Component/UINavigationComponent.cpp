928164d2e1c7432292c5f004c7c2f7612bb8abaf
Do_Suha <weqa123@naver.com>
2025-09-05 18:47:32 +0900

    [FEATURE/VG-181] UINavigationComponent 클래스 추가
    - UINavigationComponent.h와 UINavigationComponent.cpp 파일이 추가되어, 내비게이션 컴포넌트의 기본 구조와 메서드가 정의되었습니다.
    - UIBaseComponent를 상속받으며, 디버그 모드에서 내비게이션 경로를 그리는 기능을 위한 메서드가 구현되었습니다.
    - GameEngine.vcxproj 및 GameEngine.vcxproj.filters 파일에 관련 파일들이 포함되도록 수정되었습니다.

---
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/GameCore/Component/UINavigationComponent.cpp
@@ -0,0 +1,11 @@
+﻿#include "pch.h"
+#include "UINavigationComponent.h"
+
+UINavigationComponent::UINavigationComponent() = default;
+
+void UINavigationComponent::OnDrawDebugSelectedOverride()
+{
+    UIBaseComponent::OnDrawDebugSelectedOverride();
+
+    // TODO Draw Navigation Route
+}
---
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/GameCore/Component/UINavigationComponent.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+class UINavigationComponent : public UIBaseComponent
+{
+    friend class UIManager;
+    USING_PROPERTY(UINavigationComponent)
+
+public:
+    UINavigationComponent();
+
+public:
+    REFLECT_PROPERTY()
+
+protected:
+    virtual void OnFocusIn() {}
+    virtual void OnFocusOut() {}
+    virtual void OnSubmit() {}
+
+private:
+    void OnDrawDebugSelectedOverride() override;
+
+protected:
+    REFLECT_FIELDS_BEGIN(UIBaseComponent)
+    REFLECT_FIELDS_END(UINavigationComponent)
+};
