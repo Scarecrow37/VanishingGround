fcb47c6e3fa56389a4b591ff88720b542bc20dc9
bmbany <59551445+bmbany@users.noreply.github.com>
2025-09-05 20:25:22 +0900

    [FIX/VG-372] MeshRenderTarget 해상도 잘못돼 있는 버그 수정

--- Source/GA6thFinal_Framework/GraphicsEngine/SSRPass.cpp
+++ Source/GA6thFinal_Framework/GraphicsEngine/SSRPass.cpp
@@ -14,12 +14,13 @@ void SSRPass::Initialize(RenderScene* ownerScene, RenderTechnique* ownerTechniqu
                                              D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET);
 
     PipelineStateStream pss;
-    pss.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
-    pss.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
-    pss.DepthStencilState = CD3DX12_DEPTH_STENCIL_DESC(D3D12_DEFAULT);
+    pss.BlendState                        = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
+    pss.RasterizerState                   = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
+    pss.DepthStencilState                 = CD3DX12_DEPTH_STENCIL_DESC(D3D12_DEFAULT);
     (&pss.DepthStencilState)->DepthEnable = FALSE;
     pss.PrimitiveTopology                 = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
     pss.RTVFormats                        = {{DXGI_FORMAT_R32G32B32A32_FLOAT}, 1};
+
     _fxSSR.SetPipelineStateStream(pss);
     _pipelineState = Global::pipelineStateManager->GetPipelineState(pss);
 }
@@ -38,22 +39,21 @@ void SSRPass::Draw(ID3D12GraphicsCommandList* commandList)
     renderTarget->ClearRenderTarget(commandList, 0);
     renderTarget->TransitionResource(commandList, D3D12_RESOURCE_STATE_RENDER_TARGET);
     renderTarget->ClearRenderTarget(commandList, 0);
+
     commandList->OMSetRenderTargets(1, &renderTarget->GetRTVHandle(), FALSE, nullptr);
     commandList->RSSetViewports(1, &renderTarget->GetViewport());
     commandList->RSSetScissorRects(1, &renderTarget->GetScissorRect());
+
     auto        ssrProperty       = std::any_cast<SSRPassProperty>(_ownerScene->GetRenderPassProperty("SSRPass"));
     const auto& renderTargetGroup = Global::multiRenderTargetManager->GetRenderTargetGroup("GBuffer");
     auto        cameraData        = _ownerScene->_cameraBuffer->GetGPUVirtualAddress();
+
     commandList->SetGraphicsRootSignature(_fxSSR.GetRootSignature());
     commandList->SetPipelineState(_pipelineState.Get());
-    commandList->SetGraphicsRootDescriptorTable(_fxSSR.GetRootParameterIndex("screenColor"),
-                                                _meshRenderTarget->GetSRVHandle());
-    commandList->SetGraphicsRootDescriptorTable(_fxSSR.GetRootParameterIndex("screenNormal"),
-                                                renderTargetGroup[GBuffer::NORMAL]->GetSRVHandle());
-    commandList->SetGraphicsRootDescriptorTable(_fxSSR.GetRootParameterIndex("screenDepth"),
-                                                renderTargetGroup[GBuffer::DEPTH]->GetSRVHandle());
-    commandList->SetGraphicsRootDescriptorTable(_fxSSR.GetRootParameterIndex("screenORM"),
-                                                renderTargetGroup[GBuffer::ORM]->GetSRVHandle());
+    commandList->SetGraphicsRootDescriptorTable(_fxSSR.GetRootParameterIndex("screenColor"), _meshRenderTarget->GetSRVHandle());
+    commandList->SetGraphicsRootDescriptorTable(_fxSSR.GetRootParameterIndex("screenNormal"), renderTargetGroup[GBuffer::NORMAL]->GetSRVHandle());
+    commandList->SetGraphicsRootDescriptorTable(_fxSSR.GetRootParameterIndex("screenDepth"), renderTargetGroup[GBuffer::DEPTH]->GetSRVHandle());
+    commandList->SetGraphicsRootDescriptorTable(_fxSSR.GetRootParameterIndex("screenORM"), renderTargetGroup[GBuffer::ORM]->GetSRVHandle());
     commandList->SetGraphicsRootConstantBufferView(_fxSSR.GetRootParameterIndex("cameraData"), cameraData);
     commandList->SetGraphicsRoot32BitConstants(_fxSSR.GetRootParameterIndex("bit32_4_ssrProperty"), 4, &ssrProperty, 0);
     _ownerScene->_frameQuad->Render(commandList);
