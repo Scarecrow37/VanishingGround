d2e66fba84d1b0eec0dd71a933c2343de50c6bbb
power12562 <kimsiwoo1005@gmail.com>
2025-09-08 19:19:09 +0900

    [FEATURE/VG-371] RollArtifacts 함수 구현

--- Source/GA6thFinal_Framework/GameScripts/Scripts/ItemDropSystem/ItemDropSystem.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/ItemDropSystem/ItemDropSystem.cpp
@@ -5,55 +5,230 @@
 #include "RevelationSystem/RevelationSystem.h"
 #include "ItemDropSystem/UI/ArtifactUIManager.h"
 
-ItemDropSystem::ItemDropSystem() = default;
+
+//내부 사용 구조체 및 enum
+namespace
+{
+    constexpr auto ArtifactDropTypeArray = rfl::get_enumerator_array<ArtifactDropType>();
+    constexpr size_t ArtifactDropTypeArraySize = ArtifactDropTypeArray.size(); // 유물 카테고리 개수
+
+    constexpr auto RevelationGradeArray = rfl::get_enumerator_array<RevelationGrade>();
+    constexpr size_t RevelationGradeArraySize = static_cast<size_t>(RevelationGrade::EXTINCTION);
+
+    constexpr auto WeaponTypeArray = rfl::get_enumerator_array<WeaponType>();
+    constexpr size_t WeaponTypeArraySize = WeaponTypeArray.size();
+
+    constexpr auto WeaponGradeArray = rfl::get_enumerator_array<WeaponGrade>();
+    constexpr size_t WeaponGradeArraySize = WeaponGradeArray.size();
+} // namespace
+
+const size_t ItemDropSystem::ARTIFACT_TYPE_COUNT = ArtifactDropTypeArraySize; // 유물 카테고리 개수
+
+ItemDropSystem::ItemDropSystem()
+{
+    ReflectFields->MaxDropCount = {3, 3, 3, 3, 3, 2};
+}
 ItemDropSystem::~ItemDropSystem() = default;
 
-std::vector<DropItemInfo> ItemDropSystem::RollArtifacts()
+std::array<DropItemInfo, ARTIFACT_DROP_COUNT> ItemDropSystem::RollArtifacts()
 {  
-    std::vector<DropItemInfo> artifacts;
-
-    //일단 임시로 그냥 단순 6개 랜덤
     RevelationSystem* revelationSystem = RevelationSystem::GetInstance();
+    std::array<std::vector<RevelationElement*>, RevelationGradeArraySize> revelations{};
     if (revelationSystem)
     {
+        // 등급별 계시 분류
         const auto& table = revelationSystem->GetRevelationTableElements();
         for (auto& revelation : table)
         {
-            artifacts.push_back(revelation->GetItemInfo());
+            RevelationGrade grade = revelation->Grade;
+            size_t          index = static_cast<size_t>(grade);
+            revelations[index].push_back(revelation);
         }      
     }
+
+    //무기 테이블에 대한 이중 배열. 첫번째는 무기 타입, 두번째는 무기 등급
+    std::array<std::array<std::vector<WeaponElement*>, WeaponGradeArraySize>, WeaponTypeArraySize> weapons{};
     WeaponTableComponent* weaponTableComponent = WeaponTableComponent::GetInstance();
     if (weaponTableComponent)
     {
+        // 등급별 무기 분류
         const auto& table = weaponTableComponent->GetWeaponTableElements();
         for (auto& weapon : table)
         {
-            artifacts.push_back(weapon->GetItemInfo());
+            WeaponType  type       = weapon->Stats.Type;
+            size_t      typeIndex  = static_cast<size_t>(type);
+            WeaponGrade grade      = weapon->Stats.Grade;
+            size_t      gradeIndex = static_cast<size_t>(grade);
+            weapons[typeIndex][gradeIndex].push_back(weapon);
         }
     }
 
-    if (false == artifacts.empty())
+    std::array<DropItemInfo, ARTIFACT_DROP_COUNT> artifacts;      // 결과 담는 배열
+    std::array<int, ArtifactDropTypeArraySize>    maxDropCount{}; // 중복 등장 카운트용
+    for (size_t i = 0; i < maxDropCount.size(); ++i)
     {
-        std::shuffle(artifacts.begin(), artifacts.end(), Random::GetEngine());
-        artifacts.resize(6);
+        maxDropCount[i] = ReflectFields->MaxDropCount[i];
+    }
+    std::vector<ArtifactDropType> dropCategory;                  // 이번에 등장 가능한 분류
+    for (auto& artifact : artifacts)
+    {        
+        // 등장 가능한 종류 등록
+        dropCategory.clear();
+        for (size_t i = 0; i < maxDropCount.size(); ++i)
+        {
+            int count = maxDropCount[i];
+            if (0 < count)
+            {
+                auto& [str, value] = ArtifactDropTypeArray[i];
+                dropCategory.push_back(value);
+            }
+        }
+
+        // 랜덤한 종류 뽑기
+        size_t randomCategoryIndex = Random::Index(dropCategory.size());
+        --maxDropCount[randomCategoryIndex];
+        ArtifactDropType type = dropCategory[randomCategoryIndex];
+
+        //종류별 랜덤 뽑기
+        auto RollWeaponRandomGrade =[this](const std::array<double, WeaponGradeArraySize>& weight) -> WeaponGrade 
+        { 
+            auto& [gradeStr, grade] = WeaponGradeArray[Random::Index(weight)];
+            return grade;
+        };
+        auto RollRevelationRandomGrade =[this](const std::array<double, RevelationGradeArraySize>& weight) -> RevelationGrade
+        {
+            auto& [gradeStr, grade] = RevelationGradeArray[Random::Index(weight)];
+            return grade;
+        };
+
+        //임시 가중치 추후 변경 필요.
+        std::array<double, WeaponGradeArraySize> weaponTempWeight{};
+        weaponTempWeight.fill(1.0 / WeaponGradeArraySize);
+        std::array<double, RevelationGradeArraySize> revelationTempWeight{};
+        revelationTempWeight.fill(1.0 / RevelationGradeArraySize);
+
+        // 무기 랜덤 뽑기 함수
+        auto RollWeaponRandomItem = [&](int type) 
+        {
+            WeaponGrade grade      = RollWeaponRandomGrade(weaponTempWeight);
+            int         gradeIndex = static_cast<int>(grade);
+            auto&       itemTable  = weapons[type][gradeIndex];
+            if (false == itemTable.empty())
+            {
+                size_t     itemIndex  = Random::Index(itemTable.size());
+                IDropItem* randomItem = itemTable[itemIndex];
+                itemTable.erase(itemTable.begin() + itemIndex);
+                return randomItem->GetItemInfo();
+            }
+            else
+            {
+                DropItemInfo info{};
+                info.ID         = -1;
+                info.CategoryID = -1;
+                info.Name       = rfl::enum_to_string(grade);
+                info.Name += (const char*)u8" 등급 무기가 존재하지 않습니다.";
+                return info;
+            }
+        };
+        // 계시 랜덤 뽑기 함수
+        auto RollRevelationRandomItem = [&]() 
+        {
+            RevelationGrade grade      = RollRevelationRandomGrade(revelationTempWeight);
+            int             gradeIndex = static_cast<int>(grade);
+            auto&           itemTable  = revelations[gradeIndex];
+            if (false == itemTable.empty())
+            {
+                size_t     itemIndex  = Random::Index(itemTable.size());
+                IDropItem* randomItem = itemTable[itemIndex];
+                itemTable.erase(itemTable.begin() + itemIndex);
+                return randomItem->GetItemInfo();
+            }
+            else
+            {
+                DropItemInfo info{};
+                info.ID         = -1;
+                info.CategoryID = -1;
+                info.Name       = rfl::enum_to_string(grade);
+                info.Name       += (const char*)u8" 등급 계시가 존재하지 않습니다.";
+                return info;
+            }
+        };
+
+        switch (type)
+        {
+        case ArtifactDropType::SWORD:
+            artifact = RollWeaponRandomItem(static_cast<int>(WeaponType::SWORD));
+            break;
+        case ArtifactDropType::DAGGER:
+            artifact = RollWeaponRandomItem(static_cast<int>(WeaponType::DAGGER));
+            break;
+        case ArtifactDropType::WARHAMMER:
+            artifact = RollWeaponRandomItem(static_cast<int>(WeaponType::WARHAMMER));
+            break;
+        case ArtifactDropType::REVELATION:
+            artifact = RollRevelationRandomItem();
+            break;
+        default:
+            break;
+        }      
     }
     return artifacts;
 }
 
-void ItemDropSystem::SetDropItem(const std::vector<DropItemInfo>& itemInfos) 
+void ItemDropSystem::SetDropItem(const std::array<DropItemInfo, ARTIFACT_DROP_COUNT>& itemInfos)
 {
-    _dropItemsModel = itemInfos;
+    std::vector<DropItemInfo> dropItems(itemInfos.begin(), itemInfos.end());
+    _dropItemsModel = dropItems;
 }
 
 void ItemDropSystem::ImGuiDrawPropertysEvent() 
 {
     if (ImGui::TreeNode("Artifacts"))
     {
+        ImGuiDrawMaxDropCount();
+        ImGuiDrawTestRollArtifacts();   
+        ImGui::TreePop();
+    }
+}
+
+void ItemDropSystem::ImGuiDrawMaxDropCount() 
+{
+    auto TreeToolTip = []() { ImGuiHelper::HoveredToolTip(u8"한 스테이지에 카테고리별 드롭 횟수를 조절합니다."); };
+    if (ImGui::TreeNode("Max Drop Count"))
+    {
+        TreeToolTip();
+        int i = 0;
+        auto& MaxDropCount = ReflectFields->MaxDropCount;
+        for (auto& [str, value] : ArtifactDropTypeArray)
+        {
+            if (i < MaxDropCount.size())
+            {
+                if (ImGui::DragInt(str.data(), &MaxDropCount[i]))
+                {
+                    MaxDropCount[i] = std::clamp(MaxDropCount[i], 0, (int)ARTIFACT_DROP_COUNT);
+                }
+            }
+        }
+        ImGui::TreePop();
+    }
+    else
+    {
+        TreeToolTip();
+    }
+}
+
+void ItemDropSystem::ImGuiDrawTestRollArtifacts() 
+{
+    auto TreeToolTip = [](){ ImGuiHelper::HoveredToolTip(u8"아이템 보상 뽑기를 테스트합니다."); };
+    if (ImGui::TreeNode("Test Artifacts Drop"))
+    {
+        TreeToolTip();
         if (ImGui::Button("Roll Artifacts"))
         {
-            std::vector<DropItemInfo> artifacts = RollArtifacts();
+            std::array<DropItemInfo, ARTIFACT_DROP_COUNT> artifacts = RollArtifacts();
             SetDropItem(artifacts);
         }
+
         const auto& model = _dropItemsModel;
         for (auto& item : model)
         {
@@ -64,9 +239,28 @@ void ItemDropSystem::ImGuiDrawPropertysEvent()
         }
         ImGui::TreePop();
     }
+    else
+    {
+        TreeToolTip();
+    }
 }
 
-void ItemDropSystem::Awake() 
+void ItemDropSystem::SerializedReflectEvent() 
+{
+
+}
+
+void ItemDropSystem::DeserializedReflectEvent() 
+{
+
+}
+
+void ItemDropSystem::Reset() 
+{
+    ReflectFields->MaxDropCount.resize(ArtifactDropTypeArraySize);
+}
+
+void ItemDropSystem::Awake()
 {
     static_instance = this;
 }
--- Source/GA6thFinal_Framework/GameScripts/Scripts/ItemDropSystem/ItemDropSystem.h
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/ItemDropSystem/ItemDropSystem.h
@@ -2,12 +2,21 @@
 #include "UmFramework.h"
 #include "Interface/IDropItem.h"
 
+/* 유물 보상에 등장 가능한 아이템 타입 */
+enum class ArtifactDropType
+{
+    SWORD,     // 검
+    DAGGER,    // 단검
+    WARHAMMER, // 대형 망치
+    REVELATION // 계시
+};
+
 class ItemDropSystem : public Component
 {
     USING_PROPERTY(ItemDropSystem)
     inline static ItemDropSystem* static_instance = nullptr;
-
 public:
+    static const size_t ARTIFACT_TYPE_COUNT; // 유물 카테고리 개수
     static ItemDropSystem* GetInstance(std::source_location location = std::source_location::current()) 
     { 
         if (nullptr == static_instance)
@@ -22,29 +31,40 @@ public:
 
 public:
     /// <summary>
-    /// Artifact 아이템들을 랜덤으로 뽑아서 정보를 반환합니다.
+    /// Artifact 아이템들을 랜덤으로 뽑아서 정보들을 반환합니다.
     /// </summary>
-    std::vector<DropItemInfo> RollArtifacts();
-    
+    std::array<DropItemInfo, ARTIFACT_DROP_COUNT> RollArtifacts();
+
     /// <summary>
     /// 현재 보상 아이템을 설정합니다. UI도 갱신됩니다.
     /// </summary>
     /// <param name="itemInfos :"></param>
-    void SetDropItem(const std::vector<DropItemInfo>& itemInfos);
+    void SetDropItem(const std::array<DropItemInfo, ARTIFACT_DROP_COUNT>& itemInfos);
 
 public:
     REFLECT_PROPERTY()
 
 protected:
     REFLECT_FIELDS_BEGIN(Component)
-
+    std::vector<int> MaxDropCount; //카테고리별 드랍 가능한 최대 갯수
     REFLECT_FIELDS_END(ItemDropSystem)
 
 protected:
     void ImGuiDrawPropertysEvent() override;
+
+    // 카테고리별 드랍 가능 갯수 조정용 에디터 함수
+    void ImGuiDrawMaxDropCount(); 
+    // 유물 드랍뽑기를 테스트합니다.
+    void ImGuiDrawTestRollArtifacts();
+
+    void SerializedReflectEvent() override;
+    void DeserializedReflectEvent() override;
+
+    void Reset() override;
     void Awake() override;
 
 private:
     MVVM::Model<std::vector<DropItemInfo>> _dropItemsModel;
+
 
 };
