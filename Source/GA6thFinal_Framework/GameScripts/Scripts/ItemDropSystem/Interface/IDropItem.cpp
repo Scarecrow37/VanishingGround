46fb35e1d7655fc8f68f53c4cd66860e9f32f270
power12562 <kimsiwoo1005@gmail.com>
2025-09-09 17:19:14 +0900

    [FEATURE/VG-371] DropItemInfo에 GetArtifactCategoryAssetID 함수 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/ItemDropSystem/Interface/IDropItem.cpp
@@ -0,0 +1,12 @@
+﻿#include "pchScripts.h"
+#include "IDropItem.h"
+#include "ItemDropSystem/UI/ItemDropUIRootManager.h"
+
+int DropItemInfo::GetArtifactCategoryAssetID(ArtifactDropType type)
+{
+    if (ItemDropUIRootManager* uiRootManager = ItemDropUIRootManager::GetInstance())
+    {
+        return uiRootManager->GetArtifactCategoryAssetID(type);
+    }
+    return 0;
+}
