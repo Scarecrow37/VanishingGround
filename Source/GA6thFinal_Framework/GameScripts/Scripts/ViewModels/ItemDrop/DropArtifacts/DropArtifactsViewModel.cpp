03916639553cb4985239e14c64ef165c52496f87
power12562 <kimsiwoo1005@gmail.com>
2025-09-10 11:46:32 +0900

    [FIX/VG-371] public 상속으로 수정

--- Source/GA6thFinal_Framework/GameScripts/Scripts/ViewModels/ItemDrop/DropArtifacts/DropArtifactsViewModel.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/ViewModels/ItemDrop/DropArtifacts/DropArtifactsViewModel.cpp
@@ -10,24 +10,54 @@ DropArtifactsViewModel::DropArtifactsViewModel(MVVM::Model<std::vector<DropItemI
 
 }
 
-std::vector<DropArtifactsUIData> DropArtifactsViewModel::Convert(const std::vector<DropItemInfo>& value)
+namespace
 {
-    _uiDatas.clear();
+    void CheckDropArtifactsUIData(const std::string& name, const DropArtifactsUIData& datas)
+    {
+        if (datas.Category.IsNull())
+        {
+            std::u8string message = (const char8_t*)name.data();
+            message += u8" 카테고리 UI가 존재하지 않습니다.";
+            UmLogger.Log(LogLevel::LEVEL_WARNING, message);
+        }
+        if (datas.Artifact.IsNull())
+        {
+            std::u8string message = (const char8_t*)name.data();
+            message += u8" 유물 UI가 존재하지 않습니다.";
+            UmLogger.Log(LogLevel::LEVEL_WARNING, message);
+        }
+    }
+}
+
+std::vector<DropArtifactsUIData> DropArtifactsViewModel::ConvertData(const std::vector<DropItemInfo>& value)
+{
+    std::vector<DropArtifactsUIData> datas;
     ItemDropUIRootManager* uiRootManager = ItemDropUIRootManager::GetInstance();
     if (uiRootManager)
     {
+        File::Path framePath = (std::string)uiRootManager->ArtifactsUIFrameAsset;
+        if (framePath.IsNull())
+        {
+            UmLogger.Log(LogLevel::LEVEL_WARNING, u8"UI Root Manager에 Artifacts UI FrameAsset을 설정해주세요.");
+        }
         for (auto& item : value)
         {
-            File::Path framePath = (std::string)uiRootManager->ArtifactsUIFrameAsset;
             DropArtifactsUIData data
             {
                 .Frame    = framePath.ToGuid(),
-                .Artifact = UmFileSystem.GetPathFromAssetID(item.ID).ToGuid(), 
-                .Category = UmFileSystem.GetPathFromAssetID(item.CategoryID).ToGuid(), 
+                .Artifact = UmFileSystem.GetPathFromAssetID(item.ID).ToGuid(),
+                .Category = UmFileSystem.GetPathFromAssetID(item.CategoryID).ToGuid(),
             };
-            _uiDatas.push_back(data);
+            CheckDropArtifactsUIData(item.Name, data);        
+            datas.push_back(data);
         }
     } 
+    return datas;
+}
+
+std::vector<DropArtifactsUIData> DropArtifactsViewModel::Convert(const std::vector<DropItemInfo>& value)
+{
+    _uiDatas = ConvertData(value);
     return _uiDatas;
 }
 
--- Source/GA6thFinal_Framework/GameScripts/Scripts/ViewModels/ItemDrop/DropArtifacts/DropArtifactsViewModel.h
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/ViewModels/ItemDrop/DropArtifacts/DropArtifactsViewModel.h
@@ -8,13 +8,15 @@ struct DropArtifactsUIData
     File::Guid Category;
 };
 
-class DropArtifactsViewModel final : MVVM::ViewModel<std::vector<DropItemInfo>, std::vector<DropArtifactsUIData>>
+class DropArtifactsViewModel final : public MVVM::ViewModel<std::vector<DropItemInfo>, std::vector<DropArtifactsUIData>>
 {
 public:
     explicit DropArtifactsViewModel(MVVM::Model<std::vector<DropItemInfo>>& model);
 
+    static std::vector<DropArtifactsUIData> ConvertData(const std::vector<DropItemInfo>& value);
     std::vector<DropArtifactsUIData> Convert(const std::vector<DropItemInfo>& value) override;
 
 private:
     std::vector<DropArtifactsUIData> _uiDatas;
+
 };
