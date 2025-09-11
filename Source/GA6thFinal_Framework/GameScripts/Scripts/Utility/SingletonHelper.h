a37e8688ab6210decfb20817df2f5a59ad86c039
power12562 <kimsiwoo1005@gmail.com>
2025-09-11 18:44:22 +0900

    [CHORE/VG-28] 싱글톤 오타 수정

--- Source/GA6thFinal_Framework/GameScripts/Scripts/Utility/SingletonHelper.h
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/Utility/SingletonHelper.h
@@ -5,33 +5,33 @@
 template <typename T>
 class SingletonObject
 {
-    inline static GameObject* SingleToneObject = nullptr;
+    inline static GameObject* SingletonObject = nullptr;
 public:
     inline static GameObject* GetInstance(std::source_location location = std::source_location::current()) 
     { 
-        if (nullptr == SingleToneObject)
+        if (nullptr == SingletonObject)
         {
             std::string message = typeid(SingletonObject<T>).name();
             message += (const char*)"가 존재하지 않습니다.";
             UmLogger.Log(LogLevel::LEVEL_WARNING, message, location);
         }
-        return SingleToneObject; 
+        return SingletonObject; 
     }
 
 public:
     SingletonObject(T* owner) { _owner = owner; }
     ~SingletonObject()
     {
-        if (&_owner->gameObject == SingleToneObject)
+        if (&_owner->gameObject == SingletonObject)
         {
-            SingleToneObject = nullptr;
+            SingletonObject = nullptr;
         }
     }
 
     /// <summary>
     /// 이 오브젝트의 싱글톤 여부를 반환합니다.
     /// </summary>
-    bool IsSingleTon() { return &_owner->gameObject == SingleToneObject; }
+    bool IsSingleTon() { return &_owner->gameObject == SingletonObject; }
 
     /// <summary>
     /// owner의 오브젝트를 싱글톤으로 설정합니다. (에디터 모드일때만 적용됩니다.)
@@ -40,7 +40,7 @@ public:
     {
         if (false == Global::IsPlay())
         {
-            SingleToneObject = &_owner->gameObject;
+            SingletonObject = &_owner->gameObject;
         }
     }
 
@@ -53,12 +53,12 @@ public:
     {
         if (true == Global::IsPlay())
         {
-            if (nullptr == SingleToneObject || SingleToneObject == &_owner->gameObject)
+            if (nullptr == SingletonObject || SingletonObject == &_owner->gameObject)
             {
-                SingleToneObject = &_owner->gameObject;
+                SingletonObject = &_owner->gameObject;
                 if (dontDestroyOnLoad)
                 {
-                    GameObject::DontDestroyOnLoad(SingleToneObject);
+                    GameObject::DontDestroyOnLoad(SingletonObject);
                 }
                 return true;
             }
@@ -79,27 +79,27 @@ private:
 template <typename T>
 class SingletonComponent
 {
-    inline static T* SingleToneComponent = nullptr;
+    inline static T* SingletonComponent = nullptr;
 
 public:
     inline static T* GetInstance(std::source_location location = std::source_location::current()) 
     {
-        if (nullptr == SingleToneComponent)
+        if (nullptr == SingletonComponent)
         {
             std::string message = typeid(T).name();
             message += (const char*)"가 존재하지 않습니다.";
             UmLogger.Log(LogLevel::LEVEL_WARNING, message, location);
         }
-        return SingleToneComponent; 
+        return SingletonComponent; 
     }
 
 public:
     SingletonComponent(T* owner) { _owner = owner; }
     ~SingletonComponent()
     {
-        if (_owner == SingleToneComponent)
+        if (_owner == SingletonComponent)
         {
-            SingleToneComponent = nullptr;
+            SingletonComponent = nullptr;
         }
     }
     
@@ -107,7 +107,7 @@ public:
     /// 이 컴포넌트의 싱글톤 여부를 반환합니다.
     /// </summary>
     /// <returns>결과 bool</returns>
-    bool IsSingleTon() { return _owner == SingleToneComponent; }
+    bool IsSingleTon() { return _owner == SingletonComponent; }
       
     /// <summary>
     /// owner의 오브젝트를 싱글톤으로 설정합니다. (Reset에서 호출해야합니다.)
@@ -116,7 +116,7 @@ public:
     {
         if (false == Global::IsPlay())
         {
-            SingleToneComponent = _owner;
+            SingletonComponent = _owner;
         }
     }
 
@@ -128,9 +128,9 @@ public:
     {
         if (true == Global::IsPlay())
         {
-            if (nullptr == SingleToneComponent)
+            if (nullptr == SingletonComponent)
             {
-                SingleToneComponent = _owner;
+                SingletonComponent = _owner;
                 return true;
             }
             else
