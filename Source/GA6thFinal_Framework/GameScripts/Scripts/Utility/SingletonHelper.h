ba4473818aa76f4f54bc4fc1f8ad6973ebee07dd
power12562 <kimsiwoo1005@gmail.com>
2025-09-11 15:18:47 +0900

    [FEATURE/VG-370] Singleton 설정 도와주는 헬퍼 템플릿 클래스 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/Utility/SingletonHelper.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+#include "UmFramework.h"
+
+/*이 클래스를 맴버로 들고있는 오브젝트는 DontDestroyOnLoad를 통한 싱글톤으로 만듭니다.*/
+template <typename T>
+class SingletonObject
+{
+    inline static GameObject* SingleToneObject = nullptr;
+public:
+    inline static GameObject* GetInstance() { return SingleToneObject; }
+
+public:
+    SingletonObject(T* owner) { _owner = owner; }
+    ~SingletonObject()
+    {
+        if (&_owner->gameObject == SingleToneObject)
+        {
+            SingleToneObject = nullptr;
+        }
+    }
+
+    /// <summary>
+    /// owner의 오브젝트를 싱글톤으로 설정합니다.
+    /// </summary>
+    void SetSingleTone()
+    {
+        SingleToneObject = &_owner->gameObject;
+    }
+
+    /// <summary>
+    /// owner의 오브젝트를 싱글톤으로 만듭니다. DontDestroyOnLoad로 등록되면 true를 반환합니다.
+    /// </summary>
+    /// <returns>성공 여부</returns>
+    bool TrySingleTone()
+    {
+        if (Global::IsPlay())
+        {
+            if (nullptr == SingleToneObject)
+            {
+                SingleToneObject = &_owner->gameObject;
+                GameObject::DontDestroyOnLoad(SingleToneObject);
+                return true;
+            }
+            else
+            {
+
+                std::string message = typeid(T).name();
+                message += (const char*)u8"는 싱글톤 오브젝트 입니다.";
+                UmLogger.Log(LogLevel::LEVEL_WARNING, message);
+            }
+        }
+        GameObject::Destroy(_owner->gameObject);
+        return false;
+    }
+
+private:
+    T* _owner = nullptr;
+};
+
