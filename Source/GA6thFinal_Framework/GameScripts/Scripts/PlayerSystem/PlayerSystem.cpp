5daa0ba5bbbbf9ef8980f2130c2ad003ced52a55
power12562 <kimsiwoo1005@gmail.com>
2025-09-11 15:18:19 +0900

    [FEATURE/VG-370] 플레이어 시스템 싱글톤용 컴포넌트 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/PlayerSystem/PlayerSystem.cpp
@@ -0,0 +1,67 @@
+﻿#include "pchScripts.h"
+#include "PlayerSystem.h"
+#include "WeaponSystem/WeaponSystem.h"
+#include "WeaponSystem/WeaponTable/WeaponTableComponent.h"
+#include "RevelationSystem/RevelationSystem.h"
+#include "ItemDropSystem/ItemDropSystem.h"
+#include "Stats/Player/PlayerStatsComponent.h"
+
+PlayerSystem::PlayerSystem() = default;
+PlayerSystem::~PlayerSystem() = default;
+
+void PlayerSystem::Reset() 
+{
+    _singletonObject.SetSingleTone();
+    CheckSystem();
+}
+
+void PlayerSystem::Awake()
+{
+    if (_singletonObject.TrySingleTone())
+    {
+       
+    }
+}
+
+namespace
+{
+    template<typename T>
+    void CheckMessage(T* ptr)
+    {
+        std::string message;
+        message = typeid(T).name();
+        if (nullptr == ptr)
+        {
+            message += (const char*)u8" 컴포넌트를 추가해주세요";
+        }
+        else
+        {
+            message += " ok";
+        }
+        ImGui::Text(message.c_str());
+    }
+}
+
+void PlayerSystem::ImGuiDrawPropertysEvent() 
+{
+    if (ImGui::Button("Check System"))
+    {
+        CheckSystem();
+    }
+    CheckMessage(_weaponSystem);
+    CheckMessage(_weaponTableComponent);
+    CheckMessage(_revelationSystem);
+    CheckMessage(_itemDropSystem);
+    CheckMessage(_playerStatsComponent);
+}
+
+void PlayerSystem::CheckSystem() 
+{
+    _weaponSystem         = GetComponent<WeaponSystem>();
+    _weaponTableComponent = GetComponent<WeaponTableComponent>();
+    _revelationSystem     = GetComponent<RevelationSystem>();
+    _itemDropSystem       = GetComponent<ItemDropSystem>();
+    _playerStatsComponent = GetComponent<PlayerStatsComponent>();
+}
+
+
