9a268ac2773555d3e38cdd7ee3fbd2115b185bf1
power12562 <kimsiwoo1005@gmail.com>
2025-09-12 12:18:12 +0900

    [FEATURE/VG-412] 장신구 테이블 기능 추가

--- Source/GA6thFinal_Framework/GameScripts/Scripts/AccessorySystem/AccessorySystem.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/AccessorySystem/AccessorySystem.cpp
@@ -1,4 +1,60 @@
-#include "pchScripts.h"
+﻿#include "pchScripts.h"
 #include "AccessorySystem.h"
-AccessorySystem::AccessorySystem() = default;
-AccessorySystem::~AccessorySystem() = default;
+#include "TurnSystem/TurnAction/TurnActionFactory.h"
+
+AccessorySystem::AccessorySystem()
+{
+
+}
+
+AccessorySystem::~AccessorySystem()
+{
+
+}
+
+void AccessorySystem::ImGuiDrawPropertysEvent() 
+{
+
+}
+
+void AccessorySystem::SerializedReflectEvent() 
+{
+
+}
+
+void AccessorySystem::DeserializedReflectEvent() 
+{
+
+}
+
+void AccessorySystem::Reset()
+{
+    _singletonComponent.SetSingleTon();
+}
+
+void AccessorySystem::Awake() 
+{
+    if (_singletonComponent.TrySingleTon())
+    {
+
+    }
+}
+
+void AccessorySystem::ElementTableSerialized() 
+{
+    ReflectFields->ElementTableData.clear();
+    for (auto& [key, element] : _elementTable)
+    {
+        ReflectFields->ElementTableData.emplace_back(key, element.SerializedReflectFields());
+    }
+}
+
+void AccessorySystem::ElementTableDeserialized() 
+{
+    _elementTable.clear();
+    for (auto& [key, data] : ReflectFields->ElementTableData)
+    {
+        AccessoryElement element;
+        element.DeserializedReflectFields(data);
+    }
+}
--- Source/GA6thFinal_Framework/GameScripts/Scripts/AccessorySystem/AccessorySystem.h
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/AccessorySystem/AccessorySystem.h
@@ -1,5 +1,8 @@
-#pragma once
+﻿#pragma once
 #include "UmFramework.h"
+#include "Utility/SingletonHelper.h"
+#include "AccessoryElement/AccessoryElement.h"
+
 class AccessorySystem : public Component
 {
     USING_PROPERTY(AccessorySystem)
@@ -13,5 +16,37 @@ public:
 
 protected:
     REFLECT_FIELDS_BEGIN(Component)
+    std::vector<std::pair<std::string, std::string>> ElementTableData;
     REFLECT_FIELDS_END(AccessorySystem)
+
+    /// <summary>
+    /// <para>  ImGuiDrawPropertys() 호출 이후 콜되는 이벤트 함수입니다. </para>
+    /// </summary>
+    void ImGuiDrawPropertysEvent() override;
+
+    /// <summary>
+    /// <para> 직렬화 직전 자동으로 호출되는 이벤트 함수입니다. </para>
+    /// <para> 직접 override 해서 사용합니다.                 </para>
+    /// </summary>
+    void SerializedReflectEvent() override;
+
+    /// <summary>
+    /// <para> 역직렬화 이후 자동으로 호출되는 이벤트 함수 입니다.  </para>
+    /// <para> 직접 override 해서 사용합니다.                     </para>
+    /// </summary>
+    void DeserializedReflectEvent() override;
+
+    void Reset() override;
+    void Awake() override;
+
+private:
+    SingletonComponent<AccessorySystem> _singletonComponent{this};
+
+    std::map<std::string, AccessoryElement> _elementTable;        // 이름-액세서리 테이블
+    std::vector<AccessoryElement*>          _elementTableOrderID; // 아이디 순 정렬된 테이블
+
+private:
+    void ElementTableSerialized();
+    void ElementTableDeserialized();
+
 };
