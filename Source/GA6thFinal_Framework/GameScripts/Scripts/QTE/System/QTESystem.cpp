07d962a1a113620f07863d5ea74fa165a4219795
PPAK_JU <taku7664@gmail.com>
2025-09-12 12:56:51 +0900

    [CHORE/VG-373] 예외 처리 수정 및 주석 추가

--- Source/GA6thFinal_Framework/GameScripts/Scripts/QTE/System/QTESystem.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/QTE/System/QTESystem.cpp
@@ -10,22 +10,31 @@ QTESystem::QTESystem()
 
 QTESystem::~QTESystem() 
 {
-    if (this == _staticInstance)
+    for (auto& [weaponID, trackList] : _weaponIDToTrackTable)
     {
-        _staticInstance = nullptr;
+        for (auto& track : trackList)
+        {
+            if (track)
+            {
+                delete track;
+            }
+        }
     }
 }
 
 void QTESystem::Reset()
 {
-    _staticInstance = this;
+    _singletonComponent.SetSingleTon();
 }
 
 void QTESystem::Awake()
 {
-    BindInputAction(ControllerButton::X, Action::PRESSED, this, this, &QTESystem::PressedButtonX);
-    BindInputAction(ControllerButton::Y, Action::PRESSED, this, this, &QTESystem::PressedButtonY);
-    BindInputAction(ControllerButton::B, Action::PRESSED, this, this, &QTESystem::PressedButtonB);
+    if (_singletonComponent.TrySingleTon())
+    {
+        BindInputAction(ControllerButton::X, Action::PRESSED, this, this, &QTESystem::PressedButtonX);
+        BindInputAction(ControllerButton::Y, Action::PRESSED, this, this, &QTESystem::PressedButtonY);
+        BindInputAction(ControllerButton::B, Action::PRESSED, this, this, &QTESystem::PressedButtonB);
+    }
 }
 
 void QTESystem::Update()
@@ -117,10 +126,11 @@ bool QTESystem::AddMappingTrackToWeaponID(int weaponID, const File::Path& path)
     trackVector.push_back(track);
     track->SetWeaponID(weaponID);
 
-    if (File::NULL_PATH != path)
+    // 기본 경로가 아닌 경우 파일 로드 시도
+    if (File::NULL_PATH != path && false == track->LoadFile(path))
     {
         UmLogger.Log(LogLevel::LEVEL_ERROR, (const char*)u8"QTE 트랙 파일 로드에 실패했습니다.");
-        track->LoadFile(path);
+        return false;
     }
     return true;
 }
@@ -178,10 +188,13 @@ void QTESystem::StartQTE()
         auto  itr    = _weaponIDToTrackTable.find(weapon.WeaponID);
         if (itr != _weaponIDToTrackTable.end() && false == itr->second.empty())
         {
-            // 여러 트랙이 있는 경우 랜덤 선택
-            int index = Random::Range(0, (int)itr->second.size() - 1);
-            _currentQTETrack = itr->second[index];
-            StartQTE(_currentQTETrack);
+            auto& trackVector = itr->second;
+            if (false == trackVector.empty())
+            {
+                int index        = Random::Range(0, (int)trackVector.size() - 1);
+                _currentQTETrack = trackVector[index];
+                StartQTE(_currentQTETrack);
+            }
         }
     }
 }
@@ -197,9 +210,9 @@ void QTESystem::StartQTE(QTE::Track* qteTrack)
     }
 
     _isQTEPlaying = true;
+    _currentNoteIndex = 0;
     _noteAvailQueue.clear();
     _noteResultQueue.clear();
-    _currentNoteIndex = 0;
 
     if (qteTrack)
     {
@@ -239,14 +252,17 @@ bool QTESystem::IsQTEDelayEnd()
 
 bool QTESystem::IsQTETimeEnd()
 {
-    auto track = _currentQTETrack->GetEventTrack().lock();
-    if (track && _currentQTETrack)
+    if (_currentQTETrack)
     {
-        float minFrame = track->GetMinFrame();
-        float maxFrame = track->GetMaxFrame();
-        if (_qteTimer >= maxFrame)
+        auto track = _currentQTETrack->GetEventTrack().lock();
+        if (track)
         {
-            return true;
+            float minFrame = track->GetMinFrame();
+            float maxFrame = track->GetMaxFrame();
+            if (_qteTimer >= maxFrame)
+            {
+                return true;
+            }
         }
     }
     return false;
@@ -254,11 +270,11 @@ bool QTESystem::IsQTETimeEnd()
 
 QTE::ResultType QTESystem::GetQTEResult(QTE::Note* note)
 {
-    auto& [perfectMin, perfectMax] = ReflectFields->PerfectJudgeRange;
-    auto& [normalMin, normalMax]   = ReflectFields->NormalJudgeRange;
     if (note)
     {
-        float noteDelta = note->Time - _qteTimer;
+        auto& [perfectMin, perfectMax]  = ReflectFields->PerfectJudgeRange;
+        auto& [normalMin, normalMax]    = ReflectFields->NormalJudgeRange;
+        float noteDelta                 = note->Time - _qteTimer;
         if (noteDelta >= perfectMin && noteDelta <= perfectMax)
         {
             UmLogger.Log(LogLevel::LEVEL_DEBUG, (const char*)u8"퍼펙트!!");
@@ -288,15 +304,7 @@ void QTESystem::UpdateQTEDelay()
 {
     if (_delayTimer > 0.0f)
     {
-        float deltaTime = UmTime.DeltaTime();
-        float scale     = ReflectFields->QTESpeedScale;
-        if (_currentQTETrack)
-        {
-            scale *= _currentQTETrack->GetQTESpeedScale();
-        }
-        deltaTime *= scale;
-
-        _delayTimer -= deltaTime;
+        _delayTimer -= UmTime.DeltaTime();
     }
     else
     {
@@ -306,32 +314,33 @@ void QTESystem::UpdateQTEDelay()
 
 void QTESystem::UpdateQTETrack()
 {
-    auto track = _currentQTETrack->GetEventTrack().lock();
-    if (track && _currentQTETrack)
+    if (_isQTEPlaying && _currentQTETrack)
     {
-        float minFrame = track->GetMinFrame();
-        float maxFrame = track->GetMaxFrame();
-
-        float scale     = ReflectFields->QTESpeedScale * _currentQTETrack->GetQTESpeedScale();
-        float deltaTime = UmTime.DeltaTime() * scale;
-        _qteTimer += deltaTime;
-
-        if (_qteTimer < maxFrame && _currentNoteIndex < _noteAvailQueue.size())
+        auto track = _currentQTETrack->GetEventTrack().lock();
+        if (track)
         {
-            QTE::Note* curNote = _noteAvailQueue[_currentNoteIndex];
-            if (curNote)
+            _qteTimer += UmTime.DeltaTime();
+
+            float minFrame = track->GetMinFrame();
+            float maxFrame = track->GetMaxFrame();
+
+            if (_qteTimer < maxFrame && _currentNoteIndex < _noteAvailQueue.size())
             {
-                float noteTime = curNote->Time;
-                auto& [normalMin, normalMax] = ReflectFields->NormalJudgeRange;
-                if (_qteTimer > noteTime + normalMax)
-                {  
-                    PressedQTEButton();  // 최대 일격 판정 시간이 지나갔는데 버튼을 누르지 않은 경우, MISS 처리
+                QTE::Note* curNote = _noteAvailQueue[_currentNoteIndex];
+                if (curNote)
+                {
+                    float noteTime               = curNote->Time;
+                    auto& [normalMin, normalMax] = ReflectFields->NormalJudgeRange;
+                    if (_qteTimer > noteTime + normalMax)
+                    {
+                        PressedQTEButton(); // 최대 일격 판정 시간이 지나갔는데 버튼을 누르지 않은 경우, MISS 처리
+                    }
                 }
             }
-        }
-        else
-        {
-            _isQTEPlaying = false;
+            else
+            {
+                _isQTEPlaying = false;
+            }
         }
     }
 }
@@ -350,14 +359,14 @@ void QTESystem::PressedQTEButton(Input::ControllerTypes::Button type)
         {
             return;
         }
-        QTE::Note*   curNote        = _noteAvailQueue[_currentNoteIndex];
-        QTE::Result& result         = _noteResultQueue[_currentNoteIndex];
+        QTE::Note*   curNote    = _noteAvailQueue[_currentNoteIndex];
+        QTE::Result& result     = _noteResultQueue[_currentNoteIndex];
         ++_currentNoteIndex;
 
-        result.Note                 = curNote;
-        result.PressedButton        = type;
-        result.ResultType           = GetQTEResult(curNote);
-        result.TimeDelta            = curNote ? _qteTimer - curNote->Time : 0.0f;
+        result.Note             = curNote;
+        result.PressedButton    = type;
+        result.ResultType       = GetQTEResult(curNote);
+        result.TimeDelta        = curNote ? _qteTimer - curNote->Time : 0.0f;
 
         QTEPreviewer::PressedNote(&result);
     }
--- Source/GA6thFinal_Framework/GameScripts/Scripts/QTE/System/QTESystem.h
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/QTE/System/QTESystem.h
@@ -15,11 +15,10 @@ namespace QTE
 class QTESystem : public Component, public InputReceiver
 {
     USING_PROPERTY(QTESystem)
-    inline static QTESystem* _staticInstance = nullptr;
+
 public:
     QTESystem();
     virtual ~QTESystem();
-    inline static QTESystem* GetInstance() { return _staticInstance; }
 
 private:
     void Reset() override;
@@ -31,8 +30,28 @@ private:
     void ImGuiDrawPropertysEvent() override;
 
 public:
+    /// <summary>
+    /// 무기 ID에 매핑 트랙을 추가합니다. path인자를 NULL_PATH로 지정하면 빈 트랙을 생성합니다.
+    /// </summary>
+    /// <param name="weaponID">매핑 트랙을 추가할 무기의 ID입니다.</param>
+    /// <param name="path">추가할 트랙의 파일 경로입니다.</param>
+    /// <returns>매핑 트랙 추가가 성공하면 true, 실패하면 false를 반환합니다.</returns>
     bool        AddMappingTrackToWeaponID(int weaponID, const File::Path& path = File::NULL_PATH);
+
+    /// <summary>
+    /// weaponID와 관련된 매핑 트랙을 제거합니다. index가 -1이면 마지막 매핑 트랙을 제거합니다.
+    /// </summary>
+    /// <param name="weaponID">매핑을 제거할 무기의 ID입니다.</param>
+    /// <param name="index">제거할 매핑 트랙의 인덱스입니다. 기본값은 -1로, 마지막 매핑 트랙을 제거합니다.</param>
+    /// <returns>매핑이 성공적으로 제거되면 true를 반환하고, 그렇지 않으면 false를 반환합니다.</returns>
     bool        RemoveMappingTrackToWeaponID(int weaponID, int index = -1);
+
+    /// <summary>
+    /// weaponID와 선택적 인덱스에 해당하는 매핑된 트랙을 반환합니다.
+    /// </summary>
+    /// <param name="weaponID">매핑할 무기의 ID입니다.</param>
+    /// <param name="index">매핑된 트랙의 인덱스(기본값은 0)입니다.</param>
+    /// <returns>QTE::Track 객체에 대한 포인터를 반환합니다. 해당 weaponID와 인덱스에 매핑된 트랙이 없으면 nullptr일 수 있습니다.</returns>
     QTE::Track* GetMappingTrackToWeaponID(int weaponID, int index = 0);
 
     void StartQTE();
@@ -85,6 +104,8 @@ public:
     inline size_t GetCurrentNoteIndex() const { return _currentNoteIndex; }
 
 private:
+    SingletonComponent<QTESystem> _singletonComponent{this};
+
     std::unordered_map<int, std::vector<QTE::Track*>> _weaponIDToTrackTable;    // 무기 ID QTE 매핑 테이블
 
     std::vector<QTE::Note*>     _noteAvailQueue;                                // 유효한 노트 큐
