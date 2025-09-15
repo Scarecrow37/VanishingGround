10b4c9ad108961199236b8e0fa4563804bda5736
Do_Suha <weqa123@naver.com>
2025-09-15 20:11:12 +0900

    [FEATURE/VG-410] Model.h에 인덱스 접근 메서드 추가
    MVVM 네임스페이스의 Model.h 파일에서 `operator[]`와 `at` 메서드를 추가하여 인덱스를 통한 값 접근 및 수정 기능을 구현했습니다. 또한, `clear` 메서드를 추가하여 컨테이너의 모든 요소를 제거하고, 변경 사항을 알리기 위해 `Notify` 메서드를 호출합니다.

--- Source/GA6thFinal_Framework/UserInterfaceModule/Model.h
+++ Source/GA6thFinal_Framework/UserInterfaceModule/Model.h
@@ -133,6 +133,8 @@ namespace MVVM
             return *this;
         }
 
+        const_reference operator[](size_type pos) const { return ModelBase<container_type>::_value[pos]; }
+
         void clear() noexcept
         {
             ModelBase<container_type>::_value.clear();
@@ -185,6 +187,8 @@ namespace MVVM
             ModelBase<container_type>::Notify();
             return size;
         }
+
+        const_reference at(size_type pos) const { return ModelBase<container_type>::_value.at(pos); }
 
         template <typename Modifier>
         void at(size_type pos, Modifier modifier)
