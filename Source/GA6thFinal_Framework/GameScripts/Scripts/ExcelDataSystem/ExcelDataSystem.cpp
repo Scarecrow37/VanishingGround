451c939501937854b457939099df7489771809e5
power12562 <kimsiwoo1005@gmail.com>
2025-09-16 15:23:54 +0900

    [FEATURE/VG-316] Excel Data Center 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/ExcelDataSystem/ExcelDataSystem.cpp
@@ -0,0 +1,66 @@
+﻿#include "pchScripts.h"
+#include "ExcelDataSystem.h"
+ExcelDataSystem::ExcelDataSystem() 
+    : 
+    _excelParser{"64F8C1F9-344D-4D36-A232-47E68DA36134", u8"ID"} 
+{
+
+}
+
+ExcelDataSystem::~ExcelDataSystem()
+{
+
+}
+
+void ExcelDataSystem::ImGuiDrawPropertysEvent() 
+{
+    if (ImGui::Button("Excel parser"))
+    {
+        _excelParser.ShowParser = true;
+    }
+    ImGuiDrawExcelParserEdit();
+}
+
+void ExcelDataSystem::ImGuiDrawExcelParserEdit()
+{
+#ifdef _UMEDITOR
+    if (_excelParser.Draw())
+    {
+        const std::string& selectSheetName = _excelParser.GetSelectSheetName();
+        size_t rawCount    = _excelParser.GetRawCount();
+        size_t columnCount = _excelParser.GetColumnCount();
+        DataBaseType& dataBase = ReflectFields->DataBase;
+        std::pair<KeyIndexMapType, DataSheetType>& dataSheetPair = dataBase[selectSheetName];
+        auto& [keyIndexToMap, dataSheet] = dataSheetPair;
+        keyIndexToMap.clear();
+        dataSheet.clear();
+        size_t dataIndex = 0;
+        _excelParser.Apply([&](const ImGuiColumnSheetParser::ColumnDatas& datas) 
+        { 
+            bool isEmptyColumn = true;
+            for (auto& pair : datas)
+            {
+                const std::string& key = pair.first;
+                const std::string& data = pair.second;
+                if (false == key.empty())
+                {
+                    isEmptyColumn  = data.empty();
+                    auto& indexMap = keyIndexToMap[key];
+                    indexMap.try_emplace(data, dataIndex);
+                    if (dataSheet.size() <= dataIndex)
+                    {
+                        dataSheet.resize(dataIndex + 1);
+                    }
+                    dataSheet[dataIndex].push_back(data);
+                }
+            }
+            if (isEmptyColumn)
+            {
+
+            }
+            ++dataIndex;
+        });
+        _excelParser.ShowParser = true;
+    }
+#endif 
+}
---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/ExcelDataSystem/ExcelDataSystem.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include "UmFramework.h"
+#include "ExcelParser/ImGuiColumnSheetParser.h"
+
+class ExcelDataSystem : public Component
+{
+    USING_PROPERTY(ExcelDataSystem)
+
+public:
+    ExcelDataSystem();
+    ~ExcelDataSystem() override;
+
+public:
+    REFLECT_PROPERTY()
+
+protected:
+    using KeyIndexMapType = std::unordered_map<std::string, std::unordered_map<std::string, size_t>>;
+    using DataSheetType   = std::vector<std::vector<std::string>>;
+    using DataBaseType    = std::unordered_map<std::string, std::pair<KeyIndexMapType, DataSheetType>>;
+    REFLECT_FIELDS_BEGIN(Component)
+    DataBaseType DataBase;
+    REFLECT_FIELDS_END(ExcelDataSystem)
+
+    void ImGuiDrawPropertysEvent() override;
+
+private:
+#ifdef _UMEDITOR
+    ImGuiColumnSheetParser _excelParser;
+#endif
+    void ImGuiDrawExcelParserEdit();
+
+};
