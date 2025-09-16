773c4efd0e486a7951d61541b8cd599104b09d16
power12562 <kimsiwoo1005@gmail.com>
2025-09-16 11:51:53 +0900

    [FEATURE/VG-371] consumable system 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/ConsumableSystem/ConsumableSystem.cpp
@@ -0,0 +1,30 @@
+﻿#include "pchScripts.h"
+#include "ConsumableSystem.h"
+
+ConsumableSystem::ConsumableSystem()
+{
+
+}
+
+ConsumableSystem::~ConsumableSystem()
+{
+
+}
+
+void ConsumableSystem::Reset() 
+{
+    _singletonComponent.SetSingleTon();
+}
+
+void ConsumableSystem::Awake() 
+{
+    if (_singletonComponent.TrySingleTon())
+    {
+
+    }
+}
+
+void ConsumableSystem::ImGuiDrawPropertysEvent() 
+{
+
+}
