1f20fdeb9d64b17af00dfcdca01f6674e95d9d8f
SeongU <blacknaco85@gmail.com>
2025-09-17 21:25:29 +0900

    [FIX/VG-375] 리뷰 반영

--- Source/GA6thFinal_Framework/GameScripts/Scripts/Preferences/PreferencesManager.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/Preferences/PreferencesManager.cpp
@@ -17,6 +17,8 @@ void PreferencesManager::Reset()
 void PreferencesManager::Awake()
 {
     auto pannel = GameObject::Find("PreferencesPannel");
+    if (nullptr == pannel.lock())
+        UmLogger.Log(LogLevel::LEVEL_ERROR, "환경설정 패널이 없습니다!");
     pannel.lock()->SetActive(false);
 }
 
@@ -27,11 +29,15 @@ void PreferencesManager::Update()
 void PreferencesManager::OnPreferencesWindow(const Input::Controller&) 
 {
     auto pannel = GameObject::Find("PreferencesPannel");
+    if (nullptr == pannel.lock())
+        UmLogger.Log(LogLevel::LEVEL_ERROR, "환경설정 패널이 없습니다!");
     pannel.lock()->SetActive(true);
 }
 
 void PreferencesManager::OffPreferencesWindow(const Input::Controller&)
 {
     auto pannel = GameObject::Find("PreferencesPannel");
+    if (nullptr == pannel.lock())
+        UmLogger.Log(LogLevel::LEVEL_ERROR, "환경설정 패널이 없습니다!");
     pannel.lock()->SetActive(false);
 }
