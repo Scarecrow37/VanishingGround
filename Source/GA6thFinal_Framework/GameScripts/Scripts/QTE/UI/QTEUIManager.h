644004074055c9044f4eb8bc21862a226b98f051
PPAK_JU <taku7664@gmail.com>
2025-09-18 14:33:55 +0900

    [CHORE/VG-373] 주석 추가

--- Source/GA6thFinal_Framework/GameScripts/Scripts/QTE/UI/QTEUIManager.h
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/QTE/UI/QTEUIManager.h
@@ -31,6 +31,15 @@ public:
     void OnQTEStay();
     void OnQTEExit();
 
+public:
+    /// <summary>노트 프리팹의 GUID를 설정합니다. 해당 프리팹을 통해 QTE 노트 UI가 생성됩니다.</summary>
+    /// <param name="guid">설정할 File::Guid 객체입니다.</param>
+    void SetNotePrefabGuid(const File::Guid& guid);
+
+    /// <summary>QTE 관련 UI의 알파 값을 설정합니다.</summary>
+    /// <param name="factor">설정할 UI의 알파 값입니다. 0.0f에서 1.0f 사이의 값을 가집니다.</param>
+    void SetUIAlpha(float factor);
+
 private:
     void Reset() override;
     void Awake() override;
@@ -43,10 +52,6 @@ private:
     void SerializedReflectEvent() override;
     void DeserializedReflectEvent() override;
     void ImGuiDrawPropertysEvent() override;
-
-public:
-    void SetNotePrefabGuid(const File::Guid& guid);
-    void SetUIAlpha(float factor);
 
 private:
     void UpdateUITransformData();
@@ -95,6 +100,7 @@ private:
         /// <summary>페이드 아웃이 종료되었는지 반환합니다.</summary>
         bool IsFadeOutEnd() const;
 
+    public:
         /// <summary>
         /// 페이드 인이 끝났을 때 호출될 콜백 함수를 설정합니다. 생명관리를 하지 않으므로 사용 시 주의해야 합니다.
         /// </summary>
@@ -107,19 +113,45 @@ private:
         /// <param name="callback">페이드 아웃 종료 시 실행할 콜백 함수입니다.</param>
         void SetOnFadeOutEndCallback(const std::function<void()>& callback);
 
+        /// <summary>
+        /// 페이드 모드를 설정합니다.
+        /// </summary>
+        /// <param name="mode">설정할 페이드 모드입니다.</param>
         void SetFadeMode(Mode mode);
+
+        /// <summary>
+        /// 지정된 지속 시간을 설정합니다.
+        /// </summary>
+        /// <param name="duration">설정할 지속 시간(초 단위)입니다.</param>
         void SetDuration(float duration);
+
+        /// <summary>
+        /// 타이머 값을 설정합니다. 0부터 duration 사이의 값을 클램핑합니다.
+        /// </summary>
+        /// <param name="timer">설정할 타이머 값(초 단위)입니다.</param>
         void SetTimer(float timer);
+
+        /// <summary>
+        /// 페이드 인 효과의 타입과 함수 유형을 설정합니다.
+        /// </summary>
+        /// <param name="type">적용할 페이드 인 이징 타입입니다.</param>
+        /// <param name="func">사용할 이징 함수 유형입니다.</param>
         void SetFadeInType(Mathf::EaseType type, Mathf::EaseFuncType func);
+
+        /// <summary>
+        /// 페이드 아웃 효과의 타입과 함수 유형을 설정합니다.
+        /// </summary>
+        /// <param name="type">적용할 페이드 아웃 이징 타입입니다.</param>
+        /// <param name="func">적용할 이징 함수 유형입니다.</param>
         void SetFadeOutType(Mathf::EaseType type, Mathf::EaseFuncType func);
 
-        inline Mode  GetFadeMode() const { return _fadeMode; }
-        inline float GetFadeFactor() const { return _fadeFactor; }
-        inline float GetDuration() const { return _duration; }
-        inline Mathf::EaseType     GetFadeInEaseType() const { return _fadeInEaseType; }
-        inline Mathf::EaseFuncType GetFadeInFuncType() const { return _fadeInFuncType; }
-        inline Mathf::EaseType     GetFadeOutEaseType() const { return _fadeOutEaseType; }
-        inline Mathf::EaseFuncType GetFadeOutFuncType() const { return _fadeOutFuncType; }
+        inline Mode                 GetFadeMode() const { return _fadeMode; }
+        inline float                GetFadeFactor() const { return _fadeFactor; }
+        inline float                GetDuration() const { return _duration; }
+        inline Mathf::EaseType      GetFadeInEaseType() const { return _fadeInEaseType; }
+        inline Mathf::EaseFuncType  GetFadeInFuncType() const { return _fadeInFuncType; }
+        inline Mathf::EaseType      GetFadeOutEaseType() const { return _fadeOutEaseType; }
+        inline Mathf::EaseFuncType  GetFadeOutFuncType() const { return _fadeOutFuncType; }
 
     private:
         /// <summary>FadeIn을 갱신합니다. 0~1 사이의 Factor를 반환합니다.</summary>
