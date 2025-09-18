26c34ddcbea69ce0e5e2f6091d3c35d580a0b91b
PPAK_JU <taku7664@gmail.com>
2025-09-18 16:59:35 +0900

    [FEATURE/VG-423] Timeline Audio Notifier 추가

---
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/TimelineCore/Context/Audio/TimelineAudioEventContext.cpp
@@ -0,0 +1,64 @@
+﻿#include "pch.h"
+#include "TimelineAudioEventContext.h"
+
+REGISTER_CLASS(Timeline::EventTrack, Timeline::AudioEventContext)
+
+namespace Timeline
+{
+    AudioEventContext::AudioEventContext() 
+    {
+        Path.SetInputAutoEvent([this]() {
+            if (ImGui::BeginDragDropTarget())
+            {
+                if (const ImGuiPayload* payLoad = ImGui::AcceptDragDropPayload(DragDropAsset::KEY))
+                {
+                    DragDropAsset::Data* data      = static_cast<DragDropAsset::Data*>(payLoad->Data);
+                    File::Path           path      = data->GetPath();
+                    const auto           extension = path.extension();
+                    if (extension == L".wav")
+                    {
+                        _audioGuid               = data->GetGuid();
+                        ReflectFields->AudioGuid = _audioGuid.string();
+                    }
+                }
+                ImGui::EndDragDropTarget();
+            }
+        });
+    }
+
+    AudioEventContext::~AudioEventContext() 
+    {
+    }
+
+    void AudioEventContext::OnNotify() 
+    {
+        UmAudio.Play(_audioGuid.string());
+    }
+
+    void AudioEventContext::SerializedReflectEvent() 
+    {
+        ReflectFields->AudioGuid = _audioGuid.string();
+    }
+    
+    void AudioEventContext::DeserializedReflectEvent() 
+    {
+        _audioGuid = ReflectFields->AudioGuid;
+        UmAudio.LoadSound(_audioGuid.string(), _audioGuid);
+    }
+
+    void AudioEventContext::ImGuiDrawPropertysEvent() 
+    {
+    }
+
+    void AudioEventContext::SetAudioFromGuid(const File::Guid& guid) 
+    {
+        _audioGuid = guid;
+        ReflectFields->AudioGuid = _audioGuid.string();
+    }
+
+    void AudioEventContext::SetAudioFromPath(const File::Path& path) 
+    {
+        _audioGuid = path.ToGuid();
+        ReflectFields->AudioGuid = _audioGuid.string();
+    }
+}
---
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/TimelineCore/Context/Audio/TimelineAudioEventContext.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+namespace Timeline
+{
+    class AudioEventContext : public EventContext
+    {
+    public:
+        USING_PROPERTY(AudioEventContext)
+        AudioEventContext();
+        ~AudioEventContext() override;
+
+    public:
+        REFLECT_PROPERTY(Path, Volume) 
+
+        GETTER_ONLY(std::string, Path) { return _audioGuid.ToPath().string(); }
+        PROPERTY(Path)
+
+        GETTER(float, Volume) { return _volume; }
+        SETTER(float, Volume) { _volume = std::clamp(value, 0.0f, 1.0f); }
+        PROPERTY(Volume)
+
+        void OnNotify() override;
+
+        void SerializedReflectEvent() override;
+        void DeserializedReflectEvent() override;
+        void ImGuiDrawPropertysEvent() override;
+
+    public:
+        void SetAudioFromGuid(const File::Guid& guid);
+        void SetAudioFromPath(const File::Path& path);
+
+    protected:
+        File::Guid _audioGuid = File::NULL_GUID;
+        float      _volume = 1.0f;
+
+        REFLECT_FIELDS_BEGIN(EventContext)
+        std::string AudioGuid = "";
+        REFLECT_FIELDS_END(AudioEventContext)
+    };
+
+}; // namespace Timeline
