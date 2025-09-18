4ba4f5187eaa9893c4dc747a0cb2a46d16f12eb5
power12562 <kimsiwoo1005@gmail.com>
2025-09-18 11:54:39 +0900

    [FEATURE/VG-316] 엑셀 ExcelDataBase 찾는 기능 추가

--- Source/GA6thFinal_Framework/GameScripts/Scripts/ExcelDataSystem/ExcelDataSystem.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/ExcelDataSystem/ExcelDataSystem.cpp
@@ -16,6 +16,19 @@ ExcelDataSystem::~ExcelDataSystem()
 
 }
 
+std::unique_ptr<ExcelDataBase> ExcelDataSystem::GetExcelDataBase(const std::string& sheetName)
+{
+    std::unique_ptr<ExcelDataBase> excelDataBase;
+
+    auto& dataBase = ReflectFields->DataBase;
+    auto  findIter = dataBase.find(sheetName);
+    if (findIter != dataBase.end())
+    {
+        excelDataBase.reset(new ExcelDataBase(findIter->second));
+    }
+    return excelDataBase;
+}
+
 void ExcelDataSystem::Reset() 
 {
     _singletonObject.SetSingleTon();
@@ -45,30 +58,41 @@ void ExcelDataSystem::ImGuiDrawExcelParserEdit()
         const std::string& selectSheetName = _excelParser.GetSelectSheetName();
         size_t rawCount    = _excelParser.GetRawCount();
         size_t columnCount = _excelParser.GetColumnCount();
-        DataBaseType& dataBase = ReflectFields->DataBase;
-        std::pair<KeyIndexMapType, DataSheetType>& dataSheetPair = dataBase[selectSheetName];
-        auto& [keyIndexToMap, dataSheet] = dataSheetPair;
+        ExcelSystemMapType& dataBase = ReflectFields->DataBase;
+        ExcelDataBase::DataBaseType& dataSheetTuple = dataBase[selectSheetName];
+        auto& [columnIndexKeyMap, keyIndexToMap, dataSheet] = dataSheetTuple;
+        columnIndexKeyMap.clear();
         keyIndexToMap.clear();
         dataSheet.clear();
-        size_t dataIndex = 0;
+        size_t currRowIndex = 0;
         _excelParser.Apply([&](const ImGuiColumnSheetParser::ColumnDatas& datas) 
         {   
+            size_t currColumnIndex = 0;
             for (auto& pair : datas)
             {
                 const std::string& key = pair.first;
                 const std::string& data = pair.second;
                 if (false == key.empty())
                 {
-                    auto& indexMap = keyIndexToMap[key];
-                    indexMap.try_emplace(data, dataIndex);
-                    if (dataSheet.size() <= dataIndex)
+                    auto& columnAndrow = keyIndexToMap[key];
+                    auto& [columnIndex, rowIndexMap] = columnAndrow;
+                    columnIndex = currColumnIndex;                       // Key에 대한 Column 인덱스 등록
+                    if (columnIndexKeyMap.size() <= currColumnIndex)
                     {
-                        dataSheet.resize(dataIndex + 1);
+                        columnIndexKeyMap.emplace_back(key);             // Column 인덱스에 대한 Key 등록
                     }
-                    dataSheet[dataIndex].push_back(data);
+                    rowIndexMap.try_emplace(data, currRowIndex);         // Data에 대한 Row 인덱스 등록
+
+                    //2차원 배열에 데이터 등록
+                    if (dataSheet.size() <= currRowIndex)
+                    {
+                        dataSheet.resize(currRowIndex + 1);
+                    }
+                    dataSheet[currRowIndex].push_back(data);
                 }
+                ++currColumnIndex;
             }
-            ++dataIndex;
+            ++currRowIndex;
         });
         _excelParser.ShowParser = false;
     }
@@ -78,45 +102,129 @@ void ExcelDataSystem::ImGuiDrawExcelParserEdit()
 void ExcelDataSystem::ImGuiDrawDataSheetView() 
 {
 #ifdef _UMEDITOR
-    if (ImGui::TreeNode("Sheet Viewer"))
+    if (ImGui::Button("Data Base Viewer"))
     {
-        static thread_local std::string viewerSheetName = STR_NULL;
-        auto& dataBase = ReflectFields->DataBase;
-        if (ImGui::BeginCombo("Select sheet", viewerSheetName.c_str()))
+        _drawDataBaseView = true;
+    }
+
+    ImGui::Begin("Data Base Viewer##64F8C1F9-344D-4D36-A232-47E68DA36134", &_drawDataBaseView);
+    {
+        if (ImGui::TreeNode("Sheet Viewer"))
         {
-            for (auto& [key, dataPair] : dataBase)
+            static thread_local std::string viewerSheetName = STR_NULL;
+            auto&                           dataBase        = ReflectFields->DataBase;
+            if (ImGui::BeginCombo("Select sheet", viewerSheetName.c_str()))
             {
-                if (ImGui::Selectable(key.c_str(), key == viewerSheetName))
+                for (auto& [key, dataPair] : dataBase)
                 {
-                    viewerSheetName = key;
-                }
-            }
-            ImGui::EndCombo();
-        }
-        if (auto findIter = dataBase.find(viewerSheetName); findIter != dataBase.end())
-        {
-            auto& [keyIndexMap, dataSheet] = findIter->second;
-            if (false == dataSheet.empty())
-            {
-                int columnCount = static_cast<int>(dataSheet.front().size());
-                if (ImGui::BeginTable("##SheetViewerTable", columnCount, ImGuiTableFlags_Borders | ImGuiTableFlags_RowBg))
-                {
-                    for (auto& raw : dataSheet)
+                    if (ImGui::Selectable(key.c_str(), key == viewerSheetName))
                     {
-                        ImGui::TableNextRow();
-                        int columnIndex = 0;
-                        for (auto& column : raw)
-                        {
-                            ImGui::TableSetColumnIndex(columnIndex);
-                            ImGui::Text(column.c_str());
-                            ++columnIndex;
-                        }
+                        viewerSheetName = key;
                     }
-                    ImGui::EndTable();
+                }
+                ImGui::EndCombo();
+            }
+
+            if (auto findIter = dataBase.find(viewerSheetName); findIter != dataBase.end())
+            {
+                auto& [columnIndexKeyMap, keyIndexMap, dataSheet] = findIter->second;
+                if (false == columnIndexKeyMap.empty())
+                {
+                    int columnCount = static_cast<int>(columnIndexKeyMap.size());
+                    if (ImGui::BeginTable("##SheetViewerTable", columnCount,
+                                          ImGuiTableFlags_Borders | ImGuiTableFlags_RowBg))
+                    {
+                        for (auto& key : columnIndexKeyMap)
+                        {
+                            ImGui::TableSetupColumn(key.c_str());
+                        }
+                        ImGui::TableHeadersRow();
+
+                        for (auto& raw : dataSheet)
+                        {
+                            int columnIndex = 0;
+                            for (auto& column : raw)
+                            {
+                                ImGui::TableSetColumnIndex(columnIndex);
+                                ImGui::Text(column.c_str());
+                                ++columnIndex;
+                            }
+                            ImGui::TableNextRow();
+                        }
+                        ImGui::EndTable();
+                    }
                 }
             }
-        }
-        ImGui::TreePop();
-    }    
+            ImGui::TreePop();
+        }    
+    }
+    ImGui::End();
 #endif
+}
+
+ExcelDataBase::ExcelDataBase(const ExcelDataBase& rhs) 
+    :
+    _dataBase(rhs._dataBase) 
+{
+
+}
+
+size_t ExcelDataBase::FindColumnIndex(const std::string& columnKeyName)
+{
+    size_t result = FIND_INDEX_FAIL;
+    if (ExcelDataSystem* system = SingletonComponent<ExcelDataSystem>::GetInstance()) //댕글링 방지
+    {
+        auto& [columnIndexKeyMap, indexData, dataSheet] = _dataBase;
+        auto findIter = indexData.find(columnKeyName);
+        if (findIter != indexData.end())
+        {
+            auto& [columnIndex, rowIndexMap] = findIter->second;
+            result = columnIndex;
+        }
+    }
+    return result;
+}
+
+std::string_view ExcelDataBase::FindColumnKey(size_t columnIndex)
+{
+    std::string_view result = FIND_STR_FAIL;
+    if (ExcelDataSystem* system = SingletonComponent<ExcelDataSystem>::GetInstance()) // 댕글링 방지
+    {
+        auto& [columnIndexKeyMap, indexData, dataSheet] = _dataBase;
+        if (columnIndex < columnIndexKeyMap.size())
+        {
+            result = columnIndexKeyMap[columnIndex];
+        }
+    }
+    return result;
+}
+
+std::string_view ExcelDataBase::FindData(size_t rowIndex, size_t columnIndex)
+{
+    std::string_view result = FIND_STR_FAIL;
+    if (ExcelDataSystem* system = SingletonComponent<ExcelDataSystem>::GetInstance()) // 댕글링 방지
+    {
+        auto& [columnIndexKeyMap, indexData, dataSheet] = _dataBase;
+        if (rowIndex < dataSheet.size())
+        {
+            auto& columnDatas = dataSheet[rowIndex];
+            if (columnIndex < columnDatas.size())
+            {
+                result = columnDatas[columnIndex];
+            }
+        }
+    }
+    return result;
+}
+
+ExcelDataBase::ExcelDataBase(const DataBaseType& dataBase) 
+    : 
+    _dataBase(dataBase) 
+{
+
+}
+
+ExcelDataBase::~ExcelDataBase() 
+{
+
 }
--- Source/GA6thFinal_Framework/GameScripts/Scripts/ExcelDataSystem/ExcelDataSystem.h
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/ExcelDataSystem/ExcelDataSystem.h
@@ -3,6 +3,53 @@
 #include "ExcelParser/ImGuiColumnSheetParser.h"
 #include "Utility/SingletonHelper.h"
 
+class ExcelDataBase
+{
+public:
+    //인덱스 찾기 실패시 반환되는 값
+    inline static constexpr size_t FIND_INDEX_FAIL = std::numeric_limits<size_t>::max();    
+
+    //string 찾기 실패시 반환되는 값
+    inline static constexpr std::string_view FIND_STR_FAIL = STR_NULL;
+
+    using ColumnIndexKeyMap = std::vector<std::string>;
+    using KeyIndexMapType   = std::unordered_map<std::string, std::pair<size_t, std::unordered_map<std::string, size_t>>>;
+    using DataSheetType     = std::vector<std::vector<std::string>>;
+    using DataBaseType      = std::tuple<ColumnIndexKeyMap, KeyIndexMapType, DataSheetType>;
+
+    ExcelDataBase(const ExcelDataBase& rhs);
+    ~ExcelDataBase();
+    ExcelDataBase& operator=(const ExcelDataBase& rhs) = delete;
+
+public:
+    /// <summary>
+    /// 해당 문자열 키에 해당하는 Column 인덱스를 반환합니다.
+    /// </summary>
+    /// <param name="columnKeyName :">찾을 Column의 키 문자열</param>
+    /// <returns>실패시 ExcelDataBase::FIND_INDEX_FAIL를 반환합니다.</returns>
+    size_t FindColumnIndex(const std::string& columnKeyName);
+
+    /// <summary>
+    /// 해당 인덱스에 해당하는 Column key를 반환합니다.
+    /// </summary>
+    /// <param name="columnIndex :">찾을 columnIndex</param>
+    /// <returns>실패시 ExcelDataBase::FIND_STR_FAIL을 반환합니다.</returns>
+    std::string_view FindColumnKey(size_t columnIndex);
+
+    /// <summary>
+    /// 데이터 시트에서 데이터를 가져옵니다.
+    /// </summary>
+    /// <param name="rowIndex :">row Index</param>
+    /// <param name="columnIndex :">column Index</param>
+    /// <returns>실패시 FIND_STR_FAIL을 반환합니다.</returns>
+    std::string_view FindData(size_t rowIndex, size_t columnIndex);
+
+private:
+    friend class ExcelDataSystem;
+    ExcelDataBase(const DataBaseType& dataBase);
+    const DataBaseType& _dataBase;
+};
+
 class ExcelDataSystem : public Component
 {
     USING_PROPERTY(ExcelDataSystem)
@@ -11,18 +58,24 @@ public:
     ExcelDataSystem();
     ~ExcelDataSystem() override;
 
+public:
+    using ExcelSystemMapType = std::unordered_map<std::string, ExcelDataBase::DataBaseType>;
+
+    /// <summary>
+    /// 시트 이름을 키값으로 엑셀 데이터 베이스를 반환합니다. 없으면 nullptr을 반환합니다.
+    /// </summary>
+    /// <param name="sheetName :">원하는 시트 이름</param>
+    /// <returns>동적 할당된 ExcelDataBase</returns>
+    std::unique_ptr<ExcelDataBase> GetExcelDataBase(const std::string& sheetName);
+
 private:
     SingletonObject<ExcelDataSystem> _singletonObject;
 
 public:
     REFLECT_PROPERTY()
-
 protected:
-    using KeyIndexMapType = std::unordered_map<std::string, std::unordered_map<std::string, size_t>>;
-    using DataSheetType   = std::vector<std::vector<std::string>>;
-    using DataBaseType    = std::unordered_map<std::string, std::pair<KeyIndexMapType, DataSheetType>>;
     REFLECT_FIELDS_BEGIN(Component)
-    DataBaseType DataBase;
+    ExcelSystemMapType DataBase;
     REFLECT_FIELDS_END(ExcelDataSystem)
 
     void Reset() override;
@@ -32,6 +85,7 @@ protected:
 private:
 #ifdef _UMEDITOR
     ImGuiColumnSheetParser _excelParser;
+    bool                   _drawDataBaseView = false;
 #endif
     void ImGuiDrawExcelParserEdit();
     void ImGuiDrawDataSheetView();
