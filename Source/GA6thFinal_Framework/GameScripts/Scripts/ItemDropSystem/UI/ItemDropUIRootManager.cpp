36cdf7e280afb5703e473543be8c81deee00ba13
power12562 <kimsiwoo1005@gmail.com>
2025-09-18 16:27:11 +0900

    [REFACTOR/VG-316] GetArtifactIconID에서 엑셀 데이터 사용하도록 개선

--- Source/GA6thFinal_Framework/GameScripts/Scripts/ItemDropSystem/UI/ItemDropUIRootManager.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/ItemDropSystem/UI/ItemDropUIRootManager.cpp
@@ -3,7 +3,7 @@
 #include "ItemDropSystem/UI/ArtifactUIManager.h"
 #include "ItemDropSystem/ItemDropSystem.h"
 #include "RevelationSystem/RevelationSystem.h"
-
+#include "ExcelDataSystem/ExcelDataSystem.h"
 
 UMREAL_COMPONENT(ItemDropUIRootManager)
 
@@ -30,56 +30,90 @@ int ItemDropUIRootManager::GetArtifactCategoryAssetID(ArtifactDropType artifactD
 
 int ItemDropUIRootManager::GetArtifactIconID(DropItemInfo itemInfo)
 {
-    auto find = ReflectFields->ArtifactsIconIDMap.find(itemInfo.ID);
-    if (find != ReflectFields->ArtifactsIconIDMap.end())
+    if (ExcelDataSystem* excelDataSystem = SingletonComponent<ExcelDataSystem>::GetInstance())
     {
-        return find->second;
-    }
-    else
-    {
-        auto GetRevelationDefaultIcon = [](const DropItemInfo& info) -> int
-        {
-            if (RevelationSystem* system = SingletonComponent<RevelationSystem>::GetInstance())
-            {
-                RevelationElement* element = system->FindElement(info.Name);
-                if (element)
-                {
-                    RevelationGrade grade = element->Grade;
-                    switch (grade)
-                    {
-                    case RevelationGrade::COMMON:
-                        return -202000;
-                    case RevelationGrade::RARE:
-                        return -202001;
-                    case RevelationGrade::LEGENDARY:
-                        return -202002;
-                    case RevelationGrade::EXTINCTION:
-                        return 0;
-                    default:
-                        break;
-                    }
-                }
-            }
-            return 0;
-        };
-
+        std::unique_ptr<ExcelDataBase> dataBase;
         switch (itemInfo.Category)
         {
-        case ArtifactDropType::DAGGER:
-            return -201000;
-        case ArtifactDropType::WARHAMMER:
-            return -201001;
         case ArtifactDropType::SWORD:
-            return -201002;
+        case ArtifactDropType::DAGGER:
+        case ArtifactDropType::WARHAMMER:
+            dataBase = excelDataSystem->FindExcelDataBase(u8"무기");
+            break;
         case ArtifactDropType::ACCESSORY:
-            return DropItemInfo::GetArtifactCategoryAssetID(itemInfo.Category);
+            dataBase = excelDataSystem->FindExcelDataBase(u8"장신구");
+            break;
         case ArtifactDropType::REVELATION:
-            return GetRevelationDefaultIcon(itemInfo);
+            dataBase = excelDataSystem->FindExcelDataBase(u8"계시");
+            break;
         case ArtifactDropType::ERASE_REVELATION:
-            return DropItemInfo::GetArtifactCategoryAssetID(itemInfo.Category);
+            break;
+        case ArtifactDropType::Consumable:
+            dataBase = excelDataSystem->FindExcelDataBase(u8"소모품");
+            break;
         default:
             return 0;
+            break;
         }
+
+        if (dataBase)
+        {
+            const std::string& name = itemInfo.Name;
+            std::u8string_view u8Name = (const char8_t*)name.data();
+            size_t rowIndex = dataBase->FindRowIndex(u8Name, u8"Name");
+            if (rowIndex != ExcelDataBase::FIND_INDEX_FAIL)
+            {
+                std::string_view id = dataBase->FindData(rowIndex, u8"ID");
+                if (id != ExcelDataBase::FIND_STR_FAIL)
+                {
+                    return std::stoi(id.data());
+                }
+            }
+        }
+    }
+
+    auto GetRevelationDefaultIcon = [](const DropItemInfo& info) -> int 
+    {
+        if (RevelationSystem* system = SingletonComponent<RevelationSystem>::GetInstance())
+        {
+            RevelationElement* element = system->FindElement(info.Name);
+            if (element)
+            {
+                RevelationGrade grade = element->Grade;
+                switch (grade)
+                {
+                case RevelationGrade::COMMON:
+                    return -202000;
+                case RevelationGrade::RARE:
+                    return -202001;
+                case RevelationGrade::LEGENDARY:
+                    return -202002;
+                case RevelationGrade::EXTINCTION:
+                    return 0;
+                default:
+                    break;
+                }
+            }
+        }
+        return 0;
+    };
+
+    switch (itemInfo.Category)
+    {
+    case ArtifactDropType::DAGGER:
+        return -201000;
+    case ArtifactDropType::WARHAMMER:
+        return -201001;
+    case ArtifactDropType::SWORD:
+        return -201002;
+    case ArtifactDropType::ACCESSORY:
+        return DropItemInfo::GetArtifactCategoryAssetID(itemInfo.Category);
+    case ArtifactDropType::REVELATION:
+        return GetRevelationDefaultIcon(itemInfo);
+    case ArtifactDropType::ERASE_REVELATION:
+        return DropItemInfo::GetArtifactCategoryAssetID(itemInfo.Category);
+    default:
+        return 0;
     }
 }
 
