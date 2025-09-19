cc7efa0aeaf260dade4d68f02820cc1d891b86b6
power12562 <kimsiwoo1005@gmail.com>
2025-09-19 15:49:47 +0900

    [CHORE/VG-371] 불필요한 Include 제거

--- Source/GA6thFinal_Framework/GameScripts/Scripts/ViewModels/Hp/CharacterHPViewModel.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/ViewModels/Hp/CharacterHPViewModel.cpp
@@ -1,9 +1,5 @@
 ﻿#include "pchScripts.h"
 #include "CharacterHPViewModel.h"
-#include "Utility/SingletonHelper.h"
-#include "PlayerSystem/PlayerSystem.h"
-#include "Stats/Player/PlayerStatsComponent.h"
-#include "Stats/Player/PlayerStats.h"
 
 CharacterHPViewModel::CharacterHPViewModel(MVVM::Model<std::pair<int, int>>& model)
     : ViewModel(model) 
