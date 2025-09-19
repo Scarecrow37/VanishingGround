f5e5bde60fd8442a15676c5d70e8b36013198c08
power12562 <kimsiwoo1005@gmail.com>
2025-09-19 14:37:58 +0900

    [REFACTOR/VG-371] HpViewModel을 pair로 변경

--- Source/GA6thFinal_Framework/GameScripts/Scripts/ViewModels/Map/MapPlayerHPViewModel.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/ViewModels/Map/MapPlayerHPViewModel.cpp
@@ -1,16 +1,20 @@
 ﻿#include "pchScripts.h"
 #include "MapPlayerHPViewModel.h"
+#include "Utility/SingletonHelper.h"
+#include "PlayerSystem/PlayerSystem.h"
+#include "Stats/Player/PlayerStatsComponent.h"
+#include "Stats/Player/PlayerStats.h"
 
-MapPlayerHPViewModel::MapPlayerHPViewModel(MVVM::Model<int>& model, int maxHP)
+MapPlayerHPViewModel::MapPlayerHPViewModel(MVVM::Model<std::pair<int, int>>& model)
     : ViewModel(model) 
-    , _maxHP(maxHP)
-{}
-
-PlayerHP MapPlayerHPViewModel::Convert(const int& value)
 {
-    PlayerHP playerHP;
-    playerHP.CurrentHP = value;
-    playerHP.MaxHP = _maxHP;
 
+}
+
+PlayerHP MapPlayerHPViewModel::Convert(const std::pair<int, int>& value)
+{  
+    PlayerHP playerHP{100, 100};
+    playerHP.CurrentHP = value.first;
+    playerHP.MaxHP     = value.second;
     return playerHP;
 }
--- Source/GA6thFinal_Framework/GameScripts/Scripts/ViewModels/Map/MapPlayerHPViewModel.h
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/ViewModels/Map/MapPlayerHPViewModel.h
@@ -6,15 +6,13 @@ struct PlayerHP
     int MaxHP;
 };
 
-class MapPlayerHPViewModel final : public MVVM::ViewModel<int, PlayerHP>
+class MapPlayerHPViewModel final : public MVVM::ViewModel<std::pair<int, int>, PlayerHP>
 {
 public:
-    explicit MapPlayerHPViewModel(MVVM::Model<int>& model, int maxHP);
+    explicit MapPlayerHPViewModel(MVVM::Model<std::pair<int, int>>& model);
 
 protected:
     // ViewModel을(를) 통해 상속됨
-    PlayerHP Convert(const int& value) override;
+    PlayerHP Convert(const std::pair<int, int>& value) override;
 
-private:
-    int _maxHP;
 };
