fd2c16ddd40922f8cee2f9944341b2a9214b1fd3
power12562 <kimsiwoo1005@gmail.com>
2025-09-22 09:22:57 +0900

    [REFACTOR/VG-371] MapPlayerHPView 컴포넌트를 PlayerHPText와 PlayerHPImage로 리팩토링

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Views/PlayerHP/PlayerHPView.cpp
@@ -0,0 +1,72 @@
+﻿#include "pchScripts.h"
+#include "PlayerHPView.h"
+#include "UI/Elements/Image/ImageElement.h"
+#include "UI/Elements/Text/TextElement.h"
+#include "Stats/Player/PlayerStats.h"
+
+UMREAL_COMPONENT(PlayerHPTextView)
+UMREAL_COMPONENT(PlayerHPImageView)
+
+PlayerHPTextView::PlayerHPTextView()  = default;
+
+PlayerHPTextView::~PlayerHPTextView()
+{
+    UmWatcher.Blind<CharacterHPViewModel>(PlayerStats::MODEL_HP_KEY, _handle);
+}
+
+void PlayerHPTextView::Start()
+{    
+    try
+    {
+        if (_hp = GetComponent<TextElement>(); nullptr == _hp)
+        {
+            _hp = &AddComponent<TextElement>();
+        }
+
+        _handle = UmWatcher.Watch<CharacterHPViewModel, CharacterHP>(PlayerStats::MODEL_HP_KEY, [this](const CharacterHP value) 
+        {
+            _hp->Text = std::format("{} / {}", value.CurrentHP, value.MaxHP);
+        });
+    }
+    catch (const std::exception& e)
+    {
+        UmLogger.Log(LogLevel::LEVEL_ERROR, "Watch Failed.");
+        UmLogger.Log(LogLevel::LEVEL_ERROR, e.what());
+    }
+}
+
+
+PlayerHPImageView::PlayerHPImageView() 
+{
+
+}
+
+PlayerHPImageView::~PlayerHPImageView() 
+{
+
+}
+
+void PlayerHPImageView::Start() 
+{
+    try
+    {
+        _hpGage = GetComponent<ImageElement>();
+        _handle = UmWatcher.Watch<CharacterHPViewModel, CharacterHP>(PlayerStats::MODEL_HP_KEY, [this](const CharacterHP value) 
+        { 
+            if (_hpGage)
+            {
+                _hpGage->SetLinearFill(value.CurrentHP / (float)value.MaxHP);
+            }
+        });
+    }
+    catch (const std::exception& e)
+    {
+        UmLogger.Log(LogLevel::LEVEL_ERROR, "Watch Failed.");
+        UmLogger.Log(LogLevel::LEVEL_ERROR, e.what());
+    }
+}
+
+void PlayerHPImageView::OnDestroy() 
+{
+    UmWatcher.Blind<CharacterHPViewModel>(PlayerStats::MODEL_HP_KEY, _handle);
+}
