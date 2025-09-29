7fd57397c06ff331b4500749d89beba5297e0d2f
Do_Suha <weqa123@naver.com>
2025-09-29 15:09:16 +0900

    [FEATURE/VG-405] DamageElement UI 구성 요소 추가
    `DamageElement` 클래스와 관련된 헤더 및 소스 파일이 추가되었습니다.
    이 클래스는 UI 구성 요소로서 크기 측정, 배치, 디버그 시 시각적 표시 기능을 구현합니다.
    프로젝트의 빌드 및 필터링 설정에 반영되었으며, 반지름 속성 및 관련 메서드가 포함되어 있습니다.

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Contents/DamageElement.cpp
@@ -0,0 +1,67 @@
+﻿#include "pchScripts.h"
+#include "DamageElement.h"
+
+UMREAL_COMPONENT(DamageElement)
+
+
+SIZE DamageElement::MeasureOverride(const SIZE availableSize)
+{
+    const FillMode horizontalFillMode = HorizontalFillMode;
+    const FillMode verticalFillMode   = VerticalFillMode;
+    SIZE           desiredSize        = MinSize()(availableSize, _requestedSize, horizontalFillMode == FillMode::FILL,
+                                 verticalFillMode == FillMode::FILL);
+
+    const PADDING padding            = Padding;
+    const SIZE    childAvailableSize = desiredSize - padding.Size();
+    SIZE          childDesiredSize{};
+    if (const std::vector<UIComponent*> children = Children; children.size() > 0)
+    {
+        children[0]->Measure(childAvailableSize);
+        childDesiredSize = children[0]->DesiredSize;
+    }
+
+    if (horizontalFillMode == FillMode::WRAP)
+        desiredSize.cx = childDesiredSize.cx + padding.Horizontal();
+    if (verticalFillMode == FillMode::WRAP)
+        desiredSize.cy = childDesiredSize.cy + padding.Vertical();
+
+    return desiredSize;
+}
+
+SIZE DamageElement::ArrangeOverride(const SIZE finalSize)
+{
+    const SIZE    desiredSize        = DesiredSize;
+    const SIZE    actualSize         = MinSize()(finalSize, desiredSize);
+    const PADDING padding            = Padding;
+    const SIZE    childAvailableSize = actualSize - padding.Size();
+    if (const std::vector<UIComponent*> children = Children; children.size() > 0)
+    {
+        const HorizontalAlignment horizontalAlign = HorizontalAlign;
+        const VerticalAlignment   verticalAlign   = VerticalAlign;
+        const SIZE                childSize       = children[0]->DesiredSize;
+        POINT                     childPoint      = AbsoluteChildPosition;
+        childPoint += AlignPoint()(horizontalAlign, verticalAlign, childAvailableSize - childSize);
+
+        children[0]->Arrange(childPoint, childAvailableSize);
+    }
+
+    return actualSize;
+}
+
+void DamageElement::OnDrawDebugSelectedOverride()
+{
+    UIComponent::OnDrawDebugSelectedOverride();
+
+    const POINT center = AbsoluteCenterPoint;
+    const SIZE  size   = Size;
+    const float radius = Radius * std::min(size.cx, size.cy) * 0.5f;
+    DrawDebug()(center, radius, Colors::Red);
+}
+
+void DamageElement::Start()
+{
+    UIComponent::Start();
+
+
+
+}
---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Contents/DamageElement.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+class DamageElement : public UIComponent
+{
+    USING_PROPERTY(DamageElement)
+
+public:
+    REFLECT_PROPERTY(Radius)
+
+    GETTER(float, Radius) { return ReflectFields->Radius; }
+    SETTER(float, Radius) { ReflectFields->Radius = std::clamp(value, 0.0f, 1.0f); }
+    PROPERTY(Radius)
+
+protected:
+    SIZE MeasureOverride(SIZE availableSize) override;
+    SIZE ArrangeOverride(SIZE finalSize) override;
+
+    void OnDrawDebugSelectedOverride() override;
+
+    void Start() override;
+
+protected:
+    REFLECT_FIELDS_BEGIN(UIComponent)
+    float Radius = 1.0f;
+    REFLECT_FIELDS_END(DamageElement)
+
+
+};
