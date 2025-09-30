516c98e15fedb0c986f1e15e03a26c65e5029076
power12562 <kimsiwoo1005@gmail.com>
2025-09-30 18:53:25 +0900

    [FIX/VG-370] UI 아이디 가져오는 로직 수정

--- Source/GA6thFinal_Framework/GameScripts/Scripts/ItemDropSystem/UI/ItemDropUIRootManager.h
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/ItemDropSystem/UI/ItemDropUIRootManager.h
@@ -25,20 +25,6 @@ public:
     //유물 드랍 프레임 UI 에셋 경로입니다.
     PROPERTY(ArtifactsUIFrameAsset)
 
-    /// <summary>
-    /// 유물 드랍 타입에 따른 에셋 아이디를 반환합니다.
-    /// </summary>
-    /// <param name="artifactDropType :">가져올 아이디</param>
-    /// <returns>실패시 0</returns>
-    int GetArtifactCategoryAssetID(ArtifactDropType artifactDropType);
-
-    /// <summary>
-    /// 아이템 ID를 통해 아이콘 ID를 반환합니다.
-    /// </summary>
-    /// <param name="info :">아이템 정보</param>
-    /// <returns>아이콘 ID</returns>
-    int GetArtifactIconID(DropItemInfo info);
-
 protected:
     REFLECT_FIELDS_BEGIN(Component)
     std::string ArtifactsUIFrameAssetGuid;
