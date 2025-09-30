9d105fe7e083a04e00f031c9f6c1ddafd20c925f
power12562 <kimsiwoo1005@gmail.com>
2025-09-30 18:19:01 +0900

    [FIX/VG-370] 아이콘 가져오는 함수 DropItemInfo의 함수로 변경

--- Source/GA6thFinal_Framework/GameScripts/Scripts/ItemDropSystem/Interface/IDropItem.h
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/ItemDropSystem/Interface/IDropItem.h
@@ -28,6 +28,13 @@ struct DropItemInfo
     /// <param name="type :">변환할 타입</param>
     /// <returns>실패시 0</returns>
     static int GetArtifactCategoryAssetID(ArtifactDropType type);
+
+    /// <summary>
+    /// 해당 유물의 에셋 ID를 반환합니다.
+    /// </summary>
+    /// <param name="itemInfo"></param>
+    /// <returns></returns>
+    static int GetArtifactIconID(DropItemInfo itemInfo);
 };
 
 /*보상 시스템에 등장 가능한 유물들은 다음 인터페이스를 상속받아야 합니다.*/
