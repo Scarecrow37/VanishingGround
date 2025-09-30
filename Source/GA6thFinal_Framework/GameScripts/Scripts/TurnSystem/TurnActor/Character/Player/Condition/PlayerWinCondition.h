f8074c19fe2c1f6054ac42bdb8c9ef2b4dcb2e65
power12562 <kimsiwoo1005@gmail.com>
2025-09-30 16:03:46 +0900

    [FEATURE/VG-370] 플레이어 승리시 상태 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnActor/Character/Player/Condition/PlayerWinCondition.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "Base/PlayerConditionBase.h"
+
+//플레이어 승리 조건
+class PlayerWinCondition : public PlayerConditionBase
+{
+public:
+    PlayerWinCondition();
+    virtual ~PlayerWinCondition() override;
+
+protected:
+    void OnAwake() override;
+    void OnStart() override;
+    bool Evaluate() override;
+   
+};
---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnActor/Character/Player/State/PlayerWinState.cpp
@@ -0,0 +1,20 @@
+﻿#include "pchScripts.h"
+#include "PlayerWinState.h"
+#include "GameCore/FSM/FiniteStateMachine.h"
+
+REGISTER_CLASS(FSMStateFactory, PlayerWinState)
+
+PlayerWinState::PlayerWinState() {}
+
+PlayerWinState::~PlayerWinState() {}
+
+void PlayerWinState::OnAwake() {}
+
+void PlayerWinState::OnStart() {}
+
+void PlayerWinState::OnEnter() {}
+
+void PlayerWinState::OnUpdate() {}
+
+void PlayerWinState::OnExit() {}
+
