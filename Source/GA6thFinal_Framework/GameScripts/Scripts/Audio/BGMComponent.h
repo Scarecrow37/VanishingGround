ea559a8f9db18b5f980fcc36b5cdba2a0d137d58
PPAK_JU <taku7664@gmail.com>
2025-10-01 14:13:58 +0900

    [FIX/VG-437] BGM추가 및 EnemtAction 예외 처리

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/Audio/BGMComponent.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+class BGMComponent : public Component
+{
+    USING_PROPERTY(BGMComponent)
+
+public:
+    BGMComponent();
+    ~BGMComponent() override;
+
+    REFLECT_PROPERTY(AudioID, UseFade)
+
+    SETTER(int, AudioID) { ReflectFields->AudioID = value; }
+    GETTER(int, AudioID) { return ReflectFields->AudioID; }
+    PROPERTY(AudioID)
+
+    SETTER(bool, UseFade) { ReflectFields->UseFade = value; }
+    GETTER(bool, UseFade) { return ReflectFields->UseFade; }
+    PROPERTY(UseFade)
+
+private:
+    void Start() override;
+
+private:
+
+    REFLECT_FIELDS_BEGIN(Component)
+    int AudioID = 0;
+    bool UseFade = true;
+    REFLECT_FIELDS_END(BGMComponent)
+};
