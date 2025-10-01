4d1345db3857a7720147237257fb20bcf4a93629
bmbany <59551445+bmbany@users.noreply.github.com>
2025-10-01 18:33:33 +0900

    [FEATURE/VG-372] 그래픽스 설정 Import 시스템 추가
    - ImportGraphicsSetting 컴포넌트 구현
    - 그래픽스 설정 파일(.inl) 드래그 앤 드롭 지원
    - GraphicsModule 연동 기능 추가
    - 프로젝트 파일 구조 업데이트
    - 설정 파일 자동 Import 기능 구현

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/Preferences/ImportGraphicsSetting.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+class ImportGraphicsSetting : public Component
+{
+    USING_PROPERTY(ImportGraphicsSetting)
+
+public:
+    ImportGraphicsSetting();
+    ~ImportGraphicsSetting() override;
+
+public:
+    REFLECT_PROPERTY(FilePath)
+    GETTER_ONLY(std::string, FilePath)   
+    {
+        return ReflectFields->Guid;
+    }
+    PROPERTY(FilePath)
+
+protected:
+    void Reset() override;
+
+protected:
+    REFLECT_FIELDS_BEGIN(Component)
+    std::string Guid;
+    REFLECT_FIELDS_END(ImportGraphicsSetting)
+};
+
