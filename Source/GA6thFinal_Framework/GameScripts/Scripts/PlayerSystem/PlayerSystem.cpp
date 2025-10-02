3d4a5d036c28e63120391b88cabc1eaf5b8fba9a
power12562 <kimsiwoo1005@gmail.com>
2025-10-02 09:02:15 +0900

    [FIX/VG-370] 플레이어 체력 갱신 안하는 버그 수정

--- Source/GA6thFinal_Framework/GameScripts/Scripts/PlayerSystem/PlayerSystem.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/PlayerSystem/PlayerSystem.cpp
@@ -85,6 +85,16 @@ void PlayerSystem::SetStatsCombatStart()
     }
 }
 
+void PlayerSystem::NotifyPlayerHP() 
+{
+    if (true == CheckWithLog(_playerStatsComponent))
+    {
+        PlayerStats& stats = _playerStatsComponent->GetStats();
+        int          hp    = stats.CurrentHP;
+        stats.CurrentHP    = hp;
+    }
+}
+
 void PlayerSystem::Reset() 
 {
     _singletonObject.SetSingleTon();
