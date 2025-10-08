2b9a7dcb1ab5b0db0923dd004a3fb21f3a3de184
SeongU <blacknaco85@gmail.com>
2025-10-08 21:39:27 +0900

    [FEATURE/VG-448] aa pass 추가

---
+++ Source/GA6thFinal_Framework/GraphicsEngine/AntiAliasingPass.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "RenderPass.h"
+class AntiAliasingPass : public RenderPass
+{
+public:
+    AntiAliasingPass();
+    virtual ~AntiAliasingPass();
+
+public:
+    void Initialize(RenderScene* ownerScene, RenderTechnique* ownerTechnique,
+                    ID3D12GraphicsCommandList* commandList) override;
+    void AddRenderPassDatas(std::string_view sceneName) override;
+    void Begin(ID3D12GraphicsCommandList* commandList) override;
+    void Draw(ID3D12GraphicsCommandList* commandList) override;
+
+private:
+    FX<GE::VS::QUAD, GE::PS::FXAA> _fx;
+    std::unique_ptr<ConstantBufferView> _constantBuffer;
+};
