8e4c984c71cb797aca2e7e200a447b1769b276e6
power12562 <kimsiwoo1005@gmail.com>
2025-10-11 09:35:49 +0900

    [FEATURE/VG-370] 무기 변경 버튼 Navi 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/ItemDropSystem/UINavi/PlayerWeaponChangeNavi.cpp
@@ -0,0 +1,48 @@
+﻿#include "pchScripts.h"
+#include "PlayerWeaponChangeNavi.h"
+#include "UI/Elements/Image/ImageElement.h"
+#include "WeaponSystem/WeaponSystem.h"
+#include "ItemDropSystem/UI/WeaponChangeUIManager.h"
+
+UMREAL_COMPONENT(PlayerWeaponChangeNavi)
+
+void PlayerWeaponChangeNavi::Awake()
+{
+    Base::Awake();
+    _focusImage = GetComponent<ImageElement>();
+}
+
+void PlayerWeaponChangeNavi::FocusIn(FocusCallType type)
+{
+    Base::FocusIn(type);
+    if (_focusImage)
+    {
+        _focusImage->Enable = true;
+    }
+    if (WeaponSystem* weaponSystem = SingletonComponent<WeaponSystem>::GetInstance())
+    {
+        if (WeaponChangeUIManager* uiManager = SingletonComponent<WeaponChangeUIManager>::GetInstance())
+        {
+            auto& weapons = weaponSystem->GetEquipWeapons();
+            if (_weaponIndex < weapons.size())
+            {
+                uiManager->SetPlayerWeaponStatsUI(weapons[_weaponIndex]);
+            }
+        }
+    }
+}
+
+void PlayerWeaponChangeNavi::Submit() 
+{
+    Base::Submit();
+
+}
+
+void PlayerWeaponChangeNavi::FocusOut(FocusCallType type) 
+{
+    Base::FocusOut(type);
+    if (_focusImage)
+    {
+        _focusImage->Enable = false;
+    }
+}
