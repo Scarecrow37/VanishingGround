f1fb1f13c957bf51de741321f7cc8e23cc9d2871
power12562 <kimsiwoo1005@gmail.com>
2025-10-13 13:06:13 +0900

    [FEATURE/VG-370] 플레이어의 기절 저항치용 View 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Views/SturnResistanceView/SturnResistanceView.cpp
@@ -0,0 +1,27 @@
+﻿#include "pchScripts.h"
+#include "SturnResistanceView.h"
+#include "Stats/Player/PlayerStats.h"
+#include "UI/Elements/Text/TextElement.h"
+
+UMREAL_COMPONENT(PlayerSturnResistanceView)
+
+PlayerSturnResistanceView::PlayerSturnResistanceView() = default;
+PlayerSturnResistanceView::~PlayerSturnResistanceView() = default;
+
+void PlayerSturnResistanceView::Start() 
+{
+    Base::Start();
+    _viewModelHandle = UmWatcher.Watch<SturnResistanceViewModel, int>(PlayerStats::MODEL_STURN_KEY, [this](const int& value) 
+    { 
+        if (_targetText)
+        {
+            _targetText->Text = std::to_string(value);
+        }
+    });
+}
+
+void PlayerSturnResistanceView::OnDestroy() 
+{
+    Base::OnDestroy();
+    UmWatcher.Blind<SturnResistanceViewModel>(PlayerStats::MODEL_STURN_KEY, _viewModelHandle);
+}
