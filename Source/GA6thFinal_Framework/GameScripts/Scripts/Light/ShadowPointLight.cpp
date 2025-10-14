e9bd03ee2c89e4f64de7bee244c32adee9bf512d
SeongU <blacknaco85@gmail.com>
2025-10-14 11:40:48 +0900

    [FEATURE/VG-452] shadow point light comp 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/Light/ShadowPointLight.cpp
@@ -0,0 +1,39 @@
+﻿#include "GraphicsEngine/Interface/ILight.h"
+#include "ShadowPointLight.h"
+#include "pchScripts.h"
+
+UMREAL_COMPONENT(ShadowPointLight)
+
+ShadowPointLight::ShadowPointLight() = default;
+
+ShadowPointLight::~ShadowPointLight() = default;
+
+void ShadowPointLight::DeserializedReflectEvent()
+{
+    _attenuation = Vector3(ReflectFields->Attenuation.data());
+}
+
+void ShadowPointLight::Reset()
+{
+    Base::Reset();
+
+    const Vector3& color       = GetColor();
+    const Vector3& attenuation = _attenuation;
+    float&         range       = ReflectFields->Range;
+    float&         intensity   = ReflectFields->Basefields.get().Intensity;
+
+    Lighting->SetShadowPointLight(color, transform->WorldPosition, attenuation, range, intensity);
+}
+
+void ShadowPointLight::OnDrawDebug()
+{
+    Base::OnDrawDebug();
+}
+
+void ShadowPointLight::OnDrawDebugSelected()
+{
+    BoundingSphere sphere;
+    sphere.Center = transform->WorldPosition;
+    sphere.Radius = ReflectFields->Range;
+    UmGraphics.DebugDraw3D("Editor", sphere, LightComponent::DEBUG_COLOR);
+}
