e179c1550acd61202440b6efbee57bbac721bec0
Do_Suha <weqa123@naver.com>
2025-10-16 16:11:04 +0900

    [FEATURE/VG-457] 인트로 매니저 기능 추가
    - FadeDescriptionPanel을 포함하고, 인트로 매니저의 생성자 및 메서드를 구현했습니다.
    - 게임 오브젝트를 찾아 `_introDescription`을 초기화하며, 상태에 따라 페이드 효과를 시작하는 로직을 추가했습니다.
    - 새로운 `Step` 열거형과 관련 멤버 변수를 추가하여 상태 관리를 개선했습니다.

--- Source/GA6thFinal_Framework/GameScripts/Scripts/IntroManager/IntroManager.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/IntroManager/IntroManager.cpp
@@ -1,6 +1,53 @@
 ﻿#include "pchScripts.h"
 #include "IntroManager.h"
+#include "UI/Animations/FadeDescriptionPanel/FadeDescriptionPanel.h"
 
 UMREAL_COMPONENT(IntroManager)
 
 IntroManager::IntroManager() = default;
+
+void IntroManager::Start()
+{
+    Component::Start();
+
+    const std::weak_ptr<GameObject> introDescriptionObjectWeak = GameObject::FindWithTag("Intro Description");
+
+    if (const std::shared_ptr introDescriptionObject = introDescriptionObjectWeak.lock(); nullptr != introDescriptionObject)
+    {
+        _introDescription = introDescriptionObject->GetComponent<FadeDescriptionPanel>();
+    }
+}
+
+void IntroManager::Update()
+{
+    Component::Update();
+
+    _elapsedTime += UmTime.DeltaTime();
+
+    switch (_step)
+    {
+    case WAIT_INTRO_DESCRIPTION:
+        if (_elapsedTime >= ReflectFields->IntroDescriptionDelay)
+        {
+            if (nullptr != _introDescription)
+            {
+                _introDescription->StartFade();
+            }
+            _step = FADE_IN_INTRO_DESCRIPTION;
+        }
+        break;
+    case FADE_IN_INTRO_DESCRIPTION:
+        break;
+    case END:
+        break;
+    }
+}
+
+void IntroManager::Reset()
+{
+    Component::Reset();
+
+    _step             = WAIT_INTRO_DESCRIPTION;
+    _introDescription = nullptr;
+    _elapsedTime      = 0.0f;
+}
--- Source/GA6thFinal_Framework/GameScripts/Scripts/IntroManager/IntroManager.h
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/IntroManager/IntroManager.h
@@ -1,8 +1,17 @@
 ﻿#pragma once
 
+class FadeDescriptionPanel;
+
 class IntroManager : public Component
 {
     USING_PROPERTY(IntroManager)
+
+    enum Step
+    {
+        WAIT_INTRO_DESCRIPTION,
+        FADE_IN_INTRO_DESCRIPTION,
+        END
+    };
 
 public:
     IntroManager();
@@ -10,7 +19,24 @@ public:
 public:
     REFLECT_PROPERTY()
 
+    GETTER(float, IntroDescriptionDelay) { return ReflectFields->IntroDescriptionDelay; }
+    SETTER(float, IntroDescriptionDelay) { ReflectFields->IntroDescriptionDelay = std::max(0.0f, value); }
+    PROPERTY(IntroDescriptionDelay)
+
+protected:
+    void Start() override;
+
+    void Update() override;
+
+    void Reset() override;
+
 protected:
     REFLECT_FIELDS_BEGIN(Component)
+    float IntroDescriptionDelay = 2.0f;
     REFLECT_FIELDS_END(IntroManager)
+
+private:
+    Step                  _step;
+    FadeDescriptionPanel* _introDescription;
+    float                 _elapsedTime = 0.0f;
 };
