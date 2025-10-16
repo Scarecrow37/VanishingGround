408aaa8494802a3e92e3e7fe078049126d08df50
Do_Suha <weqa123@naver.com>
2025-10-16 15:49:09 +0900

    [FEATURE/VG-457] UI 애니메이션 및 IntroManager 추가
    - `IntroManager` 클래스와 관련된 헤더 및 소스 파일을 추가하여 초기화 및 리플렉션 기능을 구현했습니다.
    - UI 애니메이션 관련 파일을 추가하여 UI 기능을 확장했습니다.
    - 전투 UI 및 아이템 드롭 시스템 관련 파일을 추가하여 기능을 강화했습니다.

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/IntroManager/IntroManager.cpp
@@ -0,0 +1,6 @@
+﻿#include "pchScripts.h"
+#include "IntroManager.h"
+
+UMREAL_COMPONENT(IntroManager)
+
+IntroManager::IntroManager() = default;
---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/IntroManager/IntroManager.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+class IntroManager : public Component
+{
+    USING_PROPERTY(IntroManager)
+
+public:
+    IntroManager();
+
+public:
+    REFLECT_PROPERTY()
+
+protected:
+    REFLECT_FIELDS_BEGIN(Component)
+    REFLECT_FIELDS_END(IntroManager)
+};
