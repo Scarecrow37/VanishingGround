c8b8e50aa97b78292afc473ccc9e21cfb93fd1d4
Do_Suha <weqa123@naver.com>
2025-10-16 15:48:48 +0900

    [FEATURE/VG-457] UIAnimation 클래스 메서드 추가
    UIAnimation.cpp에 애니메이션의 지속 시간과 루프 여부를 설정하는 Reset 메서드와 애니메이션 진행 상태를 업데이트하는 Update 메서드를 추가했습니다.
    UIAnimation.h 파일에는 클래스 선언과 필요한 멤버 변수를 정의하고, Callback 타입을 통해 애니메이션 진행 상태를 외부에서 처리할 수 있도록 했습니다.

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Animations/UIAnimation.cpp
@@ -0,0 +1,45 @@
+﻿#include "pchScripts.h"
+#include "UIAnimation.h"
+
+void UIAnimation::Reset(const float duration, const bool isLoop)
+{
+    _elapsedTime = 0.0f;
+    _isLoop      = isLoop;
+
+    if (duration <= 0.0f)
+    {
+        _duration = 0.1f;
+        _isStop   = true;
+    }
+    else
+    {
+        _duration = duration;
+        _isStop   = false;
+    }
+}
+
+UIAnimation::UIAnimation(const Callback& callback)
+    : _elapsedTime(0.0f), _duration(0.1f), _isLoop(false), _callback(callback), _isStop(false)
+{
+}
+
+void UIAnimation::Update(const float deltaTime)
+{
+    if (_isStop)
+        return;
+
+    _elapsedTime += deltaTime;
+
+    const float alpha = std::clamp(_elapsedTime / _duration, 0.0f, 1.0f);
+
+    if (_callback)
+        _callback(alpha);
+
+    if (_elapsedTime >= _duration)
+    {
+        if (_isLoop)
+            _elapsedTime -= _duration;
+        else
+            _isStop = true;
+    }
+}
---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Animations/UIAnimation.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+class UIAnimation
+{
+public:
+    using Callback = std::function<void(float alpha)>;
+
+public:
+    UIAnimation(const Callback& callback);
+
+    void Reset(float duration, bool isLoop);
+
+    void Update(float deltaTime);
+
+private:
+    float    _elapsedTime;
+    float    _duration;
+    bool     _isLoop;
+    Callback _callback;
+    bool     _isStop;
+};
