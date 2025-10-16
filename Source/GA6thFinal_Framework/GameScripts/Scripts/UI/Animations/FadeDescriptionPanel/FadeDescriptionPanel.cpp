e7598a296ee975d3979a75ff994edde721460a6c
Do_Suha <weqa123@naver.com>
2025-10-16 15:49:00 +0900

    [FEATURE/VG-457] FadeDescriptionPanel 클래스 추가
    FadeDescriptionPanel.cpp와 FadeDescriptionPanel.h 파일에
    새로운 FadeDescriptionPanel 클래스를 추가하여
    페이드 애니메이션 기능을 구현했습니다.
    이 클래스는 DescriptionPanel과 UIAnimation을 상속받으며,
    애니메이션의 시작, 업데이트, 리셋 및 알파 값 조정을 위한
    여러 메서드를 포함하고 있습니다.
    또한, FadeDuration 속성과 _isFading 변수를 추가하여
    페이드 상태를 관리합니다.

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Animations/FadeDescriptionPanel/FadeDescriptionPanel.cpp
@@ -0,0 +1,41 @@
+﻿#include "pchScripts.h"
+#include "FadeDescriptionPanel.h"
+
+UMREAL_COMPONENT(FadeDescriptionPanel)
+
+FadeDescriptionPanel::FadeDescriptionPanel()
+    : UIAnimation([this](const float alpha) { UpdateAlpha(alpha); }), _isFading(false)
+{
+}
+
+void FadeDescriptionPanel::Start()
+{
+    DescriptionPanel::Start();
+
+    UIAnimation::Reset(ReflectFields->FadeDuration, false);
+}
+
+void FadeDescriptionPanel::Update()
+{
+    DescriptionPanel::Update();
+
+    if (_isFading)
+        UIAnimation::Update(UmTime.DeltaTime());
+}
+
+void FadeDescriptionPanel::Reset()
+{
+    DescriptionPanel::Reset();
+
+    _isFading = false;
+}
+
+void FadeDescriptionPanel::StartFade()
+{
+    _isFading = true;
+}
+
+void FadeDescriptionPanel::UpdateAlpha(const float alpha)
+{
+    Alpha = alpha;
+}
---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Animations/FadeDescriptionPanel/FadeDescriptionPanel.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+#include "UI/Animations/UIAnimation.h"
+#include "UI/Panels/Description/DescriptionPanel.h"
+
+class FadeDescriptionPanel : public DescriptionPanel, public UIAnimation
+{
+    USING_PROPERTY(FadeDescriptionPanel)
+
+public:
+    FadeDescriptionPanel();
+
+public:
+    REFLECT_PROPERTY(FadeDuration)
+
+    GETTER(float, FadeDuration) { return ReflectFields->FadeDuration; }
+    SETTER(float, FadeDuration) { ReflectFields->FadeDuration = std::max(0.1f, value); }
+    PROPERTY(FadeDuration)
+
+public:
+    void StartFade();
+
+protected:
+    void Start() override;
+
+    void Update() override;
+
+    void Reset() override;
+
+private:
+    void UpdateAlpha(float alpha);
+
+protected:
+    REFLECT_FIELDS_BEGIN(DescriptionPanel)
+    float FadeDuration = 1.0f;
+    REFLECT_FIELDS_END(FadeDescriptionPanel)
+
+private:
+    bool _isFading;
+};
