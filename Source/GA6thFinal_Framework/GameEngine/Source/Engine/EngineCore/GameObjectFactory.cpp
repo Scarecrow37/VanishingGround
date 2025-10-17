030ad011711fb2543b5d32bb07086e2d1512b9e6
power12562 <kimsiwoo1005@gmail.com>
2025-10-17 11:10:02 +0900

    [FEATURE/VG-28] 게임 오브젝트가 생성된 프레임 맴버변수, 맴버함수 추가

--- Source/GA6thFinal_Framework/GameEngine/Source/Engine/EngineCore/GameObjectFactory.cpp
+++ Source/GA6thFinal_Framework/GameEngine/Source/Engine/EngineCore/GameObjectFactory.cpp
@@ -925,6 +925,7 @@ void EGameObjectFactory::ResetGameObject(
         // 인스턴스 아이디 부여
         int instanceID           = InstanceID.CreateInstanceID();
         ownerObject->_instanceID = instanceID;
+        ownerObject->_creationFrame = UmTime.FrameCount();
     }
     else
     {
