25409ab35b9ddad680c663d91b396e7852033907
PPAK_JU <taku7664@gmail.com>
2025-10-20 14:56:23 +0900

    [BUILD/VG-325] 레거시 코드 삭제

--- Source/GA6thFinal_Framework/GameScripts/Scripts/EnemyAction/Factory/EnemyActionFactory.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-#include <EnemyAction/EnemyActionBase.h>
-
-class EnemyActionFactory 
-    : public FactoryConstructor<EnemyAction::ActionBase>
-{
-
-};
--- Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnActor/Character/Enemy/State/EnemyPlayTurnState.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnActor/Character/Enemy/State/EnemyPlayTurnState.cpp
@@ -3,18 +3,6 @@
 
 #include <TurnSystem/TurnActor/Character/Enemy/Enemy.h>
 
-#include <EnemyAction/Action/22000_22009/EnemyAction22000.h>
-#include <EnemyAction/Action/22000_22009/EnemyAction22001.h>
-#include <EnemyAction/Action/22000_22009/EnemyAction22002.h>
-#include <EnemyAction/Action/22000_22009/EnemyAction22003.h>
-#include <EnemyAction/Action/22000_22009/EnemyAction22004.h>
-
-#include <EnemyAction/Action/22010_22019/EnemyAction22010.h>
-#include <EnemyAction/Action/22010_22019/EnemyAction22011.h>
-#include <EnemyAction/Action/22010_22019/EnemyAction22012.h>
-#include <EnemyAction/Action/22010_22019/EnemyAction22013.h>
-#include <EnemyAction/Action/22010_22019/EnemyAction22014.h>
-
 REGISTER_CLASS(FSMStateFactory, EnemyPlayTurnState)
 
 // switch문 안에 있어야함
