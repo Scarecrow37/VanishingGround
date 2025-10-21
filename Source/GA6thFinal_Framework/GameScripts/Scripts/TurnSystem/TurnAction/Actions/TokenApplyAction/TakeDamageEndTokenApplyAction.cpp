ca811a9eb12ce901994f411c68315aa3db6b344a
power12562 <kimsiwoo1005@gmail.com>
2025-10-21 11:08:46 +0900

    [CHORE/VG-316] 클래스 이름 변경

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnAction/Actions/TokenApplyAction/TakeDamageEndTokenApplyAction.cpp
@@ -0,0 +1,34 @@
+﻿#include "pchScripts.h"
+#include "TakeDamageEndTokenApplyAction.h"
+
+const std::string& TakeDamageEndTokenApplyAction::GetActionInfo()
+{
+    return _actionInfo;
+}
+
+void TakeDamageEndTokenApplyAction::ImGuiDrawActionEditor() 
+{
+    ImGuiDrawPropertys();
+}
+
+const std::string& TakeDamageEndTokenApplyAction::GetActionName()
+{
+    const static std::string actionName = (const char*)u8"데미지 입을시 토큰 부여";
+    return actionName;
+}
+
+void TakeDamageEndTokenApplyAction::UpdateActionInfo() 
+{
+    _actionInfo.clear();
+
+}
+
+void TakeDamageEndTokenApplyAction::OnPlayerTakeDamageEnd(Player& target, int damage) 
+{
+
+}
+
+void TakeDamageEndTokenApplyAction::OnEnemyTakeDamageEnd(Enemy& target, int damage) 
+{
+
+}
---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnAction/Actions/TokenApplyAction/TakeDamageEndTokenApplyAction.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include <TurnSystem/TurnAction/Actions/TokenApplyAction/Base/TokenApplyAction.h>
+
+class TakeDamageEndTokenApplyAction : public TokenApplyAction
+{
+    USING_PROPERTY(TakeDamageEndTokenApplyAction)
+public:
+    TakeDamageEndTokenApplyAction() = default;
+    ~TakeDamageEndTokenApplyAction() override = default;
+
+public:
+    const std::string& GetActionInfo() override;
+    void               ImGuiDrawActionEditor() override;
+    const std::string& GetActionName() override;
+
+private:
+    void UpdateActionInfo() override;
+    std::string _actionInfo;
+
+protected:
+    /// <summary>
+    /// 플레이어에 TakeDamage가 들어간 후 호출됩니다.
+    /// </summary>
+    /// <param name="target :">>대상</param>
+    /// <param name="damage :">들어간 데미지</param>
+    virtual void OnPlayerTakeDamageEnd(Player& target, int damage) override;
+
+    /// <summary>
+    /// Enemy에 TakeDamage가 들어가기 직전에 호출됩니다.
+    /// </summary>
+    /// <param name="target :">대상</param>
+    /// <param name="damage :">들어갈 데미지</param>
+    virtual void OnEnemyTakeDamageEnd(Enemy& target, int damage) override;
+
+};
