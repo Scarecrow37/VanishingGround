9fdce6a918e7c93f57579a136f944975bbd1b88c
power12562 <kimsiwoo1005@gmail.com>
2025-10-21 11:01:35 +0900

    [REFACTOR/VG-316] 상속 구조 변경

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnAction/Actions/TokenApplyAction/HPDownTokenApplyAction.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include <TurnSystem/TurnAction/Actions/TokenApplyAction/Base/TokenApplyAction.h>
+
+class HPDownTokenApplyAction : public TokenApplyAction
+{
+    USING_PROPERTY(HPDownTokenApplyAction)
+public:
+    HPDownTokenApplyAction() = default;
+    ~HPDownTokenApplyAction() override = default;
+
+public:
+    const std::string& GetActionInfo() override;
+    void               ImGuiDrawActionEditor() override;
+    const std::string& GetActionName() override;
+
+
+private:
+    void UpdateActionInfo() override;
+    std::string _actionInfo;
+
+};
