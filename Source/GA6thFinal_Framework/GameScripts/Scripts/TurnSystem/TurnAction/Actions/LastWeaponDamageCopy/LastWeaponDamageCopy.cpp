0e991d9b83dd0dcfe13cb62f44c4101aa56e34bf
power12562 <kimsiwoo1005@gmail.com>
2025-10-27 17:26:11 +0900

    [FEATURE/VG-316] 마지막 사용 무기 복제하는 액션 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnAction/Actions/LastWeaponDamageCopy/LastWeaponDamageCopy.cpp
@@ -0,0 +1,52 @@
+﻿#include "pchScripts.h"
+#include "LastWeaponDamageCopy.h"
+#include "WeaponSystem/WeaponSystem.h"
+#include "TurnSystem/TurnActor/Character/Player/Player.h"
+
+const std::string& LastWeaponDamageCopy::GetActionNameStr()
+{
+    using namespace u8_literals;
+    const static std::string name = u8"이전 무기의 기본 피해량을 따라한다."_c_str;
+    return name;
+}
+
+const std::string& LastWeaponDamageCopy::GetActionName()
+{   
+    return GetActionNameStr();
+}
+
+const std::string& LastWeaponDamageCopy::GetActionInfo()
+{
+    return GetActionNameStr();
+}
+
+void LastWeaponDamageCopy::ImGuiDrawActionEditor() 
+{
+    ImGuiDrawPropertys();
+    ImguiDrawConditionEditor();
+}
+
+void LastWeaponDamageCopy::OnTurnStart(CharacterBase& destination) 
+{
+    if (WeaponSystem* system = SingletonComponent<WeaponSystem>::GetInstance())
+    {
+        if (EvaluateConditions())
+        {
+            if (typeid(Player) == typeid(destination))
+            {
+                int lastSlot = system->LastWeaponSlot;
+                if (0 <= lastSlot && lastSlot < system->EQUIP_WEAPONS_SIZE)
+                {
+                    WeaponElement& element   = system->GetCurrentWeaponElement();
+                    WeaponStats&   currStats = element.Stats;
+                    WeaponStats&   lastStats = system->GetWeaponStatsAtIndex(lastSlot);
+
+                    currStats.HitDamage                = lastStats.HitDamage;
+                    currStats.HitDamageMultiplier      = lastStats.HitDamageMultiplier;
+                    currStats.CriticalDamage           = lastStats.CriticalDamage;
+                    currStats.CriticalDamageMultiplier = lastStats.CriticalDamageMultiplier;
+                }              
+            }
+        } 
+    }  
+}
