44ca212d440a25a06e8a68f4beda0a7b71f6ea0f
PPAK_JU <taku7664@gmail.com>
2025-10-31 21:06:52 +0900

    [FIX/VG-325] 몬스터 데이터 파싱 로직 수정

--- Source/GA6thFinal_Framework/GameScripts/Scripts/Monster/Context/MonsterDataContext.h
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/Monster/Context/MonsterDataContext.h
@@ -5,10 +5,10 @@ namespace Monster
 {
     struct DataContext
     {
-        DataID          ID                                  = 0;                // 몬스터 ID
-        std::string     Name                                = "";               // 몬스터 이름
-        int             PrefabID                            = 0;                // 몬스터 프리팹 ID
-        std::array<FSMID, MAX_FSM_COUNT>      FsmIDs        = {0, 0, 0};        // AI FSM ID
-        std::array<ActionID, MAX_SKILL_COUNT> ActionIDs     = {0, 0, 0, 0, 0};  // Action ID
+        DataID                  ID          = 0;    // 몬스터 ID
+        std::string             Name        = "";   // 몬스터 이름
+        int                     PrefabID    = 0;    // 몬스터 프리팹 ID
+        std::vector<FSMID>      FsmIDs;             // AI FSM ID
+        std::vector<ActionID>   ActionIDs;          // Action ID
     };
 };
