3c28ebf7a4c7faf1c106a08a36a03faee250d868
SeongU <blacknaco85@gmail.com>
2025-10-31 20:13:06 +0900

    [CHORE/VG-471] 병합전 커밋

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/Ending/EndingDialogManager.cpp
@@ -0,0 +1,74 @@
+﻿#include "pchScripts.h"
+#include "EndingDialogManager.h"
+#include "SceneTransition/SceneTransitionComponent.h"
+#include "UI/Animations/ChildsAnimationsController/ChildsAnimationsController.h"
+UMREAL_COMPONENT(EndingDialogManager)
+
+EndingDialogManager::EndingDialogManager()
+{
+    MainMenuScene.SetInputAutoEvent([this]() {
+        if (ImGui::BeginDragDropTarget())
+        {
+            if (const ImGuiPayload* payLoad = ImGui::AcceptDragDropPayload(DragDropAsset::KEY))
+            {
+                const DragDropAsset::Data* data = static_cast<DragDropAsset::Data*>(payLoad->Data);
+                if (const auto extension = data->GetPath().extension(); extension == L".UmScene")
+                {
+                    ReflectFields->MainMenuSceneGuid = data->GetGuid().string();
+                }
+            }
+            ImGui::EndDragDropTarget();
+        }
+    });
+}
+EndingDialogManager::~EndingDialogManager() = default;
+
+void EndingDialogManager::Reset()
+{
+    _isDialogEnded = false;
+    auto EndingDialog = GameObject::FindWithTag("EndingDialogPannel").lock();
+    if (EndingDialog.get())
+    {
+        _childsAnimationsController = EndingDialog->GetComponent<ChildsAnimationsController>();
+        if (!_childsAnimationsController)
+        {
+            UmLogger.Log(LogLevel::LEVEL_WARNING, "ChildsAnimationsController not found in EndingDialogPannel!");
+        }
+    }
+    else
+    {
+        UmLogger.Log(LogLevel::LEVEL_WARNING, "EndingDialogPannel not found!");
+    }
+}
+
+void EndingDialogManager::Awake() {}
+
+void EndingDialogManager::Update()
+{
+    if (_isDialogEnded)
+    {
+        _isDialogEnded = false;
+        TransitionToMainMenuScene();
+    } 
+    
+}
+
+void EndingDialogManager::TransitionToMainMenuScene()
+{
+    if (ReflectFields->MainMenuSceneGuid.empty())
+    {
+        UmLogger.Log(LogLevel::LEVEL_WARNING, "MainMenuSceneGuid is empty!");
+        return;
+    }
+    File::Path path = File::Guid(ReflectFields->MainMenuSceneGuid).ToPath();   
+    GameObject* transitionManager = SingletonObject<SceneTransitionComponent>::GetInstance();
+    if (transitionManager)
+    {
+        auto transitionComponent = transitionManager->GetComponent<SceneTransitionComponent>();
+        if (transitionComponent)
+        {
+            transitionComponent->SceneTransitionFade("in_fast", "out_fast",
+                                                     [path]() { UmSceneManager.LoadScene(path.string()); });
+        }
+    }
+}
---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/Ending/EndingDialogManager.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+class EndingDialogManager : public Component
+{
+    USING_PROPERTY(EndingDialogManager)
+
+public:
+    EndingDialogManager();
+    ~EndingDialogManager() override;
+
+public:
+    void Reset() override;
+    void Awake() override;
+    void Update() override;
+
+private:
+    void TransitionToMainMenuScene();
+
+public:
+    REFLECT_PROPERTY(MainMenuScene)
+    GETTER_ONLY(std::string, MainMenuScene)
+    {
+        File::Guid guid = ReflectFields->MainMenuSceneGuid;
+        return guid.ToPath().string();
+    }
+    PROPERTY(MainMenuScene)
+
+protected:
+    REFLECT_FIELDS_BEGIN(Component)
+    std::string MainMenuSceneGuid;
+    REFLECT_FIELDS_END(EndingDialogManager)
+private:
+    bool _isDialogEnded = true;
+    class ChildsAnimationsController* _childsAnimationsController = nullptr;
+};
+
