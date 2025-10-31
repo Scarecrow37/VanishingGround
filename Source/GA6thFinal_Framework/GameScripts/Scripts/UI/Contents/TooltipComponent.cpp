aa85fecf4a93e68eef90e2611c64141ebe5f79eb
Do_Suha <weqa123@naver.com>
2025-10-31 10:42:08 +0900

    [FEATURE/VG-468] TooltipComponent 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Contents/TooltipComponent.cpp
@@ -0,0 +1,117 @@
+﻿#include "pchScripts.h"
+#include "TooltipComponent.h"
+#include "Scripts/UI/Elements/Text/TextElement.h"
+#include "Scripts/UI/Panels/Description/DescriptionPanel.h"
+
+UMREAL_COMPONENT(TooltipComponent)
+
+TooltipComponent::TooltipComponent() = default;
+
+void TooltipComponent::SetTooltip(const TooltipData& data) const
+{
+    if (const auto title = _title.lock())
+    {
+        title->Text  = data.Title;
+        title->Color = data.TitleColor;
+    }
+
+    if (const auto description = _description.lock())
+    {
+        description->Description = data.Description;
+    }
+}
+
+void TooltipComponent::ShowTooltip() const
+{
+    if (const auto panel = _panel.lock())
+    {
+        panel->SetActive(true);
+    }
+}
+
+void TooltipComponent::HideTooltip() const
+{
+    if (const auto panel = _panel.lock())
+    {
+        panel->SetActive(false);
+    }
+}
+
+void TooltipComponent::Awake()
+{
+    Component::Awake();
+
+    FindComponent();
+}
+
+void TooltipComponent::Start()
+{
+    Component::Start();
+
+    HideTooltip();
+}
+
+void TooltipComponent::ImGuiDrawPropertysEvent()
+{
+    Component::ImGuiDrawPropertysEvent();
+
+    static TooltipData data = {};
+    ImGui::InputText("Title", &data.Title);
+    ImGui::InputText("Description", &data.Description);
+    ImGui::ColorEdit4("TitleColor", &data.TitleColor.x);
+
+
+    if (ImGui::Button("Set Tooltip"))
+    {
+        SetTooltip(data);
+    }
+
+    if (ImGui::Button("Show Tooltip"))
+    {
+        ShowTooltip();
+    }
+
+    if (ImGui::Button("Hide Tooltip"))
+    {
+        HideTooltip();
+    }
+}
+
+void TooltipComponent::FindComponent()
+{
+    _panel = gameObject->GetWeakPtr();
+
+    if (_panel.expired())
+    {
+        UmLogger.Log(LogLevel::LEVEL_WARNING, "Fail to find tooltip panel object.");
+    }
+
+    if (const Transform* titleTransform = transform->FindWithTag(TAG_TITLE_COMPONENT))
+    {
+        const GameObject&  titleObject       = titleTransform->gameObject;
+        if (const TextElement* titleComponentRaw = titleObject.GetComponentDynamic<TextElement>())
+        {
+            _title = titleComponentRaw->GetWeakPtrAs<TextElement>();
+        }
+    }
+
+    if (_title.expired())
+    {
+        UmLogger.Log(LogLevel::LEVEL_WARNING, "Fail to find tooltip title text component.");
+    }
+
+    if (const Transform* descriptionTransform = transform->FindWithTag(TAG_DESCRIPTION_COMPONENT))
+    {
+        const GameObject&      descriptionObject = descriptionTransform->gameObject;
+        if (const DescriptionPanel* descriptionComponentRaw = descriptionObject.GetComponentDynamic<DescriptionPanel>())
+        {
+            _description = descriptionComponentRaw->GetWeakPtrAs<DescriptionPanel>();
+        }
+    }
+
+    if (_description.expired())
+    {
+        UmLogger.Log(LogLevel::LEVEL_WARNING, "Fail to find tooltip description panel component.");
+    }
+}
+
---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Contents/TooltipComponent.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+
+class TextElement;
+class DescriptionPanel;
+
+class TooltipComponent : public Component
+{
+    USING_PROPERTY(TooltipComponent)
+
+    static constexpr const char* TAG_PANEL_OBJECT          = "Tooltip Panel";
+    static constexpr const char* TAG_TITLE_COMPONENT       = "Tooltip Title";
+    static constexpr const char* TAG_DESCRIPTION_COMPONENT = "Tooltip Description";
+
+public:
+    struct TooltipData
+    {
+        std::string Title;
+        std::string Description;
+        Color TitleColor;
+    };
+
+public:
+    TooltipComponent();
+
+public:
+    REFLECT_PROPERTY()
+
+public:
+    void SetTooltip(const TooltipData& data) const;
+    void ShowTooltip() const;
+    void HideTooltip() const;
+
+protected:
+    void Awake() override;
+    void Start() override;
+
+    void ImGuiDrawPropertysEvent() override;
+
+private:
+    void FindComponent();
+
+protected:
+    REFLECT_FIELDS_BEGIN(Component)
+    REFLECT_FIELDS_END(TooltipComponent)
+
+private:
+    std::weak_ptr<GameObject>       _panel;
+    std::weak_ptr<TextElement>      _title;
+    std::weak_ptr<DescriptionPanel> _description;
+};
