ecc627b8dcea949c4ca622b11c16fcece5ceb465
power12562 <kimsiwoo1005@gmail.com>
2025-11-01 16:37:42 +0900

    [FEATURE/VG-316] 인트로 제어용 컴포넌트 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/BattleIntroUIController/BattleIntroUIController.cpp
@@ -0,0 +1,69 @@
+﻿#include "pchScripts.h"
+#include "BattleIntroUIController.h"
+#include "UI/Animations/ChildsAnimationsController/ChildsAnimationsController.h"
+#include "UI/Animations/FadeUIComponent/FadeUIComponent.h"
+
+UMREAL_COMPONENT(BattleIntroUIController)
+
+BattleIntroUIController::BattleIntroUIController() = default;
+BattleIntroUIController::~BattleIntroUIController() = default;
+
+float BattleIntroUIController::PlayIntro(int stage, int roundCount)
+{
+    float introDuration = 0.f;
+    gameObject->SetActive(true);
+    if (_fadeUIComponent.expired() || _animationController.expired())
+    {
+        FindAnimations();
+    }
+
+    if (auto animation = _animationController.lock())
+    {
+        std::string stageTag = "Stage " + std::to_string(stage);
+        introDuration        = std::max(introDuration, animation->FadeInWithTag(stageTag));
+
+        std::string roundTag = "Round " + std::to_string(roundCount);
+        introDuration        = std::max(introDuration, animation->StartAnimationWithTag(roundTag));
+
+        float delay = IntroTextShowTime;
+        introDuration += delay;
+    }
+
+    if (auto fadeUI = _fadeUIComponent.lock())
+    {
+        UmTime.Invoke(fadeUI.get(), introDuration, [fadeUI = fadeUI.get()]()
+        { 
+            fadeUI->FadeOut();
+        });
+        introDuration += fadeUI->FadeDuration;
+    }
+    UmTime.Invoke(this, introDuration, [this]() 
+    { 
+        gameObject->SetActive(false); 
+    });
+    return introDuration;
+}
+
+void BattleIntroUIController::Added() 
+{
+    if (UmCore->IsPlay())
+    {
+        gameObject->SetActive(false);
+    }
+}
+
+void BattleIntroUIController::FindAnimations()
+{
+    if (Transform* tr =  transform->FindWithTag("Animations"))
+    {
+        GameObject& animationsPanel = tr->gameObject; 
+        if (FadeUIComponent* fadeUI = animationsPanel.GetComponent<FadeUIComponent>())
+        {
+            _fadeUIComponent = fadeUI->GetWeakPtrAs<FadeUIComponent>();
+        }
+        if (ChildsAnimationsController* controller = animationsPanel.GetComponent<ChildsAnimationsController>())
+        {
+            _animationController = controller->GetWeakPtrAs<ChildsAnimationsController>();
+        }
+    }
+}
---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/BattleIntroUIController/BattleIntroUIController.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+class ChildsAnimationsController;
+class FadeUIComponent;
+
+class BattleIntroUIController : public Component
+{
+    USING_PROPERTY(BattleIntroUIController)
+
+public:
+    BattleIntroUIController();
+    ~BattleIntroUIController() override;
+
+    /// <summary>
+    /// 인트로 UI 연출을 실행합니다.
+    /// </summary>
+    /// <param name="stage">: 현재 스테이지</param>
+    /// <param name="roundCount">: 현재 라운드</param>
+    /// <returns>인트로 총 시간</returns>
+    float PlayIntro(int stage, int roundCount);
+
+public:
+    REFLECT_PROPERTY()
+
+    GETTER(float, IntroTextShowTime) { return ReflectFields->IntroTextShowTime; }
+    SETTER(float, IntroTextShowTime) { ReflectFields->IntroTextShowTime = value; }
+    PROPERTY(IntroTextShowTime)
+
+protected:
+    REFLECT_FIELDS_BEGIN(Component)
+    float IntroTextShowTime = 0.5f;
+    REFLECT_FIELDS_END(BattleIntroUIController)
+
+    void Added() override;
+
+private:
+    std::weak_ptr<ChildsAnimationsController> _animationController;
+    std::weak_ptr<FadeUIComponent>            _fadeUIComponent;
+
+private:
+    void FindAnimations();
+};
+
