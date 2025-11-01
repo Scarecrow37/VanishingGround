0acc98356a04607b53c230138d723d533661a41a
power12562 <kimsiwoo1005@gmail.com>
2025-11-02 02:13:43 +0900

    [FEATURE/VG-316] 무기 속도 영구 증가 기능 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnAction/Actions/WeaponStas/WeaponSpeedAction.cpp
@@ -0,0 +1,109 @@
+﻿#include "pchScripts.h"
+#include "WeaponSpeedAction.h"
+#include <Stats/Player/PlayerStats.h>
+#include <Stats/Player/PlayerStatsComponent.h>
+#include <WeaponSystem/WeaponSystem.h>
+
+using namespace u8_literals;
+
+REFLECT_FUNCTION(WeaponSpeedAction)
+
+REGISTER_TURN_ACTION(WeaponSpeedAction)
+
+WeaponSpeedAction::WeaponSpeedAction()
+{
+    UpdateActionInfo();
+}
+
+const std::string& WeaponSpeedAction::GetActionName()
+{
+    static std::string actionName = u8"무기 속도 영구 증가"_c_str;
+    return actionName;
+}
+
+const std::string& WeaponSpeedAction::GetActionInfo()
+{
+    return _actionInfo;
+}
+
+void WeaponSpeedAction::ImGuiDrawActionEditor()
+{
+    ImGui::Text("Action");
+    ImGuiDrawPropertys();
+    ImGui::Separator();
+    ImGui::Text("Condition");
+    ImguiDrawConditionEditor();
+}
+
+void WeaponSpeedAction::DeserializedReflectEvent()
+{
+    UpdateActionInfo();
+}
+
+void WeaponSpeedAction::OnPlayerQTEResult(Player& player, const QTE::OverallResult& result)
+{
+    WeaponSystem* weaponSystem = SingletonComponent<WeaponSystem>::GetInstance();
+    if (weaponSystem)
+    {
+        if (TriggerType::QTE_END == ReflectFields->Trigger)
+        {
+            if (EvaluateConditions())
+            {
+                WeaponStats& stats = weaponSystem->GetCurrentWeaponElement().Stats;
+                stats.Speed += Speed;
+
+                const std::string& weaponName = stats.WeaponName;
+                std::string        msg        = weaponName;
+                msg += u8"의"_c_str;
+                msg += u8"무기의 속도 "_c_str;
+                msg += std::to_string(Speed);
+                msg += u8" 증가"_c_str;
+                UmLogger.Message(LogLevel::LEVEL_INFO, msg);
+            }
+        }
+    }
+}
+
+void WeaponSpeedAction::OnEnemyDeadByWeapon(Enemy& enemy, WeaponElement& weapon)
+{
+    WeaponSystem* weaponSystem = SingletonComponent<WeaponSystem>::GetInstance();
+    if (weaponSystem)
+    {
+        if (TriggerType::ENEMY_DEAD_BY_WEAPON == ReflectFields->Trigger)
+        {
+            if (EvaluateConditions())
+            {
+                WeaponStats& stats = weaponSystem->GetCurrentWeaponElement().Stats;
+                stats.Speed += Speed;
+
+                const std::string& weaponName = stats.WeaponName;
+                std::string        msg        = weaponName;
+                msg += u8"의"_c_str;
+                msg += u8"무기의 속도 "_c_str;
+                msg += std::to_string(Speed);
+                msg += u8" 증가"_c_str;
+                UmLogger.Message(LogLevel::LEVEL_INFO, msg);
+            }
+        }
+    }
+}
+
+void WeaponSpeedAction::UpdateActionInfo()
+{
+    std::string_view triggerInfo;
+    switch (ReflectFields->Trigger)
+    {
+    case TriggerType::QTE_END:
+        triggerInfo = u8"QTE 종료시"_c_str;
+        break;
+    case TriggerType::ENEMY_DEAD_BY_WEAPON:
+        triggerInfo = u8"공격으로 적 처치시"_c_str;
+        break;
+    default:
+        break;
+    }
+    _actionInfo = triggerInfo;
+    _actionInfo += u8"무기의 속도 "_c_str;
+    _actionInfo += std::to_string(Speed);
+    _actionInfo += u8" 증가"_c_str;
+}
---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnAction/Actions/WeaponStas/WeaponSpeedAction.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+#include <TurnSystem/TurnAction/TurnActionFactory.h>
+
+//사용한 무기 스피드 영구 증가
+class WeaponSpeedAction : public TurnAction
+{
+    USING_PROPERTY(WeaponSpeedAction)
+public:
+    WeaponSpeedAction();
+    ~WeaponSpeedAction() override = default;
+
+    enum class TriggerType
+    {
+        QTE_END,
+        ENEMY_DEAD_BY_WEAPON,
+    };
+
+public:
+    GETTER(TriggerType, Trigger) { return ReflectFields->Trigger; }
+    SETTER(TriggerType, Trigger)
+    {
+        ReflectFields->Trigger = value;
+        UpdateActionInfo();
+    }
+    PROPERTY(Trigger)
+
+    GETTER(int, Speed) { return ReflectFields->Speed; }
+    SETTER(int, Speed)
+    {
+        ReflectFields->Speed = value;
+        UpdateActionInfo();
+    }
+    PROPERTY(Speed)
+
+    REFLECT_PROPERTY(Trigger, Speed)
+protected:
+    REFLECT_FIELDS_BEGIN(TurnAction)
+    TriggerType Trigger = TriggerType::QTE_END;
+    int         Speed   = 0;
+    REFLECT_FIELDS_END(WeaponSpeedAction)
+
+    const std::string& GetActionName() override;
+    const std::string& GetActionInfo() override;
+    void               ImGuiDrawActionEditor() override;
+
+    void DeserializedReflectEvent() override;
+    void OnPlayerQTEResult(Player& player, const QTE::OverallResult& result) override;
+    void OnEnemyDeadByWeapon(Enemy& enemy, WeaponElement& weapon) override;
+
+private:
+    void        UpdateActionInfo();
+    std::string _actionInfo;
+};
