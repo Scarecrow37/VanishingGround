8fd366ef5e90a67c2d3025098187a43f91eaacbd
power12562 <kimsiwoo1005@gmail.com>
2025-11-03 02:59:52 +0900

    [FEATURE/VG-316] 추가 토큰 부여 액션 기능 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnAction/Actions/TokenApplyAction/TokenGrantorAction.cpp
@@ -0,0 +1,40 @@
+﻿#include "pchScripts.h"
+#include "TokenGrantorAction.h"
+#include "TurnSystem/TurnSystemHelper.h"
+#include "TurnSystem/TurnAction/TurnActionFactory.h"
+
+REGISTER_TURN_ACTION(TokenGrantorAction)
+
+const std::string& TokenGrantorAction::GetActionName()
+{
+    return NAME;
+}
+
+const std::string& TokenGrantorAction::GetActionInfo()
+{
+    return NAME;
+}
+
+void TokenGrantorAction::ImGuiDrawActionEditor() 
+{
+    ImGuiDrawPropertys();
+}
+
+void TokenGrantorAction::OnTokenAddedStart(CharacterBase& targetCharacter, int& tokenID, int& tokenCount) 
+{
+    if (ReflectFields->TokenID == tokenID)
+    {
+        auto targets = TurnSystemHelper::GetTargetCharacters(ReflectFields->TokenTarget);
+        for (auto& target : targets)
+        {
+            if (target == &targetCharacter)
+            {
+                if (EvaluateConditions())
+                {
+                    tokenCount += ReflectFields->TokenCount;
+                }
+                break;
+            }
+        }     
+    }   
+}
