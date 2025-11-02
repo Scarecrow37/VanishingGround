fbcb9ad01e18e18b9c1cbcfc632e04f589acf306
power12562 <kimsiwoo1005@gmail.com>
2025-11-02 18:56:28 +0900

    [FEATURE/VG-316] QTE 속도 제어 액션 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnAction/Actions/QTESpeedAction/QTESpeedAction.cpp
@@ -0,0 +1,36 @@
+﻿#include "pchScripts.h"
+#include "QTESpeedAction.h"
+
+#include "TurnSystem/TurnAction/TurnActionFactory.h"
+
+REFLECT_FUNCTION(QTESpeedAction)
+
+REGISTER_TURN_ACTION(QTESpeedAction)
+
+const std::string& QTESpeedAction::GetActionName()
+{
+    return NAME;
+}
+
+const std::string& QTESpeedAction::GetActionInfo()
+{
+    return NAME;
+}
+
+void QTESpeedAction::ImGuiDrawActionEditor() 
+{
+    ImGuiDrawPropertys();
+    ImguiDrawConditionEditor();
+}
+
+void QTESpeedAction::OnEquipAccessory() 
+{
+    if (ReflectFields->Trigger == TriggerType::EQUIP_ACCESSORY)
+    {
+        if (EvaluateConditions())
+        {
+            //TODO: QTE 속도 제어해야함
+            int speed = ReflectFields->Speed;
+        }
+    }
+}
---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnAction/Actions/QTESpeedAction/QTESpeedAction.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include "TurnSystem/TurnAction/TurnAction.h"
+
+class QTESpeedAction : public TurnAction
+{
+    USING_PROPERTY(QTESpeedAction)
+    inline static const std::string NAME = (const char*)u8"QTE 속도를 조절합니다.";
+public:
+    enum class TriggerType
+    {
+        EQUIP_ACCESSORY,
+    };
+
+    QTESpeedAction() = default;
+    ~QTESpeedAction() = default;
+   
+protected:
+    REFLECT_PROPERTY(
+        ReflectFields->Trigger,
+        ReflectFields->Speed
+    )
+
+    REFLECT_FIELDS_BEGIN(TurnAction)
+    TriggerType Trigger = TriggerType::EQUIP_ACCESSORY;
+    int         Speed   = 0;
+    REFLECT_FIELDS_END(QTESpeedAction)
+    
+// TurnAction을(를) 통해 상속됨
+    const std::string& GetActionName() override;
+    const std::string& GetActionInfo() override;
+    void               ImGuiDrawActionEditor() override;
+
+    void OnEquipAccessory() override;
+};
+
