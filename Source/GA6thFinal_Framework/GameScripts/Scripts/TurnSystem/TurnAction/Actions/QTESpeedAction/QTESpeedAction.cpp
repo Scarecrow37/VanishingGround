a2d99c6023ee62f1cc0c12a73edbf30e67dbb54d
power12562 <kimsiwoo1005@gmail.com>
2025-11-02 21:25:45 +0900

    [FIX/VG-316] QTE 속도 액션 기능 추가

--- Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnAction/Actions/QTESpeedAction/QTESpeedAction.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnAction/Actions/QTESpeedAction/QTESpeedAction.cpp
@@ -1,7 +1,7 @@
 ﻿#include "pchScripts.h"
 #include "QTESpeedAction.h"
 
-#include "Token/TokenSystem.h"
+#include "QTE/System/QTESystem.h"
 #include "TurnSystem/TurnAction/TurnActionFactory.h"
 
 REFLECT_FUNCTION(QTESpeedAction)
@@ -31,9 +31,9 @@ void QTESpeedAction::OnEquipAccessory()
         if (EvaluateConditions())
         {          
             int speed = ReflectFields->Speed;
-            if (TokenSystem* system = SingletonComponent<TokenSystem>::GetInstance())
+            if (QTESystem* system = SingletonComponent<QTESystem>::GetInstance())
             {
-                //TODO: QTE 속도 조정 필요
+                system->IncreaseQTESpeedLevel(speed);
             }
         }
     }
