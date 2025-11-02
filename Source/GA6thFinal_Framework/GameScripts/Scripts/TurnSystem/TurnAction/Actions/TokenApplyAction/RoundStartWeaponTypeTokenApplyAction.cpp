15b20bd816fb082dc03bbecc825dc3e069d4015f
power12562 <kimsiwoo1005@gmail.com>
2025-11-03 00:07:41 +0900

    [FEATURE/VG-316] 라운드 시작 시 특정 무기 종류 개수만큼 토큰 획득 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnAction/Actions/TokenApplyAction/RoundStartWeaponTypeTokenApplyAction.cpp
@@ -0,0 +1,56 @@
+﻿#include "pchScripts.h"
+#include "RoundStartWeaponTypeTokenApplyAction.h"
+#include "TurnSystem/TurnAction/TurnActionFactory.h"
+#include "Token/TokenSystem.h"
+#include "TurnSystem/TurnSystemHelper.h"
+#include "TurnSystem/TurnActor/Character/CharacterBase.h"
+#include "WeaponSystem/WeaponSystem.h"
+
+REGISTER_TURN_ACTION(RoundStartWeaponTypeTokenApplyAction)
+REFLECT_FUNCTION(RoundStartWeaponTypeTokenApplyAction)
+
+const std::string& RoundStartWeaponTypeTokenApplyAction::GetActionInfo()
+{
+    TryTokenSystemInfoUpdate();
+    return NAME;
+}
+
+void RoundStartWeaponTypeTokenApplyAction::ImGuiDrawActionEditor()
+{
+    ImGuiDrawPropertys();
+}
+
+const std::string& RoundStartWeaponTypeTokenApplyAction::GetActionName()
+{
+    return NAME;
+}
+
+void RoundStartWeaponTypeTokenApplyAction::OnRoundStart()
+{
+    if (EvaluateConditions())
+    {
+        if (TokenSystem* tokenSystem = SingletonComponent<TokenSystem>::GetInstance())
+        {
+            std::vector<CharacterBase*> targets = TurnSystemHelper::GetTargetCharacters(Target);
+            int tokenCount = TokenCount;
+            int typeCount = 0;
+            if (WeaponSystem* system = SingletonComponent<WeaponSystem>::GetInstance())
+            {
+                typeCount = system->GetEquipWeaponTypeCount(ReflectFields->Type);
+            }
+
+            int applyCount = tokenCount * typeCount;
+            if (0 < applyCount)
+            {
+                if (false == targets.empty())
+                {
+                    for (auto& target : targets)
+                    {
+                        TokenInventory& tokenInventory = target->GetTokenInventory();
+                        tokenInventory.AddTokenStackFromID(TokenID, applyCount);
+                    }
+                }
+            }
+        }
+    }
+}
