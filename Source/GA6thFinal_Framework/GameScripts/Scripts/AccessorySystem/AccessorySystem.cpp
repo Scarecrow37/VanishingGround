b1d4f8dcf8adb46519728a92cc663ce7fbd92f2a
power12562 <kimsiwoo1005@gmail.com>
2025-11-03 02:12:56 +0900

    [FIX/VG-316] 플레이중일때만 액션 호출하도록 수정

--- Source/GA6thFinal_Framework/GameScripts/Scripts/AccessorySystem/AccessorySystem.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/AccessorySystem/AccessorySystem.cpp
@@ -24,11 +24,14 @@ bool AccessorySystem::EquipAccessory(const AccessoryElement& accessory)
     if (result)
     {
         _playerAccessoryItems.push_back(accessory);
-        const auto& lastAccessory = _playerAccessoryItems[_playerAccessoryItems.size() - 1];
-        for (auto& action : lastAccessory._actions)
+        if (UmCore->IsPlay())
         {
-            if (action)
-                action->OnEquipAccessory();
+            const auto& lastAccessory = _playerAccessoryItems[_playerAccessoryItems.size() - 1];
+            for (auto& action : lastAccessory._actions)
+            {
+                if (action)
+                    action->OnEquipAccessory();
+            }
         }
     }
     return result;
