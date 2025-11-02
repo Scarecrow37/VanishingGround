e46ae545381fb068fa8e575e04d06804255b5dcd
power12562 <kimsiwoo1005@gmail.com>
2025-11-02 22:54:52 +0900

    [FEATURE/VG-316] 무기 종류별 데미지 증가 기능 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnAction/Actions/WeaponStas/WeaponTypeStatsAction.cpp
@@ -0,0 +1,92 @@
+﻿#include "pchScripts.h"
+#include "WeaponTypeStatsAction.h"
+#include "TurnSystem/TurnAction/TurnActionFactory.h"
+
+REFLECT_FUNCTION(WeaponTypeStatsAction)
+REGISTER_TURN_ACTION(WeaponTypeStatsAction)
+
+WeaponTypeStatsAction::WeaponTypeStatsAction() = default;
+
+const std::string& WeaponTypeStatsAction::GetActionName()
+{
+    return NAME;
+}
+
+const std::string& WeaponTypeStatsAction::GetActionInfo()
+{
+    return NAME;
+}
+
+void WeaponTypeStatsAction::ImGuiDrawActionEditor() 
+{
+    ImGuiDrawPropertys();
+    ImguiDrawConditionEditor();
+}
+
+void WeaponTypeStatsAction::OnPlayerBattleCalculateChainModifier(Player& attacker, PlayerStats& attackerStats,
+                                                                 WeaponStats& weaponStats, Enemy& target,
+                                                                 EnemyStats& targetStats)
+{
+    if (weaponStats.Type == ReflectFields->Type)
+    {
+        if (EvaluateConditions())
+        {
+            // 연격만 증가
+            int chain = ReflectFields->AttackPerChain;
+            float multiplier = ReflectFields->AttackPerChainMultiplier;
+
+            weaponStats.AttackPerChain += chain;
+            weaponStats.AttackPerChainMultiplier *= multiplier;
+        }
+    }
+}
+
+void WeaponTypeStatsAction::OnPlayerBattleCalculateDamageModifier(Player& attacker, PlayerStats& attackerStats,
+                                                                  WeaponStats& weaponStats, Enemy& target,
+                                                                  EnemyStats& targetStats)
+{
+    if (weaponStats.Type == ReflectFields->Type)
+    {
+        if (EvaluateConditions())
+        {
+            // 데미지만 증가
+            int hitDamage = ReflectFields->HitDamage;
+            float hitMultiplier = ReflectFields->HitDamageMultiplier;
+            weaponStats.HitDamage += hitDamage;
+            weaponStats.HitDamageMultiplier *= hitMultiplier;
+
+            int criticalDamage = ReflectFields->CriticalDamage;
+            float criticalMultiplier = ReflectFields->CriticalDamageMultiplier;
+            weaponStats.CriticalDamage += criticalDamage;
+            weaponStats.CriticalDamageMultiplier *= criticalMultiplier;
+        }
+    }
+}
+
+void WeaponTypeStatsAction::OnWeaponRoundSpeedApply(WeaponElement& weapon, int& plusSpeed) 
+{
+    if (weapon.Stats.Type == ReflectFields->Type)
+    {
+        if (EvaluateConditions())
+        {
+            //속도 증가 (UI도 적용됨)
+            plusSpeed += ReflectFields->Speed;
+        }
+    }
+}
+
+void WeaponTypeStatsAction::OnConvertWeaponViewModel(WeaponStats& stats) 
+{
+    if (0 == ConditionCount) //상시 적용만 UI 적용
+    {
+        if (stats.Type == ReflectFields->Type)
+        {
+            if (EvaluateConditions())
+            {
+                // UI 적용
+                stats.HitDamage += ReflectFields->HitDamage;
+                stats.CriticalDamage += ReflectFields->CriticalDamage;
+            }
+        }
+    }
+}
---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnAction/Actions/WeaponStas/WeaponTypeStatsAction.h
@@ -0,0 +1,51 @@
+﻿#pragma once
+#include <TurnSystem/TurnAction/TurnActionFactory.h>
+#include "WeaponSystem/WeaponElement/WeaponElement.h"
+
+//특정 종류에 대한 무기 스텟 항상 증가
+class WeaponTypeStatsAction : public TurnAction
+{
+    USING_PROPERTY(WeaponTypeStatsAction)
+public:
+    inline static const std::string NAME = (const char*)u8"특정 종류에 대한 무기 스텟 항상 증가";
+
+    WeaponTypeStatsAction();
+    ~WeaponTypeStatsAction() override = default;
+
+public:
+    REFLECT_PROPERTY(
+        ReflectFields->Type,
+        ReflectFields->HitDamage,
+        ReflectFields->HitDamageMultiplier,
+        ReflectFields->CriticalDamage,
+        ReflectFields->CriticalDamageMultiplier,
+        ReflectFields->Speed,
+        ReflectFields->AttackPerChain,
+        ReflectFields->AttackPerChainMultiplier
+    )
+protected:
+    REFLECT_FIELDS_BEGIN(TurnAction)
+    WeaponType Type                     = WeaponType::SWORD;
+    int        HitDamage                = 0;
+    float      HitDamageMultiplier      = 1.f;
+    int        CriticalDamage           = 0;
+    float      CriticalDamageMultiplier = 1.f;
+    int        Speed                    = 0;
+    int        AttackPerChain           = 0;
+    float      AttackPerChainMultiplier = 1.f;
+    REFLECT_FIELDS_END(WeaponTypeStatsAction)
+
+    const std::string& GetActionName() override;
+    const std::string& GetActionInfo() override;
+    void               ImGuiDrawActionEditor() override;
+
+    void OnPlayerBattleCalculateChainModifier(Player& attacker, PlayerStats& attackerStats, WeaponStats& weaponStats,
+                                              Enemy& target, EnemyStats& targetStats) override;
+
+    void OnPlayerBattleCalculateDamageModifier(Player& attacker, PlayerStats& attackerStats, WeaponStats& weaponStats,
+                                               Enemy& target, EnemyStats& targetStats) override;
+
+    void OnWeaponRoundSpeedApply(WeaponElement& weapon, int& plusSpeed) override;
+
+    void OnConvertWeaponViewModel(WeaponStats& stats) override;
+};
