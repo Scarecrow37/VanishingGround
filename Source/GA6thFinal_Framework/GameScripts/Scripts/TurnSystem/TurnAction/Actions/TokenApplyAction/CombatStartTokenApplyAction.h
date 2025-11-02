9e4a0f6c299e19417744b266643758fa8bdfb582
power12562 <kimsiwoo1005@gmail.com>
2025-11-02 16:51:12 +0900

    [FEATURE/VG-316] 전투 시작시 토큰 부여 액션 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnAction/Actions/TokenApplyAction/CombatStartTokenApplyAction.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "TurnSystem/TurnAction/Actions/TokenApplyAction/Base/TokenApplyAction.h"
+
+class CombatStartTokenApplyAction : public TokenApplyAction
+{
+    USING_PROPERTY(CombatStartTokenApplyAction)
+public:
+    CombatStartTokenApplyAction()          = default;
+    ~CombatStartTokenApplyAction() override = default;
+
+public:
+    const std::string& GetActionInfo() override;
+    void               ImGuiDrawActionEditor() override;
+    const std::string& GetActionName() override;
+    void               OnCombatStart() override;
+
+private:
+    void        UpdateActionInfo() override;
+    std::string _actionInfo;
+};
