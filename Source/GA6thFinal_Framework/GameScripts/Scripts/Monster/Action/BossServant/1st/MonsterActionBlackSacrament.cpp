794886e61c9f241fb55b14bcf51d454f9a6e94e7
PPAK_JU <taku7664@gmail.com>
2025-11-03 13:06:19 +0900

    [FIX/VG-437] 몬스터 보스 부하 액션 수정

--- Source/GA6thFinal_Framework/GameScripts/Scripts/Monster/Action/BossServant/1st/MonsterActionBlackSacrament.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/Monster/Action/BossServant/1st/MonsterActionBlackSacrament.cpp
@@ -32,19 +32,6 @@ namespace Monster
                     tokenInventory.AddTokenStackFromID(tokenParam.TokenID, tokenParam.Count);
                 }
             }
-            {   // 자신에게 T_Param(2)를 S_Param(1)~S_Param(2) 만큼 부여한다.
-                if (CharacterBase* target = GetOwnerEnemy())
-                {
-                    TokenParam  tokenParam     = GetTokenParam(2);
-                    ActionParam actionParam1   = GetActionParam(1);
-                    ActionParam actionParam2   = GetActionParam(2);
-
-                    int randomRange = Random::Range(actionParam1.Param, actionParam2.Param);
-
-                    TokenInventory& tokenInventory = target->GetTokenInventory();
-                    tokenInventory.AddTokenStackFromID(tokenParam.TokenID, randomRange);
-                }
-            }
         }
     }
 }
