03fc9a05778da0d65286c86471a94f440aa82a9e
PPAK_JU <taku7664@gmail.com>
2025-11-03 12:16:45 +0900

    [FIX/VG-437] 검은 갑옷 토큰 로직 수정

--- Source/GA6thFinal_Framework/GameScripts/Scripts/Token/Object/BlackArmor/BlackArmorToken.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/Token/Object/BlackArmor/BlackArmorToken.cpp
@@ -15,10 +15,11 @@ namespace TokenObject
             auto& tokenInventory = owner->GetTokenInventory();
             int   tokenID        = GetTokenID();
             tokenInventory.RemoveTokenStackFromID(tokenID);
-            // 토큰이 0이 되면 기절 부여
+            // 토큰이 0이 되면 기절 저항 제거하고 기절 부여
             if (false == tokenInventory.HasTokenFromID(tokenID))
             {
                 int param = GetTokenParam(0);
+                tokenInventory.RemoveTokenFromID(TokenObject::StunResistance::ID);
                 tokenInventory.AddTokenStackFromID(TokenObject::Stun::ID, param);
             }
             UmLogger.Log(LogLevel::LEVEL_TRACE, TokenLog(*owner));
