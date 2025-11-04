2153cc83da707e524b26ae07e58fefa9aa52d893
power12562 <kimsiwoo1005@gmail.com>
2025-11-04 21:16:13 +0900

    [FIX/VG-316] UI 초기화 코드 호출 타이밍 개선

--- Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnMode/State/CombatStartPhase.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnMode/State/CombatStartPhase.cpp
@@ -130,19 +130,6 @@ void CombatStartPhase::OnAwake()
 void CombatStartPhase::OnStart() 
 {
     TurnModeStateBase::OnStart();
-    RefreshUI(); //Find UI가 가장먼저 호출되야함
-    if (CombatUIManager* combatUIManager = SingletonComponent<CombatUIManager>::GetInstance())
-    {
-        // 켜져 있어야 하는거
-        combatUIManager->AccessoriesGroup.ActiveUI(true);
-        combatUIManager->ConsumableGroup.ActiveUI(true);
-
-        // 꺼져 있어야 하는거
-        combatUIManager->CharacterHUDGroup.ActiveUI(false);
-        combatUIManager->WeaponGroup.ActiveUI(false);
-        combatUIManager->RevelationsGroup.ActiveUI(false);
-        combatUIManager->TurnQueueGroup.ActiveUI(false);
-    }
 
     if (MonsterSystem* system = SingletonComponent<MonsterSystem>::GetInstance())
     {
@@ -477,13 +464,5 @@ void CombatStartPhase::ResetPlayer()
     if (_player)
     {
         _player->TurnActor::Revive();
-    }
-}
-
-void CombatStartPhase::RefreshUI() 
-{
-    if (CombatUIManager* manager = SingletonComponent<CombatUIManager>::GetInstance())
-    {
-        manager->Refresh();
     }
 }
