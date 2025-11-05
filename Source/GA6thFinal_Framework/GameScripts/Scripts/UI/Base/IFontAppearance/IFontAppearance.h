338cf5d2f51f21b4e33a21841e750f6cf19db03b
Do_Suha <weqa123@naver.com>
2025-11-05 12:07:31 +0900

    [FEATURE/VG-316] GlobalFontManager 추가 및 IFontAppearance 인터페이스 구현

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/UI/Base/IFontAppearance/IFontAppearance.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+class IFontAppearance
+{
+public:
+    IFontAppearance()                        = default;
+    IFontAppearance(const IFontAppearance&)      = default;
+    IFontAppearance& operator=(const IFontAppearance&) = default;
+    IFontAppearance(IFontAppearance&&)                 = default;
+    IFontAppearance& operator=(IFontAppearance&&)      = default;
+    virtual ~IFontAppearance()                     = default;
+
+    virtual void SetFontWeight(float fontWeight) = 0;
+};
