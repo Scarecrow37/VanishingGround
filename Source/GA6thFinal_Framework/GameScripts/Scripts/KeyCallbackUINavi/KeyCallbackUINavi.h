e8fbfc82f3ea1046dd89826445998589c1297601
power12562 <kimsiwoo1005@gmail.com>
2025-11-06 11:34:16 +0900

    [FEATURE/VG-316] KeyCallbackUINavi 추가

---
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/KeyCallbackUINavi/KeyCallbackUINavi.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+class KeyCallbackUINavi : public UISFXNavigationComponent
+{
+    USING_PROPERTY(KeyCallbackUINavi)
+
+public:
+    KeyCallbackUINavi();
+    ~KeyCallbackUINavi() override;
+
+    inline static std::unordered_map<std::string, UmDelegate<>> EventListenerMapFocusIn;
+    inline static std::unordered_map<std::string, UmDelegate<>> EventListenerMapFocusOut;
+    inline static std::unordered_map<std::string, UmDelegate<>> EventListenerMapSubmit;
+
+    inline static std::unordered_map<std::string, UmDelegate<>> EventListenerMapShowTooltips;
+    inline static std::unordered_map<std::string, UmDelegate<>> EventListenerMapHideTooltips;
+
+public:
+    GETTER(const std::string&, CallbackKey) { return ReflectFields->CallbackKey; }
+    SETTER(const std::string&, CallbackKey) { ReflectFields->CallbackKey = value; }
+    // 콜백 함수 키
+    PROPERTY(CallbackKey)
+
+    REFLECT_PROPERTY(CallbackKey)
+protected:
+    REFLECT_FIELDS_BEGIN(UISFXNavigationComponent)
+    std::string CallbackKey;
+    REFLECT_FIELDS_END(KeyCallbackUINavi)
+
+    void Added() override;
+
+    void FocusIn(FocusCallType callType) override;
+    void FocusOut(FocusCallType callType) override;
+    void Submit() override;
+
+    void ShowTooltips() override;
+    void HideTooltips() override;
+};
+
