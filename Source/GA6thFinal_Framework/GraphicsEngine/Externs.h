ba463da00160e601fc6f8f5ee289eb94c4f4b309
bmbany <59551445+bmbany@users.noreply.github.com>
2025-11-10 12:59:44 +0900

    [FIX/VG-472] 외부 DLL에서 전역변수 접근 시 null이 나오는 문제 수정
    - 외부 DLL에서 dummyTextureHandle 접근 시 빈 값으로 나오던 버그 수정

--- Source/GA6thFinal_Framework/GraphicsEngine/Externs.h
+++ Source/GA6thFinal_Framework/GraphicsEngine/Externs.h
@@ -2,22 +2,22 @@
 
 namespace Global
 {
-    extern Device*                     device;
-    extern Renderer*                   renderer;
-    extern CommandController*          commandController;
-    extern DXResourceManager*          dxResourceManager;
-    extern MultiRenderTargetManager*   multiRenderTargetManager;
-    extern ResourceManager*            resourceManager;
-    extern ViewManager*                viewManager;
-    extern AnimationCore*              animationCore;
-    extern LightCore*                  lightCore;
-    extern ParticleManager*            particleManager;
-    extern DebugDrawCore*              debugDrawCore;
-    extern RenderPassDatas*            renderPassDatas;
-    extern ModuleManager*              moduleManager;
-    extern PipelineStateManager*       pipelineStateManager;
-    extern ThreadPool*                 threadPool;
-    extern bool                        isRayTracing;
-    extern SceneTransitionCore*        sceneTransitionCore;
-    extern D3D12_GPU_DESCRIPTOR_HANDLE dummyTextureHandle;
+    extern Device*                      device;
+    extern Renderer*                    renderer;
+    extern CommandController*           commandController;
+    extern DXResourceManager*           dxResourceManager;
+    extern MultiRenderTargetManager*    multiRenderTargetManager;
+    extern ResourceManager*             resourceManager;
+    extern ViewManager*                 viewManager;
+    extern AnimationCore*               animationCore;
+    extern LightCore*                   lightCore;
+    extern ParticleManager*             particleManager;
+    extern DebugDrawCore*               debugDrawCore;
+    extern RenderPassDatas*             renderPassDatas;
+    extern ModuleManager*               moduleManager;
+    extern PipelineStateManager*        pipelineStateManager;
+    extern ThreadPool*                  threadPool;
+    extern bool                         isRayTracing;
+    extern SceneTransitionCore*         sceneTransitionCore;
+    extern D3D12_GPU_DESCRIPTOR_HANDLE* dummyTextureHandle;
 }
