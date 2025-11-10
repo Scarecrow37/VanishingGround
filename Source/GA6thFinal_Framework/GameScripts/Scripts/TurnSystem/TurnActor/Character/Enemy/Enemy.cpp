7e14c9ab4bc40f6724ca79fd54478c1899b41033
power12562 <kimsiwoo1005@gmail.com>
2025-11-10 10:57:43 +0900

    [FEATURE/VG-316] 몬스터 액션 툴팁 기능 추가

--- Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnActor/Character/Enemy/Enemy.cpp
+++ Source/GA6thFinal_Framework/GameScripts/Scripts/TurnSystem/TurnActor/Character/Enemy/Enemy.cpp
@@ -672,9 +672,21 @@ void Enemy::ShowTooltip()
     {
         if (TooltipSystem* system = SingletonComponent<TooltipSystem>::GetInstance())
         {
-            auto&            tokenInventory = GetTokenInventory();
-            std::vector<int> ids            = tokenInventory.GetTokensTooltips();
-            system->Show(Tooltip::Group::ENEMY, ids);
+            // 토큰 툴팁
+            auto& tokenInventory = GetTokenInventory();
+            std::vector<int> tooltipIds = tokenInventory.GetTokensTooltips();
+
+            // 패턴 툴팁
+            Monster::Controller& controller = GetController();
+            if (auto* currAction = controller.GetCurrentAction())
+            {
+                std::vector<int> actionTooltips = currAction->GetActionTooltipIDs();
+                if (false == actionTooltips.empty())
+                {
+                    std::ranges::move(actionTooltips, std::back_inserter(tooltipIds));
+                }           
+            }
+            system->Show(Tooltip::Group::ENEMY, tooltipIds);
         }
     }
 }
